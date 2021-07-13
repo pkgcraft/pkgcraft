e2ff5ba2e8e138f73148bac2dff6fd7e8a36cf9a
Tim Harder <radhermit@gmail.com>
2021-07-13 17:09:37 -0600

    benches: always use latest EAPI for parsing

--- benches/atom.rs
+++ benches/atom.rs
@@ -3,34 +3,34 @@ use std::str::FromStr;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use arcanist::atom;
-use arcanist::eapi;
+use arcanist::eapi::EAPI_LATEST;
 
 #[allow(unused_must_use)]
 fn bench_parse_unversioned(c: &mut Criterion) {
     c.bench_function("atom-unversioned", |b| b.iter(|| {
-        atom::parse("cat/pkg", &eapi::EAPI1);
+        atom::parse("cat/pkg", EAPI_LATEST);
     }));
 
     c.bench_function("atom-slotdep", |b| b.iter(|| {
-        atom::parse("cat/pkg:0", &eapi::EAPI5);
+        atom::parse("cat/pkg:0", EAPI_LATEST);
     }));
 
     c.bench_function("atom-versioned", |b| b.iter(|| {
-        atom::parse(">=cat/pkg-4-r1", &eapi::EAPI1);
+        atom::parse(">=cat/pkg-4-r1", EAPI_LATEST);
     }));
 
     c.bench_function("atom-versioned-slotdep", |b| b.iter(|| {
-        atom::parse(">=cat/pkg-4-r1:0=", &eapi::EAPI5);
+        atom::parse(">=cat/pkg-4-r1:0=", EAPI_LATEST);
     }));
 
     c.bench_function("atom-usedeps", |b| b.iter(|| {
-        atom::parse(">=cat/pkg-4-r1:0=[a,b=,!c=,d?,!e?,-f]", &eapi::EAPI5);
+        atom::parse(">=cat/pkg-4-r1:0=[a,b=,!c=,d?,!e?,-f]", EAPI_LATEST);
     }));
 
     c.bench_function("atom-long-usedeps", |b| {
         let flags: Vec<String> = (0..100).map(|s| s.to_string()).collect();
         let s = format!("cat/pkg[{}]", &flags.join(","));
-        b.iter(|| atom::parse(&s, &eapi::EAPI5));
+        b.iter(|| atom::parse(&s, EAPI_LATEST));
     });
 
     c.bench_function("atom-sorting", |b| {
