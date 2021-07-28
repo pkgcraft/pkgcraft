37ec47405bf1c0988b22bbd0a4b946a6e1d0df61
Tim Harder <radhermit@gmail.com>
2021-07-28 05:59:27 -0600

    arcanist: add initial stubs for args and config support

--- arcanist/src/main.rs
+++ arcanist/src/main.rs
@@ -1,3 +1,43 @@
-pub fn main() -> Result<(), &'static str> {
+use anyhow::Result;
+use clap::{App, AppSettings, Arg};
+
+use settings::Settings;
+
+mod settings;
+
+fn main() -> Result<()> {
+    let app = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("package-building daemon leveraging pkgcraft")
+        .setting(AppSettings::DisableHelpSubcommand)
+        .setting(AppSettings::DisableVersionForSubcommands)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .arg(Arg::new("debug").long("debug").about("enable debug output"))
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .multiple_occurrences(true)
+                .about("enable verbose output"),
+        )
+        .arg(
+            Arg::new("quiet")
+                .short('q')
+                .long("quiet")
+                .multiple_occurrences(true)
+                .about("suppress non-error messages"),
+        );
+
+    let matches = app.get_matches();
+
+    // load config settings and then override them with command-line settings
+    let mut settings = Settings::new()?;
+
+    if matches.is_present("debug") {
+        settings.debug = true;
+    }
+    settings.verbosity += matches.occurrences_of("verbose") as i32;
+    settings.verbosity -= matches.occurrences_of("quiet") as i32;
+
     Ok(())
 }
---
+++ arcanist/src/settings.rs
@@ -0,0 +1,28 @@
+use anyhow::{Context, Result};
+use config::{Config, Environment};
+use pkgcraft::config::Config as PkgcraftConfig;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct Settings {
+    pub debug: bool,
+    pub verbosity: i32,
+    pub config: PkgcraftConfig,
+}
+
+impl Settings {
+    pub fn new() -> Result<Self> {
+        let mut s = Config::default();
+
+        // use defaults
+        s.merge(Config::try_from(&Settings::default())?)
+            .context("failed merging config defaults")?;
+
+        // env variables override defaults
+        s.merge(Environment::with_prefix("ARCANIST").separator("_"))
+            .context("failed merging env settings")?;
+
+        // serialize to struct
+        s.try_into().context("failed serializing settings")
+    }
+}
