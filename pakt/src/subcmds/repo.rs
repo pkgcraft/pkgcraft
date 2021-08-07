69c513c336771cf1333d0e123f4162ba2b9228e7
Tim Harder <radhermit@gmail.com>
2021-08-07 05:09:08 -0600

    pakt repo: another minor command description simplification

--- pakt/src/subcmds/repo.rs
+++ pakt/src/subcmds/repo.rs
@@ -2,7 +2,7 @@ use clap::{App, AppSettings};
 
 pub fn cmd() -> App<'static> {
     App::new("repo")
-        .about("manage available repos")
+        .about("manage repos")
         .setting(AppSettings::DisableHelpSubcommand)
         .setting(AppSettings::DisableVersionForSubcommands)
         .setting(AppSettings::SubcommandRequiredElseHelp)
