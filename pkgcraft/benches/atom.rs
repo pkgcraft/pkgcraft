e28593e631d873b97cfa4d8b7ba21005d97de877
Tim Harder <radhermit@gmail.com>
2021-08-09 18:02:14 -0600

    pkgcraft: update benchmarks for atom parsing changes

--- pkgcraft/benches/atom.rs
+++ pkgcraft/benches/atom.rs
@@ -9,38 +9,38 @@ use pkgcraft::eapi::EAPI_LATEST;
 pub fn bench_parse_unversioned(c: &mut Criterion) {
     c.bench_function("atom-unversioned", |b| {
         b.iter(|| {
-            atom::parse("cat/pkg", EAPI_LATEST);
+            atom::parse::dep("cat/pkg", EAPI_LATEST);
         })
     });
 
     c.bench_function("atom-slotdep", |b| {
         b.iter(|| {
-            atom::parse("cat/pkg:0", EAPI_LATEST);
+            atom::parse::dep("cat/pkg:0", EAPI_LATEST);
         })
     });
 
     c.bench_function("atom-versioned", |b| {
         b.iter(|| {
-            atom::parse(">=cat/pkg-4-r1", EAPI_LATEST);
+            atom::parse::dep(">=cat/pkg-4-r1", EAPI_LATEST);
         })
     });
 
     c.bench_function("atom-versioned-slotdep", |b| {
         b.iter(|| {
-            atom::parse(">=cat/pkg-4-r1:0=", EAPI_LATEST);
+            atom::parse::dep(">=cat/pkg-4-r1:0=", EAPI_LATEST);
         })
     });
 
     c.bench_function("atom-usedeps", |b| {
         b.iter(|| {
-            atom::parse(">=cat/pkg-4-r1:0=[a,b=,!c=,d?,!e?,-f]", EAPI_LATEST);
+            atom::parse::dep(">=cat/pkg-4-r1:0=[a,b=,!c=,d?,!e?,-f]", EAPI_LATEST);
         })
     });
 
     c.bench_function("atom-long-usedeps", |b| {
         let flags: Vec<String> = (0..100).map(|s| s.to_string()).collect();
         let s = format!("cat/pkg[{}]", &flags.join(","));
-        b.iter(|| atom::parse(&s, EAPI_LATEST));
+        b.iter(|| atom::parse::dep(&s, EAPI_LATEST));
     });
 
     c.bench_function("atom-sorting", |b| {
