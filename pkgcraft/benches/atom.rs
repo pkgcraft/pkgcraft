422a1f5b7b5b57dc2cb1c90724df629b4773e558
Tim Harder <radhermit@gmail.com>
2021-08-12 03:26:49 -0600

    pkgcraft: simplify atom benchmarks to parse via EAPI method

--- pkgcraft/benches/atom.rs
+++ pkgcraft/benches/atom.rs
@@ -1,51 +1,36 @@
-use std::str::FromStr;
-
 use criterion::Criterion;
 
-use pkgcraft::atom;
 use pkgcraft::eapi::EAPI_LATEST;
 
 #[allow(unused_must_use)]
 pub fn bench_parse_unversioned(c: &mut Criterion) {
     c.bench_function("atom-unversioned", |b| {
-        b.iter(|| {
-            atom::parse::dep("cat/pkg", EAPI_LATEST);
-        })
+        b.iter(|| EAPI_LATEST.atom("cat/pkg"))
     });
 
-    c.bench_function("atom-slotdep", |b| {
-        b.iter(|| {
-            atom::parse::dep("cat/pkg:0", EAPI_LATEST);
-        })
-    });
+    c.bench_function("atom-slotdep", |b| b.iter(|| EAPI_LATEST.atom("cat/pkg:0")));
 
     c.bench_function("atom-versioned", |b| {
-        b.iter(|| {
-            atom::parse::dep(">=cat/pkg-4-r1", EAPI_LATEST);
-        })
+        b.iter(|| EAPI_LATEST.atom(">=cat/pkg-4-r1"))
     });
 
     c.bench_function("atom-versioned-slotdep", |b| {
-        b.iter(|| {
-            atom::parse::dep(">=cat/pkg-4-r1:0=", EAPI_LATEST);
-        })
+        b.iter(|| EAPI_LATEST.atom(">=cat/pkg-4-r1:0="))
     });
 
     c.bench_function("atom-usedeps", |b| {
-        b.iter(|| {
-            atom::parse::dep(">=cat/pkg-4-r1:0=[a,b=,!c=,d?,!e?,-f]", EAPI_LATEST);
-        })
+        b.iter(|| EAPI_LATEST.atom(">=cat/pkg-4-r1:0=[a,b=,!c=,d?,!e?,-f]"))
     });
 
     c.bench_function("atom-long-usedeps", |b| {
         let flags: Vec<String> = (0..100).map(|s| s.to_string()).collect();
         let s = format!("cat/pkg[{}]", &flags.join(","));
-        b.iter(|| atom::parse::dep(&s, EAPI_LATEST));
+        b.iter(|| EAPI_LATEST.atom(&s));
     });
 
     c.bench_function("atom-sorting", |b| {
-        let mut atoms: Vec<atom::Atom> = (0..100)
-            .map(|s| atom::Atom::from_str(&format!("=cat/pkg-{}", s)).unwrap())
+        let mut atoms: Vec<_> = (0..100)
+            .map(|s| EAPI_LATEST.atom(&format!("=cat/pkg-{}", s)).unwrap())
             .collect();
         b.iter(|| atoms.sort());
     });
