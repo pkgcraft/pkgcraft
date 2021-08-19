2653bce3a2b690ffa0e04bec4a76312e65d8b84e
Tim Harder <radhermit@gmail.com>
2021-08-19 00:43:52 -0600

    pkgcraft: split config paths into their own struct

--- pkgcraft/src/config.rs
+++ pkgcraft/src/config.rs
@@ -9,18 +9,18 @@ use crate::error::Error;
 mod repo;
 
 #[derive(Debug, Default, Deserialize, Serialize)]
-pub struct Config {
-    cache_dir: PathBuf,
-    config_dir: PathBuf,
-    data_dir: PathBuf,
-    db_dir: PathBuf,
-    pub repos: repo::Config,
+pub struct ConfigPath {
+    cache: PathBuf,
+    config: PathBuf,
+    data: PathBuf,
+    db: PathBuf,
+    run: PathBuf,
 }
 
-impl Config {
-    pub fn new(name: &str, prefix: &str, create: bool) -> crate::Result<Config> {
+impl ConfigPath {
+    fn new(name: &str, prefix: &str, create: bool) -> crate::Result<ConfigPath> {
         let home = env::var("HOME").ok().unwrap_or_else(|| "/root".to_string());
-        let (config_dir, cache_dir, data_dir, db_dir): (PathBuf, PathBuf, PathBuf, PathBuf);
+        let (config, cache, data, db, run): (PathBuf, PathBuf, PathBuf, PathBuf, PathBuf);
 
         // prefix a given path
         let prefixed = |p: PathBuf| -> PathBuf {
@@ -39,52 +39,75 @@ impl Config {
         let system_config = prefixed(PathBuf::from(format!("/etc/{}", name)));
 
         // determine if user config or system config will be used
-        config_dir = match (
+        config = match (
             user_config.exists(),
             system_config.exists() || home == "/root",
         ) {
             (false, true) => {
-                cache_dir = prefixed(PathBuf::from(format!("/var/cache/{}", name)));
-                data_dir = prefixed(PathBuf::from(format!("/usr/share/{}", name)));
-                db_dir = prefixed(PathBuf::from(format!("/var/db/{}", name)));
+                cache = prefixed(PathBuf::from(format!("/var/cache/{}", name)));
+                data = prefixed(PathBuf::from(format!("/usr/share/{}", name)));
+                db = prefixed(PathBuf::from(format!("/var/db/{}", name)));
+                run = prefixed(PathBuf::from(format!("/run/{}", name)));
                 system_config
             }
             _ => {
-                // pull user cache dir from $XDG_CACHE_HOME, otherwise $HOME/.cache
-                cache_dir = match env::var("XDG_CACHE_HOME") {
+                // pull user cache path from $XDG_CACHE_HOME, otherwise $HOME/.cache
+                cache = match env::var("XDG_CACHE_HOME") {
                     Ok(x) => prefixed([&x, name].iter().collect::<PathBuf>()),
                     Err(_) => prefixed([&home, ".cache", name].iter().collect::<PathBuf>()),
                 };
 
-                // pull user data dir from $XDG_DATA_HOME, otherwise $HOME/.local/share
-                data_dir = match env::var("XDG_DATA_HOME") {
+                // pull user data path from $XDG_DATA_HOME, otherwise $HOME/.local/share
+                data = match env::var("XDG_DATA_HOME") {
                     Ok(x) => prefixed([&x, name].iter().collect::<PathBuf>()),
                     Err(_) => {
                         prefixed([&home, ".local", "share", name].iter().collect::<PathBuf>())
                     }
                 };
 
-                db_dir = data_dir.clone();
+                db = data.clone();
+                run = cache.clone();
                 user_config
             }
         };
 
-        // create dirs on request
+        // create paths on request
         if create {
-            for dir in [&cache_dir, &config_dir, &data_dir, &db_dir] {
-                fs::create_dir_all(dir).map_err(|e| Error::Config(e.to_string()))?;
+            for path in [&cache, &config, &data, &db, &run] {
+                fs::create_dir_all(path).map_err(|e| Error::Config(e.to_string()))?;
             }
         }
 
-        let repos = repo::Config::new(&config_dir, &db_dir)?;
-
-        Ok(Config {
-            cache_dir,
-            config_dir,
-            data_dir,
-            db_dir,
-            repos,
+        Ok(ConfigPath {
+            cache,
+            config,
+            data,
+            db,
+            run,
         })
+    }
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct Config {
+    pub path: ConfigPath,
+    pub repos: repo::Config,
+}
+
+impl Config {
+    pub fn new(name: &str, prefix: &str, create: bool) -> crate::Result<Config> {
+        let path = ConfigPath::new(name, prefix, create)?;
+        let repos = repo::Config::new(&path.config, &path.db)?;
+        Ok(Config { path, repos })
+    }
+
+    pub fn get_socket(&self, name: &str, refresh: bool) -> crate::Result<PathBuf> {
+        let socket = self.path.run.join(name);
+        if refresh {
+            fs::create_dir_all(&self.path.run).map_err(|e| Error::Config(e.to_string()))?;
+            fs::remove_file(&socket).unwrap_or_default();
+        }
+        Ok(socket)
     }
 }
 
@@ -102,13 +125,13 @@ mod tests {
 
         // XDG var and HOME are set
         let config = Config::new("pkgcraft", "", false).unwrap();
-        assert_eq!(config.cache_dir, PathBuf::from("/cache/pkgcraft"));
-        assert_eq!(config.config_dir, PathBuf::from("/config/pkgcraft"));
+        assert_eq!(config.path.cache, PathBuf::from("/cache/pkgcraft"));
+        assert_eq!(config.path.config, PathBuf::from("/config/pkgcraft"));
 
         // prefix
         let config = Config::new("pkgcraft", "/prefix", false).unwrap();
-        assert_eq!(config.cache_dir, PathBuf::from("/prefix/cache/pkgcraft"));
-        assert_eq!(config.config_dir, PathBuf::from("/prefix/config/pkgcraft"));
+        assert_eq!(config.path.cache, PathBuf::from("/prefix/cache/pkgcraft"));
+        assert_eq!(config.path.config, PathBuf::from("/prefix/config/pkgcraft"));
 
         env::remove_var("XDG_CACHE_HOME");
         env::remove_var("XDG_CONFIG_HOME");
@@ -116,28 +139,28 @@ mod tests {
         // XDG var is unset and HOME is set
         let config = Config::new("pkgcraft", "", false).unwrap();
         assert_eq!(
-            config.cache_dir,
+            config.path.cache,
             PathBuf::from("/home/user/.cache/pkgcraft")
         );
         assert_eq!(
-            config.config_dir,
+            config.path.config,
             PathBuf::from("/home/user/.config/pkgcraft")
         );
 
         // prefix
         let config = Config::new("pkgcraft", "/prefix", false).unwrap();
         assert_eq!(
-            config.cache_dir,
+            config.path.cache,
             PathBuf::from("/prefix/home/user/.cache/pkgcraft")
         );
         assert_eq!(
-            config.config_dir,
+            config.path.config,
             PathBuf::from("/prefix/home/user/.config/pkgcraft")
         );
         env::remove_var("HOME");
 
         // XDG var and HOME are unset
         let config = Config::new("pkgcraft", "", false).unwrap();
-        assert_eq!(config.config_dir, PathBuf::from("/etc/pkgcraft"));
+        assert_eq!(config.path.config, PathBuf::from("/etc/pkgcraft"));
     }
 }
