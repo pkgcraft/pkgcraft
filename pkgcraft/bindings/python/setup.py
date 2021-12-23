fd6fd731e37b488fe317cc3d29829febe07ae82f
Tim Harder <radhermit@gmail.com>
2021-12-23 12:23:30 -0700

    pkgcraft: drop cython-based bindings in favor of ones using pyo3

--- pkgcraft/bindings/python/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import Extension, setup
-from Cython.Build import cythonize
-
-extensions = [Extension("pkgcraft", ["src/pkgcraft.pyx"], libraries=["pkgcraft"])]
-
-setup(
-    name='python bindings for pkgcraft',
-    ext_modules=cythonize(extensions),
-)
