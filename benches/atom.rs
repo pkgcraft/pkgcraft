e17d544d01e3c06aac8944096db2a78f86a604d8
Tim Harder <radhermit@gmail.com>
2021-12-29 10:53:03 -0700

    benches: update atom parsing benchmarks and add simple atom comparisons

--- benches/atom.rs
+++ benches/atom.rs
@@ -1,36 +1,59 @@
+use std::str::FromStr;
+
 use criterion::Criterion;
 
-use pkgcraft::eapi::EAPI_LATEST;
+use pkgcraft::atom::Atom;
 
 #[allow(unused_must_use)]
 pub fn bench_parse_unversioned(c: &mut Criterion) {
-    c.bench_function("atom-unversioned", |b| {
-        b.iter(|| EAPI_LATEST.atom("cat/pkg"))
+    c.bench_function("atom-parse-unversioned", |b| {
+        b.iter(|| Atom::from_str("cat/pkg"))
     });
 
-    c.bench_function("atom-slotdep", |b| b.iter(|| EAPI_LATEST.atom("cat/pkg:0")));
-
-    c.bench_function("atom-versioned", |b| {
-        b.iter(|| EAPI_LATEST.atom(">=cat/pkg-4-r1"))
+    c.bench_function("atom-parse-slotdep", |b| {
+        b.iter(|| Atom::from_str("cat/pkg:0"))
     });
 
-    c.bench_function("atom-versioned-slotdep", |b| {
-        b.iter(|| EAPI_LATEST.atom(">=cat/pkg-4-r1:0="))
+    c.bench_function("atom-parse-versioned", |b| {
+        b.iter(|| Atom::from_str(">=cat/pkg-4-r1"))
     });
 
-    c.bench_function("atom-usedeps", |b| {
-        b.iter(|| EAPI_LATEST.atom(">=cat/pkg-4-r1:0=[a,b=,!c=,d?,!e?,-f]"))
+    c.bench_function("atom-parse-versioned-slotdep", |b| {
+        b.iter(|| Atom::from_str(">=cat/pkg-4-r1:0="))
     });
 
-    c.bench_function("atom-long-usedeps", |b| {
+    c.bench_function("atom-parse-usedeps", |b| {
+        b.iter(|| Atom::from_str(">=cat/pkg-4-r1:0=[a,b=,!c=,d?,!e?,-f]"))
+    });
+
+    c.bench_function("atom-parse-long-usedeps", |b| {
         let flags: Vec<String> = (0..100).map(|s| s.to_string()).collect();
         let s = format!("cat/pkg[{}]", &flags.join(","));
-        b.iter(|| EAPI_LATEST.atom(&s));
+        b.iter(|| Atom::from_str(&s));
     });
 
-    c.bench_function("atom-sorting", |b| {
+    c.bench_function("atom-cmp-eq", |b| {
+        let a1 = Atom::from_str("=cat/pkg-1.2.3").unwrap();
+        let a2 = Atom::from_str("=cat/pkg-1.2.3").unwrap();
+        b.iter(|| a1 == a2);
+    });
+
+    c.bench_function("atom-cmp-lt", |b| {
+        let a1 = Atom::from_str("=cat/pkg-1.2.3_alpha").unwrap();
+        let a2 = Atom::from_str("=cat/pkg-1.2.3").unwrap();
+        b.iter(|| a1 < a2);
+    });
+
+    c.bench_function("atom-cmp-gt", |b| {
+        let a1 = Atom::from_str("=cat/pkg-1.2.3_p").unwrap();
+        let a2 = Atom::from_str("=cat/pkg-1.2.3").unwrap();
+        b.iter(|| a1 > a2);
+    });
+
+    c.bench_function("atom-cmp-sort", |b| {
         let mut atoms: Vec<_> = (0..100)
-            .map(|s| EAPI_LATEST.atom(&format!("=cat/pkg-{}", s)).unwrap())
+            .rev()
+            .map(|s| Atom::from_str(&format!("=cat/pkg-{}", s)).unwrap())
             .collect();
         b.iter(|| atoms.sort());
     });
