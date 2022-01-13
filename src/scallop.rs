29cf300130ffd718215b40a74a63b0da7d9150a5
Tim Harder <radhermit@gmail.com>
2022-01-13 14:16:05 -0700

    scallop: add initial global build state using thread locals
    Since bash isn't thread-friendly and each build will need its own
    process when using the scallop backend, we can resort to using thread
    locals for mutable global state in builtins and other bash-related
    support.
    
    Mostly for testing/dev purposes right now to wire up more builtins.

--- src/scallop.rs
+++ src/scallop.rs
@@ -1,6 +1,55 @@
+use std::cell::RefCell;
+use std::collections::VecDeque;
+
+use indexmap::IndexSet;
+
+use crate::eapi::Eapi;
 use crate::Error;
 
 pub mod builtins;
+
+#[derive(Debug, Default, Clone)]
+pub struct BuildData {
+    pub repo: String,
+    pub eapi: &'static Eapi,
+
+    /// Eclasses directly inherited by an ebuild.
+    pub inherit: Vec<String>,
+    /// Full set of eclasses inherited by an ebuild.
+    pub inherited: IndexSet<String>,
+
+    // ebuild metadata fields
+    pub iuse: VecDeque<String>,
+    pub required_use: VecDeque<String>,
+    pub depend: VecDeque<String>,
+    pub rdepend: VecDeque<String>,
+    pub pdepend: VecDeque<String>,
+    pub bdepend: VecDeque<String>,
+    pub idepend: VecDeque<String>,
+    pub properties: VecDeque<String>,
+    pub restrict: VecDeque<String>,
+}
+
+impl BuildData {
+    pub fn get_deque(&mut self, name: &str) -> &mut VecDeque<String> {
+        match name {
+            "IUSE" => &mut self.iuse,
+            "REQUIRED_USE" => &mut self.required_use,
+            "DEPEND" => &mut self.depend,
+            "RDEPEND" => &mut self.rdepend,
+            "PDEPEND" => &mut self.pdepend,
+            "BDEPEND" => &mut self.bdepend,
+            "IDEPEND" => &mut self.idepend,
+            "PROPERTIES" => &mut self.properties,
+            "RESTRICT" => &mut self.restrict,
+            s => panic!("unknown field name: {}", s),
+        }
+    }
+}
+
+thread_local! {
+    pub static BUILD_DATA: RefCell<BuildData> = RefCell::new(Default::default());
+}
 
 impl From<Error> for scallop::Error {
     fn from(e: Error) -> Self {
