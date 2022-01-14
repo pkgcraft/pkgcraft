cca2fbbcb9517af877c9261f5699cb6b1c62c833
Tim Harder <radhermit@gmail.com>
2022-01-14 00:31:06 -0700

    scallop: fix usex default values override logic

--- src/scallop/builtins/usex.rs
+++ src/scallop/builtins/usex.rs
@@ -8,13 +8,14 @@ Returns --with-${opt} and --without-${opt} configure flags based on a given USE 
 
 #[doc = stringify!(LONG_DOC)]
 pub(crate) fn run(args: &[&str]) -> Result<ExecStatus> {
+    let defaults = ["", "yes", "no", "", ""];
     let (flag, vals) = match args.len() {
-        1..=5 => {
-            // override missing args with default values
-            let mut vals = ["", "yes", "no", "", ""];
-            let start = args.len();
-            let stop = vals.len();
-            vals[start..stop].clone_from_slice(&args[start..stop]);
+        1 => (args[0], defaults),
+        2..=5 => {
+            // override default values with args
+            let stop = args.len();
+            let mut vals = defaults;
+            vals[1..stop].clone_from_slice(&args[1..stop]);
             (args[0], vals)
         }
         n => return Err(Error::new(format!("requires 1 to 5 args, got {}", n))),
