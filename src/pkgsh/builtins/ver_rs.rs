2d0b299fcee10f3d91329959281d7971229552b5
Tim Harder <radhermit@gmail.com>
2022-02-05 01:39:43 -0700

    pkgsh: add expected output tests for `ver_rs` builtin

--- src/pkgsh/builtins/ver_rs.rs
+++ src/pkgsh/builtins/ver_rs.rs
@@ -63,11 +63,65 @@ pub static BUILTIN: Builtin = Builtin {
 
 #[cfg(test)]
 mod tests {
+    use std::io::Read;
+
     use super::super::assert_invalid_args;
     use super::run as ver_rs;
 
-    #[test]
-    fn invalid_args() {
-        assert_invalid_args(ver_rs, vec![0, 1]);
+    use gag::BufferRedirect;
+    use rusty_fork::rusty_fork_test;
+    use scallop::builtins::ExecStatus;
+    use scallop::variables::*;
+
+    rusty_fork_test! {
+        #[test]
+        fn invalid_args() {
+            assert_invalid_args(ver_rs, vec![0, 1]);
+        }
+
+        #[test]
+        fn invalid_range() {
+            for rng in ["-", "-2"] {
+                let r = ver_rs(&[rng, "2", "1.2.3"]);
+                assert!(r.unwrap_err().to_string().contains("invalid range"));
+            }
+        }
+
+        #[test]
+        fn output() {
+            let mut pv = Variable::new("PV");
+            let mut buf = BufferRedirect::stdout().unwrap();
+            for (mut args, expected) in [
+                    (vec!["1", "-", "1.2.3"], "1-2.3"),
+                    (vec!["2", "-", "1.2.3"], "1.2-3"),
+                    (vec!["1-2", "-", "1.2.3.4"], "1-2-3.4"),
+                    (vec!["2-", "-", "1.2.3.4"], "1.2-3-4"),
+                    (vec!["2", ".", "1.2-3"], "1.2.3"),
+                    (vec!["3", ".", "1.2.3a"], "1.2.3.a"),
+                    (vec!["2-3", "-", "1.2_alpha4"], "1.2-alpha-4"),
+                    (vec!["3", "-", "2", "", "1.2.3b_alpha4"], "1.23-b_alpha4"),
+                    (vec!["3-5", "_", "4-6", "-", "a1b2c3d4e5"], "a1b_2-c-3-d4e5"),
+                    (vec!["1", "-", ".1.2.3"], ".1-2.3"),
+                    (vec!["0", "-", ".1.2.3"], "-1.2.3"),
+                    (vec!["0", "-", "1.2.3"], "1.2.3"),
+                    (vec!["3", ".", "1.2.3"], "1.2.3"),
+                    (vec!["3-", ".", "1.2.3"], "1.2.3"),
+                    (vec!["3-5", ".", "1.2.3"], "1.2.3"),
+                    ] {
+                let r = ver_rs(args.as_slice()).unwrap();
+                let mut output = String::new();
+                buf.read_to_string(&mut output).unwrap();
+                assert_eq!(&output[..], expected);
+                assert_eq!(r, ExecStatus::Success);
+
+                // test pulling version from $PV
+                pv.bind(args.pop().unwrap(), None, None).unwrap();
+                let r = ver_rs(args.as_slice()).unwrap();
+                let mut output = String::new();
+                buf.read_to_string(&mut output).unwrap();
+                assert_eq!(&output[..], expected);
+                assert_eq!(r, ExecStatus::Success);
+            }
+        }
     }
 }
