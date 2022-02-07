9d4d6e90a7e82869567cfe6d067a368c7d3eaf7d
Tim Harder <radhermit@gmail.com>
2022-02-07 03:29:58 -0700

    pkgsh: add initial output tests for `use_enable` and `use_with`

--- src/pkgsh/builtins/use_enable.rs
+++ src/pkgsh/builtins/use_enable.rs
@@ -18,3 +18,110 @@ pub static BUILTIN: Builtin = Builtin {
     usage: "use_enable flag",
     error_func: Some(output_error_func),
 };
+
+#[cfg(test)]
+mod tests {
+    use std::io::Read;
+
+    use super::super::assert_invalid_args;
+    use super::run as use_enable;
+    use crate::eapi::OFFICIAL_EAPIS;
+    use crate::macros::assert_err_re;
+    use crate::pkgsh::BUILD_DATA;
+
+    use gag::BufferRedirect;
+    use rusty_fork::rusty_fork_test;
+    use scallop::builtins::ExecStatus;
+
+    rusty_fork_test! {
+        #[test]
+        fn invalid_args() {
+            assert_invalid_args(use_enable, vec![0, 4]);
+
+            BUILD_DATA.with(|d| {
+                for eapi in OFFICIAL_EAPIS.values() {
+                    if !eapi.has("use_conf_arg") {
+                        d.borrow_mut().eapi = eapi;
+                        assert_invalid_args(use_enable, vec![3]);
+                    }
+                }
+            });
+        }
+
+        #[test]
+        fn empty_iuse_effective() {
+            assert_err_re!(use_enable(&["use"]), "^.* not in IUSE$");
+        }
+
+        #[test]
+        fn disabled() {
+            BUILD_DATA.with(|d| {
+                d.borrow_mut().iuse_effective.insert("use".to_string());
+                let mut buf = BufferRedirect::stdout().unwrap();
+
+                for (args, status, expected) in [
+                        (vec!["use"], ExecStatus::Failure, "--disable-use"),
+                        (vec!["use", "opt"], ExecStatus::Failure, "--disable-opt"),
+                        (vec!["!use", "opt"], ExecStatus::Success, "--enable-opt"),
+                        ] {
+                    assert_eq!(use_enable(args.as_slice()).unwrap(), status);
+                    let mut output = String::new();
+                    buf.read_to_string(&mut output).unwrap();
+                    assert_eq!(output, expected);
+                }
+
+                // check EAPIs that support two arg variant
+                for eapi in OFFICIAL_EAPIS.values() {
+                    if eapi.has("use_conf_arg") {
+                        d.borrow_mut().eapi = eapi;
+                        for (args, status, expected) in [
+                                (&["use", "opt", "val"], ExecStatus::Failure, "--disable-opt=val"),
+                                (&["!use", "opt", "val"], ExecStatus::Success, "--enable-opt=val"),
+                                ] {
+                            assert_eq!(use_enable(args).unwrap(), status);
+                            let mut output = String::new();
+                            buf.read_to_string(&mut output).unwrap();
+                            assert_eq!(output, expected);
+                        }
+                    }
+                }
+            });
+        }
+
+        #[test]
+        fn enabled() {
+            BUILD_DATA.with(|d| {
+                d.borrow_mut().iuse_effective.insert("use".to_string());
+                d.borrow_mut().use_.insert("use".to_string());
+
+                let mut buf = BufferRedirect::stdout().unwrap();
+                for (args, status, expected) in [
+                        (vec!["use"], ExecStatus::Success, "--enable-use"),
+                        (vec!["use", "opt"], ExecStatus::Success, "--enable-opt"),
+                        (vec!["!use", "opt"], ExecStatus::Failure, "--disable-opt"),
+                        ] {
+                    assert_eq!(use_enable(args.as_slice()).unwrap(), status);
+                    let mut output = String::new();
+                    buf.read_to_string(&mut output).unwrap();
+                    assert_eq!(output, expected);
+                }
+
+                // check EAPIs that support two arg variant
+                for eapi in OFFICIAL_EAPIS.values() {
+                    if eapi.has("use_conf_arg") {
+                        d.borrow_mut().eapi = eapi;
+                        for (args, status, expected) in [
+                                (&["use", "opt", "val"], ExecStatus::Success, "--enable-opt=val"),
+                                (&["!use", "opt", "val"], ExecStatus::Failure, "--disable-opt=val"),
+                                ] {
+                            assert_eq!(use_enable(args).unwrap(), status);
+                            let mut output = String::new();
+                            buf.read_to_string(&mut output).unwrap();
+                            assert_eq!(output, expected);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
