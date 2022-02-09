8dd01266caeacc9d2a545130a3cef515ef8d5865
Tim Harder <radhermit@gmail.com>
2022-02-08 22:49:43 -0700

    pkgsh: verify args exist for `dodoc`

--- src/pkgsh/builtins/dodoc.rs
+++ src/pkgsh/builtins/dodoc.rs
@@ -1,14 +1,19 @@
 use once_cell::sync::Lazy;
 use scallop::builtins::{Builtin, ExecStatus};
-use scallop::Result;
+use scallop::{Error, Result};
 
 use super::PkgBuiltin;
 
 static LONG_DOC: &str = "Install documentation files.";
 
 #[doc = stringify!(LONG_DOC)]
-pub(crate) fn run(_args: &[&str]) -> Result<ExecStatus> {
+pub(crate) fn run(args: &[&str]) -> Result<ExecStatus> {
+    if args.is_empty() {
+        return Err(Error::Builtin("requires 1 or more args, got 0".into()));
+    }
+
     // TODO: fill out this stub
+
     Ok(ExecStatus::Success)
 }
 
@@ -24,3 +29,14 @@ pub(super) static BUILTIN: Lazy<PkgBuiltin> = Lazy::new(|| {
         &["src_install"],
     )
 });
+
+#[cfg(test)]
+mod tests {
+    use super::super::assert_invalid_args;
+    use super::run as dodoc;
+
+    #[test]
+    fn invalid_args() {
+        assert_invalid_args(dodoc, &[0]);
+    }
+}
