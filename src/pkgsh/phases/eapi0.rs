38df20df70102fc3d95000110dd48f8541921d99
Tim Harder <radhermit@gmail.com>
2022-02-09 02:01:22 -0700

    pkgsh: don't call `unpack` if no distfiles exist for src_unpack()

--- src/pkgsh/phases/eapi0.rs
+++ src/pkgsh/phases/eapi0.rs
@@ -7,6 +7,9 @@ pub(crate) fn src_unpack() -> Result<ExecStatus> {
     BUILD_DATA.with(|d| -> Result<ExecStatus> {
         let distfiles = &d.borrow().distfiles;
         let args: Vec<&str> = distfiles.iter().map(|s| s.as_str()).collect();
-        unpack::run(&args)
+        match !args.is_empty() {
+            true => unpack::run(&args),
+            false => Ok(ExecStatus::Success),
+        }
     })
 }
