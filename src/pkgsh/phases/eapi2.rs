e3e7adca3b3e95400b33e93d2651ce636e170c41
Tim Harder <radhermit@gmail.com>
2022-02-09 05:07:05 -0700

    pkgsh: use proper string expansion for src_configure() default

--- src/pkgsh/phases/eapi2.rs
+++ src/pkgsh/phases/eapi2.rs
@@ -1,15 +1,15 @@
-use std::path::PathBuf;
+use std::path::Path;
 
 use is_executable::IsExecutable;
 use scallop::builtins::ExecStatus;
-use scallop::variables::string_value;
+use scallop::variables::expand;
 use scallop::Result;
 
 use super::super::builtins::econf;
 
 pub(crate) fn src_configure() -> Result<ExecStatus> {
-    let dir = string_value("ECONF_SOURCE").unwrap_or_else(|| String::from("."));
-    let configure: PathBuf = [&dir, "configure"].iter().collect();
+    let path = expand("${ECONF_SOURCE:-.}/configure").unwrap();
+    let configure = Path::new(&path);
     match configure.is_executable() {
         true => econf::run(&[]),
         false => Ok(ExecStatus::Success),
