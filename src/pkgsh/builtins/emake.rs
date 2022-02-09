0a20d8df2b2367bdcfe6ac6252761c777fb99ad4
Tim Harder <radhermit@gmail.com>
2022-02-09 16:09:48 -0700

    pkgsh: add initial implementation for `emake` builtin

--- src/pkgsh/builtins/emake.rs
+++ src/pkgsh/builtins/emake.rs
@@ -1,15 +1,27 @@
+use std::process::Command;
+
 use once_cell::sync::Lazy;
 use scallop::builtins::{Builtin, ExecStatus};
-use scallop::Result;
+use scallop::variables::{string_value, string_vec};
+use scallop::{Error, Result};
 
 use super::{PkgBuiltin, GLOBAL};
 
 static LONG_DOC: &str = "Run the make command for a package.";
 
 #[doc = stringify!(LONG_DOC)]
-pub(crate) fn run(_args: &[&str]) -> Result<ExecStatus> {
-    // TODO: fill out this stub
-    Ok(ExecStatus::Success)
+pub(crate) fn run(args: &[&str]) -> Result<ExecStatus> {
+    let make_prog = string_value("MAKE").unwrap_or_else(|| String::from("make"));
+    let mut make = Command::new(make_prog);
+    if let Ok(opts) = string_vec("MAKEOPTS") {
+        make.args(&opts);
+    }
+    make.args(args);
+
+    make.status().map_or_else(
+        |e| Err(Error::Builtin(format!("failed running: {}", e))),
+        |v| Ok(ExecStatus::from(v)),
+    )
 }
 
 pub(super) static BUILTIN: Lazy<PkgBuiltin> = Lazy::new(|| {
