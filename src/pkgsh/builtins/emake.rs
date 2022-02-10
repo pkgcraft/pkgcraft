76d3d89bebaee3e563756ce45da51377a8bb5186
Tim Harder <radhermit@gmail.com>
2022-02-10 12:01:40 -0700

    emake: return error if makefile doesn't exist

--- src/pkgsh/builtins/emake.rs
+++ src/pkgsh/builtins/emake.rs
@@ -6,11 +6,16 @@ use scallop::variables::{string_value, string_vec};
 use scallop::{Error, Result};
 
 use super::{PkgBuiltin, PHASE};
+use crate::pkgsh::utils::makefile_exists;
 
 static LONG_DOC: &str = "Run the make command for a package.";
 
 #[doc = stringify!(LONG_DOC)]
 pub(crate) fn run(args: &[&str]) -> Result<ExecStatus> {
+    if !makefile_exists() {
+        return Err(Error::Builtin("nonexistent makefile".into()));
+    }
+
     let make_prog = string_value("MAKE").unwrap_or_else(|| String::from("make"));
     let mut make = Command::new(make_prog);
     if let Ok(opts) = string_vec("MAKEOPTS") {
