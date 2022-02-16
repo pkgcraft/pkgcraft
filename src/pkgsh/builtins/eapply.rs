1fb4b92c296b936e66476af7ea7e7559bd801534
Tim Harder <radhermit@gmail.com>
2022-02-16 02:11:06 -0700

    eapply: add test for empty dir

--- src/pkgsh/builtins/eapply.rs
+++ src/pkgsh/builtins/eapply.rs
@@ -164,6 +164,13 @@ mod tests {
                 let r = eapply(&args);
                 assert_err_re!(r, format!("^nonexistent file: .*$"));
             }
+
+            // empty dir
+            let dir = tempdir().unwrap();
+            env::set_current_dir(&dir).unwrap();
+            fs::create_dir("files").unwrap();
+            let r = eapply(&["files"]);
+            assert_err_re!(r, format!("^no patches in directory: .*$"));
         }
 
         #[test]
