60a81c89764cb67714c75992056e99cb4c2af3f2
Tim Harder <radhermit@gmail.com>
2022-02-21 14:42:13 -0700

    pkgsh: add creation tests for `dodir`

--- src/pkgsh/builtins/dodir.rs
+++ src/pkgsh/builtins/dodir.rs
@@ -35,11 +35,82 @@ pub(super) static BUILTIN: Lazy<PkgBuiltin> = Lazy::new(|| {
 
 #[cfg(test)]
 mod tests {
-    use super::super::assert_invalid_args;
-    use super::run as dodir;
+    use std::os::unix::fs::MetadataExt;
+    use std::path::{Path, PathBuf};
+    use std::{env, fs};
 
-    #[test]
-    fn invalid_args() {
-        assert_invalid_args(dodir, &[0]);
+    use rusty_fork::rusty_fork_test;
+    use tempfile::tempdir;
+
+    use super::super::assert_invalid_args;
+    use super::super::diropts::run as diropts;
+    use super::run as dodir;
+    use crate::pkgsh::BUILD_DATA;
+
+    rusty_fork_test! {
+        #[test]
+        fn invalid_args() {
+            assert_invalid_args(dodir, &[0]);
+        }
+
+        #[test]
+        fn creation() {
+            BUILD_DATA.with(|d| {
+                let dir = tempdir().unwrap();
+                env::set_current_dir(&dir).unwrap();
+                let prefix = dir.path();
+                d.borrow_mut().env.insert("ED".into(), prefix.to_str().unwrap().into());
+
+                let default = 0o40755;
+
+                for dirs in [
+                        vec!["dir"],
+                        vec!["path/to/dir"],
+                        vec!["/etc"],
+                        vec!["/usr/bin"],
+                        vec!["dir", "/usr/bin"],
+                        ] {
+                    dodir(&dirs).unwrap();
+                    for dir in dirs {
+                        let path = Path::new(dir.strip_prefix("/").unwrap_or(dir));
+                        let path: PathBuf = [prefix, path].iter().collect();
+                        assert!(path.is_dir(), "failed creating dir: {dir:?}");
+                        let meta = fs::metadata(&path).unwrap();
+                        let mode = meta.mode();
+                        assert!(mode == default, "dir mode {mode:#o} is not default {default:#o}");
+                    }
+                }
+            })
+        }
+
+        #[test]
+        fn custom_diropts() {
+            BUILD_DATA.with(|d| {
+                let dir = tempdir().unwrap();
+                env::set_current_dir(&dir).unwrap();
+                let prefix = dir.path();
+                d.borrow_mut().env.insert("ED".into(), prefix.to_str().unwrap().into());
+
+                let default = 0o40755;
+                let custom = 0o40777;
+
+                for dir in ["dir", "/usr/bin"] {
+                    diropts(&["-m0755"]).unwrap();
+                    dodir(&[dir]).unwrap();
+                    let path = Path::new(dir.strip_prefix("/").unwrap_or(dir));
+                    let path: PathBuf = [prefix, path].iter().collect();
+                    let meta = fs::metadata(&path).unwrap();
+                    let mode = meta.mode();
+                    assert!(mode == default, "dir mode {mode:#o} is not default {default:#o}");
+
+                    // change mode for installed directories and re-run dodir()
+                    diropts(&["-m0777"]).unwrap();
+                    dodir(&[dir]).unwrap();
+                    let meta = fs::metadata(&path).unwrap();
+                    let mode = meta.mode();
+                    assert!(mode == custom, "dir mode {mode:#o} is not custom {custom:#o}");
+                }
+            })
+        }
     }
 }
