a41230817be9068271c254ceec7d6a3a4327e841
Tim Harder <radhermit@gmail.com>
2022-03-04 13:40:25 -0700

    pkgsh: rework dolib* and newlib* to use FileTree assertions

--- src/pkgsh/builtins/dolib_a.rs
+++ src/pkgsh/builtins/dolib_a.rs
@@ -30,63 +30,49 @@ pub(super) static BUILTIN: Lazy<PkgBuiltin> = Lazy::new(|| {
 
 #[cfg(test)]
 mod tests {
-    use std::os::unix::fs::MetadataExt;
-    use std::path::{Path, PathBuf};
-    use std::{env, fs};
+    use std::fs;
 
     use rusty_fork::rusty_fork_test;
-    use tempfile::tempdir;
 
     use super::super::assert_invalid_args;
     use super::super::libopts::run as libopts;
     use super::run as dolib_a;
     use crate::macros::assert_err_re;
-    use crate::pkgsh::BUILD_DATA;
+    use crate::pkgsh::test::FileTree;
 
     rusty_fork_test! {
         #[test]
         fn invalid_args() {
             assert_invalid_args(dolib_a, &[0]);
 
-            BUILD_DATA.with(|d| {
-                let dir = tempdir().unwrap();
-                env::set_current_dir(&dir).unwrap();
-                let prefix = dir.path();
-                d.borrow_mut().env.insert("ED".into(), prefix.to_str().unwrap().into());
+            let _file_tree = FileTree::new();
 
-                // nonexistent
-                let r = dolib_a(&["pkgcraft"]);
-                assert_err_re!(r, format!("^invalid file \"pkgcraft\": .*$"));
-            })
+            // nonexistent
+            let r = dolib_a(&["pkgcraft"]);
+            assert_err_re!(r, format!("^invalid file \"pkgcraft\": .*$"));
         }
 
         #[test]
         fn creation() {
-            BUILD_DATA.with(|d| {
-                let dir = tempdir().unwrap();
-                let prefix = dir.path();
-                let src_dir = prefix.join("src");
-                fs::create_dir(&src_dir).unwrap();
-                env::set_current_dir(&src_dir).unwrap();
-                d.borrow_mut().env.insert("ED".into(), prefix.to_str().unwrap().into());
+            let file_tree = FileTree::new();
+            let default_mode = 0o100644;
 
-                let default = 0o100644;
+            fs::File::create("pkgcraft.a").unwrap();
+            dolib_a(&["pkgcraft.a"]).unwrap();
+            file_tree.assert(format!(r#"
+                [[files]]
+                path = "/usr/lib/pkgcraft.a"
+                mode = {default_mode}
+            "#));
 
-                fs::File::create("pkgcraft.a").unwrap();
-                dolib_a(&["pkgcraft.a"]).unwrap();
-                let path = Path::new("usr/lib/pkgcraft.a");
-                let path: PathBuf = [prefix, path].iter().collect();
-                let meta = fs::metadata(&path).unwrap();
-                let mode = meta.mode();
-                assert!(mode == default, "mode {mode:#o} is not default {default:#o}");
-
-                // verify libopts are ignored
-                libopts(&["-m0755"]).unwrap();
-                dolib_a(&["pkgcraft.a"]).unwrap();
-                let meta = fs::metadata(&path).unwrap();
-                let mode = meta.mode();
-                assert!(mode == default, "mode {mode:#o} is not default {default:#o}");
-            })
+            // verify libopts are ignored
+            libopts(&["-m0755"]).unwrap();
+            dolib_a(&["pkgcraft.a"]).unwrap();
+            file_tree.assert(format!(r#"
+                [[files]]
+                path = "/usr/lib/pkgcraft.a"
+                mode = {default_mode}
+            "#));
         }
     }
 }
--- src/pkgsh/builtins/newlib_a.rs
+++ src/pkgsh/builtins/newlib_a.rs
@@ -27,15 +27,15 @@ pub(super) static BUILTIN: Lazy<PkgBuiltin> = Lazy::new(|| {
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
     use std::io::Write;
-    use std::{env, fs};
 
     use rusty_fork::rusty_fork_test;
-    use tempfile::tempdir;
 
     use super::super::assert_invalid_args;
     use super::run as newlib_a;
-    use crate::pkgsh::{write_stdin, BUILD_DATA};
+    use crate::pkgsh::test::FileTree;
+    use crate::pkgsh::write_stdin;
 
     rusty_fork_test! {
         #[test]
@@ -45,24 +45,23 @@ mod tests {
 
         #[test]
         fn creation() {
-            BUILD_DATA.with(|d| {
-                let dir = tempdir().unwrap();
-                let prefix = dir.path();
-                let src_dir = prefix.join("src");
-                fs::create_dir(&src_dir).unwrap();
-                env::set_current_dir(&src_dir).unwrap();
-                d.borrow_mut().env.insert("ED".into(), prefix.to_str().unwrap().into());
+            let file_tree = FileTree::new();
 
-                fs::File::create("lib").unwrap();
-                newlib_a(&["lib", "pkgcraft.a"]).unwrap();
-                let path = prefix.join("usr/lib/pkgcraft.a");
-                assert!(path.exists(), "missing file: {path:?}");
+            fs::File::create("lib").unwrap();
+            newlib_a(&["lib", "pkgcraft.a"]).unwrap();
+            file_tree.assert(r#"
+                [[files]]
+                path = "/usr/lib/pkgcraft.a"
+            "#);
 
-                // re-run using data from stdin
-                write_stdin!("pkgcraft");
-                newlib_a(&["-", "pkgcraft.a"]).unwrap();
-                assert_eq!(fs::read_to_string(&path).unwrap(), "pkgcraft");
-            })
+            // re-run using data from stdin
+            write_stdin!("pkgcraft");
+            newlib_a(&["-", "pkgcraft.a"]).unwrap();
+            file_tree.assert(r#"
+                [[files]]
+                path = "/usr/lib/pkgcraft.a"
+                data = "pkgcraft"
+            "#);
         }
     }
 }
