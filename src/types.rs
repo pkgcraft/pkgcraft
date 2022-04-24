c24fb623de211433194ab231b504b6d25ad9e4b8
Tim Harder <radhermit@gmail.com>
2022-04-23 22:48:48 -0600

    types: minor walkdir filter none type simplification

--- src/types.rs
+++ src/types.rs
@@ -3,4 +3,4 @@ use walkdir::DirEntry;
 // None value coerced to a directory filtering predicate function pointer for use with
 // Option-wrapped closure parameter generics.
 type WalkDirFilter = fn(&DirEntry) -> bool;
-pub(crate) const NO_WALKDIR_FILTER: Option<WalkDirFilter> = None::<WalkDirFilter>;
+pub(crate) const NO_WALKDIR_FILTER: Option<WalkDirFilter> = None;
