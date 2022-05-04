befdb47f8aa278db3f4871c5a21cf860799c3efb
Tim Harder <radhermit@gmail.com>
2022-05-03 19:20:38 -0600

    benches: update benchmarks for EAPI_LATEST type change

--- benches/required_use.rs
+++ benches/required_use.rs
@@ -7,7 +7,7 @@ use pkgcraft::eapi::EAPI_LATEST;
 pub fn bench_parse_required_use(c: &mut Criterion) {
     c.bench_function("required-use-conditional", |b| {
         b.iter(|| {
-            required_use::parse("u1? ( u2 )", EAPI_LATEST);
+            required_use::parse("u1? ( u2 )", &EAPI_LATEST);
         })
     });
 }
