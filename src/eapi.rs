095d3542cb1724f56b7ba6ded49911d3e5d97564
Tim Harder <radhermit@gmail.com>
2022-05-19 05:36:53 -0600

    eapi: allow get_eapi() to use anything with a string reference

--- src/eapi.rs
+++ src/eapi.rs
@@ -347,7 +347,8 @@ impl fmt::Display for Eapi {
 }
 
 /// Get a EAPI given its identifier.
-pub fn get_eapi(id: &str) -> Result<&'static Eapi> {
+pub fn get_eapi<S: AsRef<str>>(id: S) -> Result<&'static Eapi> {
+    let id = id.as_ref();
     match EAPIS.get(id) {
         Some(eapi) => Ok(eapi),
         None => match VALID_EAPI_RE.is_match(id) {
