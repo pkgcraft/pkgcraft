f3dc18c1b2157b2ecf0d648a01d62b1f3896c70a
Tim Harder <radhermit@gmail.com>
2022-07-30 08:40:46 -0600

    pkgsh::builtins: add simple EXPORT_FUNCTIONS test

--- src/pkgsh/builtins/export_functions.rs
+++ src/pkgsh/builtins/export_functions.rs
@@ -23,7 +23,8 @@ pub(crate) fn run(args: &[&str]) -> Result<ExecStatus> {
         None => return Err(Error::Builtin("no ECLASS defined".into())),
     };
 
-    let funcs: Vec<String> = args
+    // TODO: verify phase function existence?
+    let funcs: Vec<_> = args
         .iter()
         .map(|func| format!("{func}() {{ {eclass}_{func} \"$@\"; }}", func = func, eclass = eclass))
         .collect();
@@ -46,11 +47,56 @@ pub(super) static PKG_BUILTIN: Lazy<PkgBuiltin> =
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
+    use scallop::functions;
+    use scallop::variables::bind;
+
+    use crate::config::Config;
+    use crate::pkgsh::{source_ebuild, BUILD_DATA};
+
     use super::super::assert_invalid_args;
-    use super::run as inherit;
+    use super::run as export_functions;
 
     #[test]
     fn invalid_args() {
-        assert_invalid_args(inherit, &[0]);
+        assert_invalid_args(export_functions, &[0]);
+    }
+
+    #[test]
+    fn test_single() {
+        let mut config = Config::new("pkgcraft", "", false).unwrap();
+        let (t, repo) = config.temp_repo("test", 0).unwrap();
+        let temp_file = tempfile::NamedTempFile::new().unwrap();
+        bind("TEMP_FILE", temp_file.path().to_string_lossy(), None, None).unwrap();
+
+        // create eclass
+        let eclass = indoc::indoc! {r#"
+            # stub eclass
+            EXPORT_FUNCTIONS src_compile
+
+            e1_src_compile() {
+                echo compiling >> ${TEMP_FILE}
+            }
+        "#};
+        t.create_eclass("e1", eclass).unwrap();
+
+        let data = indoc::indoc! {r#"
+            inherit e1
+            DESCRIPTION="testing EXPORT_FUNCTIONS support"
+            SLOT=0
+        "#};
+        let path = t.create_ebuild_raw("cat/pkg-1", data).unwrap();
+        BUILD_DATA.with(|d| {
+            d.borrow_mut().repo = repo.clone();
+            source_ebuild(&path).unwrap();
+            // execute eclass-defined function
+            let mut func = functions::find("src_compile").unwrap();
+            func.execute(&[]).unwrap();
+            // verify the function was run
+            let output = fs::read_to_string(temp_file.path()).unwrap();
+            let output: Vec<_> = output.split_whitespace().collect();
+            assert_eq!(output, ["compiling"]);
+        });
     }
 }
