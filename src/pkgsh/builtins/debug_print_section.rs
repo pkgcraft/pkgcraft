2241643fcf3528ad504109f2b7d3e200a8bbef3c
Tim Harder <radhermit@gmail.com>
2022-08-07 09:10:28 -0600

    pkgsh::builtins: add initial implementation for `debug-print-section`

--- src/pkgsh/builtins/debug_print_section.rs
+++ src/pkgsh/builtins/debug_print_section.rs
@@ -1,15 +1,20 @@
 use scallop::builtins::ExecStatus;
-use scallop::Result;
+use scallop::{Error, Result};
 
+use super::debug_print::run as debug_print;
 use super::{make_builtin, ALL};
 
 const LONG_DOC: &str = "\
-Calls debug-print with now in section $*.";
+Calls debug-print with `now in section $*`.";
 
 #[doc = stringify!(LONG_DOC)]
-pub(crate) fn run(_args: &[&str]) -> Result<ExecStatus> {
-    // TODO: fill out this stub
-    Ok(ExecStatus::Success)
+pub(crate) fn run(args: &[&str]) -> Result<ExecStatus> {
+    if args.is_empty() {
+        return Err(Error::Base("requires 1 or more args, got 0".into()));
+    }
+
+    let args = &[&["now in section"], args].concat();
+    debug_print(args)
 }
 
 const USAGE: &str = "debug-print-section arg1 arg2";
@@ -24,10 +29,16 @@ make_builtin!(
 
 #[cfg(test)]
 mod tests {
-    use super::super::builtin_scope_tests;
+    use super::super::{assert_invalid_args, builtin_scope_tests};
+    use super::run as debug_print_section;
     use super::*;
 
     builtin_scope_tests!(USAGE);
+
+    #[test]
+    fn invalid_args() {
+        assert_invalid_args(debug_print_section, &[0]);
+    }
 
     // TODO: add usage tests
 }
