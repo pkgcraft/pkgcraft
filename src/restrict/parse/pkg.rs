7b0b94bb943a91008360ebe1032662c131ecbf99
Tim Harder <radhermit@gmail.com>
2022-09-04 02:18:30 -0600

    restrict::parse: add more attribute support for pkg restrictions

--- src/restrict/parse/pkg.rs
+++ src/restrict/parse/pkg.rs
@@ -13,46 +13,69 @@ peg::parser! {
         rule string_ops() -> &'input str
             = " "* op:$("==" / "!=" / "=~" / "!~") " "* { op }
 
-        rule category() -> Restrict
-            = ("category" / "CATEGORY") op:string_ops() s:quoted_string()
+        rule non_optional_str() -> Restrict
+            = attr:$(['a'..='z' | '_']+) op:string_ops() s:quoted_string()
             {?
+                let restrict_fn = match attr {
+                    "ebuild" => Ebuild,
+                    "category" => Category,
+                    "description" => Description,
+                    "slot" => Slot,
+                    "subslot" => Subslot,
+                    _ => return Err("unknown package attribute"),
+                };
+
                 let r: Restrict = match op {
-                    "==" => Category(Str::matches(s)).into(),
-                    "!=" => Restrict::not(Category(Str::matches(s))),
+                    "==" => restrict_fn(Str::matches(s)).into(),
+                    "!=" => Restrict::not(restrict_fn(Str::matches(s))),
                     "=~" => match Regex::new(s) {
-                        Ok(r) => Category(Str::Regex(r)).into(),
+                        Ok(r) => restrict_fn(Str::Regex(r)).into(),
                         Err(_) => return Err("invalid regex"),
                     },
                     "!~" => match Regex::new(s) {
-                        Ok(r) => Restrict::not(Category(Str::Regex(r))),
+                        Ok(r) => Restrict::not(restrict_fn(Str::Regex(r))),
                         Err(_) => return Err("invalid regex"),
                     },
                     _ => return Err("invalid string operator"),
                 };
+
                 Ok(r)
             }
 
-        rule description() -> Restrict
-            = ("description" / "DESCRIPTION") op:string_ops() s:quoted_string()
-            {?
+        rule optional_str() -> Restrict
+            = attr:$(['a'..='z' | '_']+) " is " ("None" / "none") {?
+                let restrict_fn = match attr {
+                    "raw_subslot" => RawSubslot,
+                    "long_description" => LongDescription,
+                    _ => return Err("unknown optional package attribute"),
+                };
+                Ok(restrict_fn(None).into())
+            } / attr:$(['a'..='z' | '_']+) op:string_ops() s:quoted_string() {?
+                let restrict_fn = match attr {
+                    "raw_subslot" => RawSubslot,
+                    "long_description" => LongDescription,
+                    _ => return Err("unknown optional package attribute"),
+                };
+
                 let r: Restrict = match op {
-                    "==" => Description(Str::matches(s)).into(),
-                    "!=" => Restrict::not(Description(Str::matches(s))),
+                    "==" => restrict_fn(Some(Str::matches(s))).into(),
+                    "!=" => Restrict::not(restrict_fn(Some(Str::matches(s)))),
                     "=~" => match Regex::new(s) {
-                        Ok(r) => Description(Str::Regex(r)).into(),
+                        Ok(r) => restrict_fn(Some(Str::Regex(r))).into(),
                         Err(_) => return Err("invalid regex"),
                     },
                     "!~" => match Regex::new(s) {
-                        Ok(r) => Restrict::not(Description(Str::Regex(r))),
+                        Ok(r) => Restrict::not(restrict_fn(Some(Str::Regex(r)))),
                         Err(_) => return Err("invalid regex"),
                     },
                     _ => return Err("invalid string operator"),
                 };
+
                 Ok(r)
             }
 
         pub rule expr() -> Restrict
-            = " "* invert:"!"? r:(category() / description()) " "* {
+            = " "* invert:"!"? r:(non_optional_str() / optional_str()) " "* {
                 let mut restrict = r;
                 if invert.is_some() {
                     restrict = Restrict::not(restrict);
