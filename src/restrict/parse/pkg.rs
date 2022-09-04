b025e1cc360e3e3be1416076240ea475dc879ccd
Tim Harder <radhermit@gmail.com>
2022-09-04 03:40:08 -0600

    restrict::parse: add initial pkg maintainer query support

--- src/restrict/parse/pkg.rs
+++ src/restrict/parse/pkg.rs
@@ -6,6 +6,24 @@ use crate::restrict::{Restrict, Str};
 
 peg::parser! {
     grammar restrict() for str {
+        rule attr_optional() -> Restrict
+            = attr:$(['a'..='z' | '_']+) " is " ("None" / "none") {?
+                let r = match attr {
+                    "raw_subslot" => RawSubslot(None).into(),
+                    "homepage" => Homepage(None).into(),
+                    "defined_phases" => DefinedPhases(None).into(),
+                    "keywords" => Keywords(None).into(),
+                    "iuse" => Iuse(None).into(),
+                    "inherit" => Inherit(None).into(),
+                    "inherited" => Inherited(None).into(),
+                    "long_description" => LongDescription(None).into(),
+                    "maintainers" => Maintainers(None).into(),
+                    "upstreams" => Upstreams(None).into(),
+                    _ => return Err("unknown optional package attribute"),
+                };
+                Ok(r)
+            }
+
         rule quoted_string() -> &'input str
             = "\"" s:$([^ '\"']+) "\"" { s }
             / "\'" s:$([^ '\'']+) "\'" { s }
@@ -42,24 +60,6 @@ peg::parser! {
                 Ok(r)
             }
 
-        rule attr_optional() -> Restrict
-            = attr:$(['a'..='z' | '_']+) " is " ("None" / "none") {?
-                let r = match attr {
-                    "raw_subslot" => RawSubslot(None).into(),
-                    "homepage" => Homepage(None).into(),
-                    "defined_phases" => DefinedPhases(None).into(),
-                    "keywords" => Keywords(None).into(),
-                    "iuse" => Iuse(None).into(),
-                    "inherit" => Inherit(None).into(),
-                    "inherited" => Inherited(None).into(),
-                    "long_description" => LongDescription(None).into(),
-                    "maintainers" => Maintainers(None).into(),
-                    "upstreams" => Upstreams(None).into(),
-                    _ => return Err("unknown optional package attribute"),
-                };
-                Ok(r)
-            }
-
         rule str_restrict_optional() -> Restrict
             = attr:$(['a'..='z' | '_']+) op:string_ops() s:quoted_string() {?
                 let restrict_fn = match attr {
@@ -85,8 +85,64 @@ peg::parser! {
                 Ok(r)
             }
 
+        rule maintainers() -> Restrict
+            = "maintainer"
+                    r:(maintainers_attr_optional()
+                       / maintainers_contains()
+                    ) " "* { r }
+
+        rule maintainers_attr_optional() -> Restrict
+            = attr:$(['a'..='z' | '_']+) " is " ("None" / "none") {?
+                use crate::metadata::ebuild::MaintainerRestrict::*;
+                use crate::metadata::ebuild::SliceMaintainers::Contains;
+                let r = match attr {
+                    "name" => Contains(Name(None)),
+                    "description" => Contains(Description(None)),
+                    "type" => Contains(Type(None)),
+                    "proxied" => Contains(Proxied(None)),
+                    _ => return Err("unknown optional maintainer attribute"),
+                };
+                Ok(r.into())
+            }
+
+        rule maintainers_contains() -> Restrict
+            = "contains" " " attr:$(['a'..='z' | '_']+) op:string_ops() s:quoted_string() {?
+                use crate::metadata::ebuild::MaintainerRestrict::{self, *};
+                use crate::metadata::ebuild::SliceMaintainers::{self, Contains};
+
+                let restrict_fn = match attr {
+                    "email" => |r: Str| -> SliceMaintainers { Contains(Email(r)) },
+                    "name" => |r: Str| -> SliceMaintainers { Contains(Name(Some(r))) },
+                    "description" => |r: Str| -> SliceMaintainers { Contains(Description(Some(r))) },
+                    "type" => |r: Str| -> SliceMaintainers { Contains(Type(Some(r))) },
+                    "proxied" => |r: Str| -> SliceMaintainers { Contains(Proxied(Some(r))) },
+                    _ => return Err("unknown maintainer attribute"),
+                };
+
+                let r: Restrict = match op {
+                    "==" => restrict_fn(Str::matches(s)).into(),
+                    "!=" => Restrict::not(restrict_fn(Str::matches(s))),
+                    "=~" => match Regex::new(s) {
+                        Ok(r) => restrict_fn(Str::Regex(r)).into(),
+                        Err(_) => return Err("invalid regex"),
+                    },
+                    "!~" => match Regex::new(s) {
+                        Ok(r) => Restrict::not(restrict_fn(Str::Regex(r))),
+                        Err(_) => return Err("invalid regex"),
+                    },
+                    _ => return Err("invalid string operator"),
+                };
+
+                Ok(r)
+            }
+
         rule expr() -> Restrict
-            = " "* invert:"!"? r:(attr_optional() / str_restrict() / str_restrict_optional()) " "* {
+            = " "* invert:"!"?
+                    r:(attr_optional()
+                       / str_restrict()
+                       / str_restrict_optional()
+                       / maintainers()
+                    ) " "* {
                 let mut restrict = r;
                 if invert.is_some() {
                     restrict = Restrict::not(restrict);
