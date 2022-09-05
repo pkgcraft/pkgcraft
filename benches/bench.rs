97873a166494aaff0d9497dc39da2a25d95171c6
Tim Harder <radhermit@gmail.com>
2022-09-04 21:10:57 -0600

    benches: add initial repo benchmarks
    For simple iteration tests matching the ones used for the python
    bindings.

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,11 +1,13 @@
 use criterion::*;
 
 mod atom;
+mod repo;
 mod required_use;
 mod version;
 
 criterion_group!(atom, atom::bench_pkg_atoms);
+criterion_group!(repo, repo::bench_repo_ebuild);
 criterion_group!(required_use, required_use::bench_parse_required_use);
 criterion_group!(version, version::bench_pkg_versions);
 
-criterion_main!(atom, required_use, version);
+criterion_main!(atom, repo, required_use, version);
---
+++ benches/repo.rs
@@ -0,0 +1,37 @@
+use criterion::Criterion;
+
+use pkgcraft::config::Config;
+use pkgcraft::{atom, restrict::Restrict};
+
+#[allow(unused_must_use)]
+pub fn bench_repo_ebuild(c: &mut Criterion) {
+    let mut config = Config::new("pkgcraft", "", false).unwrap();
+    let (t, repo) = config.temp_repo("test", 0).unwrap();
+    for i in 0..100 {
+        t.create_ebuild(&format!("cat/pkg-{i}"), []).unwrap();
+    }
+    let repo = repo.as_ref();
+
+    c.bench_function("repo-ebuild-iter", |b| {
+        let mut pkgs = 0;
+        b.iter(|| {
+            pkgs = 0;
+            for _ in repo {
+                pkgs += 1;
+            }
+        });
+        assert_eq!(pkgs, 100);
+    });
+
+    c.bench_function("repo-ebuild-iter-restrict", |b| {
+        let mut pkgs = 0;
+        let r: Restrict = atom::cpv("cat/pkg-50").unwrap().into();
+        b.iter(|| {
+            pkgs = 0;
+            for _ in repo.iter_restrict(r.clone()) {
+                pkgs += 1;
+            }
+        });
+        assert_eq!(pkgs, 1);
+    });
+}
