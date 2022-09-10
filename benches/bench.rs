7a906ff1f77e15108c7fe3d131993ead07a79a01
Tim Harder <radhermit@gmail.com>
2022-09-10 00:27:27 -0600

    benches: fix required_use parsing benchmark for depspec updates

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,13 +1,13 @@
 use criterion::*;
 
 mod atom;
+mod depspec;
 mod repo;
-mod required_use;
 mod version;
 
 criterion_group!(atom, atom::bench_pkg_atoms);
 criterion_group!(repo, repo::bench_repo_ebuild);
-criterion_group!(required_use, required_use::bench_parse_required_use);
+criterion_group!(depspec, depspec::bench_parse_required_use);
 criterion_group!(version, version::bench_pkg_versions);
 
-criterion_main!(atom, repo, required_use, version);
+criterion_main!(atom, repo, depspec, version);
