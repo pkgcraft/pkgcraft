c101950b0868c7367f208af4baa2aca650c31ec8
Tim Harder <radhermit@gmail.com>
2022-11-12 03:34:41 -0700

    benches: add pkgdep-related benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,7 +7,7 @@ mod version;
 
 criterion_group!(atom, atom::bench_pkg_atoms);
 criterion_group!(repo, repo::bench_repo_ebuild);
-criterion_group!(depset, depset::bench_parse_required_use);
+criterion_group!(depset, depset::bench_depsets);
 criterion_group!(version, version::bench_pkg_versions);
 
 criterion_main!(atom, repo, depset, version);
--- benches/depset.rs
+++ benches/depset.rs
@@ -1,12 +1,29 @@
+use std::str::FromStr;
+
 use criterion::Criterion;
 
+use pkgcraft::atom::Atom;
 use pkgcraft::depset::parse;
 use pkgcraft::eapi::EAPI_LATEST;
+use pkgcraft::restrict::{Restrict, Restriction};
 
-pub fn bench_parse_required_use(c: &mut Criterion) {
-    c.bench_function("required-use-conditional", |b| {
+pub fn bench_depsets(c: &mut Criterion) {
+    c.bench_function("depset-parse-required-use", |b| {
         b.iter(|| {
             parse::required_use("u1? ( u2 )", &EAPI_LATEST).unwrap();
         })
+    });
+
+    let deps = "c/p1 u1? ( c/p2 !u2? ( c/p3 ) ) || ( c/p4 c/p5 )";
+    c.bench_function("depset-parse-pkgdep", |b| {
+        b.iter(|| {
+            parse::pkgdep(deps, &EAPI_LATEST).unwrap();
+        })
+    });
+
+    c.bench_function("depset-restrict-pkgdep", |b| {
+        let r: Restrict = Atom::from_str("c/p5").unwrap().into();
+        let depset = parse::pkgdep(deps, &EAPI_LATEST).unwrap().unwrap();
+        b.iter(|| assert!(r.matches(&depset)));
     });
 }
