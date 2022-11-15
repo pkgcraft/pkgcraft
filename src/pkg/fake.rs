39c4f17a60b3fc43a656d18a5bc01963a6ece87b
Tim Harder <radhermit@gmail.com>
2022-11-15 09:43:47 -0700

    pkg::fake: match atom repo restricts against the proper pkg attribute

--- src/pkg/fake.rs
+++ src/pkg/fake.rs
@@ -37,6 +37,7 @@ impl Restriction<&Pkg<'_>> for restrict::Restrict {
     fn matches(&self, pkg: &Pkg) -> bool {
         restrict::restrict_match! {
             self, pkg,
+            Self::Atom(atom::Restrict::Repo(Some(r))) => r.matches(pkg.repo().id()),
             Self::Atom(r) => r.matches(pkg.atom()),
             Self::Pkg(pkg::Restrict::Eapi(r)) => r.matches(pkg.eapi().as_str()),
             Self::Pkg(pkg::Restrict::Repo(r)) => r.matches(pkg.repo().id())
