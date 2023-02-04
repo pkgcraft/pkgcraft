86f0e21dab7f127854b26c3374b9e67510c0ec05
Tim Harder <radhermit@gmail.com>
2023-02-04 07:14:23 -0700

    pkgcraft: simplify intersect args for `pk`

--- crates/pkgcraft/src/bin/pk/atom/intersect.rs
+++ crates/pkgcraft/src/bin/pk/atom/intersect.rs
@@ -1,26 +1,21 @@
 use std::process::ExitCode;
 use std::str::FromStr;
 
-use anyhow::anyhow;
 use clap::Args;
-use itertools::Itertools;
 use pkgcraft::atom::Atom;
 
 use crate::Run;
 
 #[derive(Debug, Args)]
 pub(crate) struct Intersect {
-    vals: Vec<String>,
+    atom1: String,
+    atom2: String,
 }
 
 impl Run for Intersect {
     fn run(&self) -> anyhow::Result<ExitCode> {
-        let (s1, s2) = self
-            .vals
-            .iter()
-            .collect_tuple()
-            .ok_or_else(|| anyhow!("invalid intersects args: {:?}", self.vals))?;
-        let (a1, a2) = (Atom::from_str(s1)?, Atom::from_str(s2)?);
+        let a1 = Atom::from_str(&self.atom1)?;
+        let a2 = Atom::from_str(&self.atom2)?;
         Ok(ExitCode::from(!a1.intersects(&a2) as u8))
     }
 }
--- crates/pkgcraft/src/bin/pk/version/intersect.rs
+++ crates/pkgcraft/src/bin/pk/version/intersect.rs
@@ -1,26 +1,21 @@
 use std::process::ExitCode;
 use std::str::FromStr;
 
-use anyhow::anyhow;
 use clap::Args;
-use itertools::Itertools;
 use pkgcraft::atom::Version;
 
 use crate::Run;
 
 #[derive(Debug, Args)]
 pub(crate) struct Intersect {
-    vals: Vec<String>,
+    version1: String,
+    version2: String,
 }
 
 impl Run for Intersect {
     fn run(&self) -> anyhow::Result<ExitCode> {
-        let (s1, s2) = self
-            .vals
-            .iter()
-            .collect_tuple()
-            .ok_or_else(|| anyhow!("invalid intersects args: {:?}", self.vals))?;
-        let (v1, v2) = (Version::from_str(s1)?, Version::from_str(s2)?);
+        let v1 = Version::from_str(&self.version1)?;
+        let v2 = Version::from_str(&self.version2)?;
         Ok(ExitCode::from(!v1.intersects(&v2) as u8))
     }
 }
