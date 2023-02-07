0d6f132ee95084b1fcf34e9433dac5798a761e16
Tim Harder <radhermit@gmail.com>
2023-02-06 21:06:45 -0700

    pkgcraft-c: export initial support for ebuild repo metadata

--- crates/pkgcraft-c/src/repo/ebuild.rs
+++ crates/pkgcraft-c/src/repo/ebuild.rs
@@ -1,9 +1,40 @@
+use std::ffi::c_char;
 use std::sync::Arc;
 
 use pkgcraft::eapi::Eapi;
-use pkgcraft::repo::{ebuild::Repo as EbuildRepo, Repo};
+use pkgcraft::repo::ebuild::Repo as EbuildRepo;
+use pkgcraft::repo::Repo;
 
 use crate::macros::*;
+use crate::utils::str_to_raw;
+
+/// Return an ebuild repo's metadata arches.
+///
+/// # Safety
+/// The argument must be a non-null Repo pointer.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_repo_ebuild_arches(
+    r: *mut Repo,
+    len: *mut usize,
+) -> *mut *mut c_char {
+    let repo = null_ptr_check!(r.as_ref());
+    let repo = repo.as_ebuild().expect("invalid repo type: {repo:?}");
+    iter_to_array!(repo.metadata().arches().iter(), len, str_to_raw)
+}
+
+/// Return an ebuild repo's metadata categories.
+///
+/// # Safety
+/// The argument must be a non-null Repo pointer.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_repo_ebuild_categories(
+    r: *mut Repo,
+    len: *mut usize,
+) -> *mut *mut c_char {
+    let repo = null_ptr_check!(r.as_ref());
+    let repo = repo.as_ebuild().expect("invalid repo type: {repo:?}");
+    iter_to_array!(repo.metadata().categories().iter(), len, str_to_raw)
+}
 
 /// Return an ebuild repo's EAPI.
 ///
