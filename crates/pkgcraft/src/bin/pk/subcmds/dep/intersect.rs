8874345aad71b11af6e78545f762ec3e7096f392
Tim Harder <radhermit@gmail.com>
2023-02-17 18:05:12 -0700

    pk dep intersect: allow cpv fallback from regular deps

--- crates/pkgcraft/src/bin/pk/subcmds/dep/intersect.rs
+++ crates/pkgcraft/src/bin/pk/subcmds/dep/intersect.rs
@@ -14,8 +14,8 @@ pub struct Intersect {
 
 impl Run for Intersect {
     fn run(self) -> anyhow::Result<ExitCode> {
-        let d1 = Dep::from_str(&self.dep1)?;
-        let d2 = Dep::from_str(&self.dep2)?;
+        let d1 = Dep::from_str(&self.dep1).or_else(|_| Dep::new_cpv(&self.dep1))?;
+        let d2 = Dep::from_str(&self.dep2).or_else(|_| Dep::new_cpv(&self.dep2))?;
         Ok(ExitCode::from(!d1.intersects(&d2) as u8))
     }
 }
