edb7a55bf6a027e22611c8c217afd52fab47e42a
Tim Harder <radhermit@gmail.com>
2023-02-18 04:59:21 -0700

    pk: add more `pk dep intersect` tests

--- crates/pkgcraft/tests/pk/dep/intersect.rs
+++ crates/pkgcraft/tests/pk/dep/intersect.rs
@@ -4,26 +4,35 @@ use predicates::prelude::*;
 #[test]
 fn args() {
     // intersect
-    cmd("pk dep intersect")
-        .args(["a/a", "a/a"])
-        .assert()
-        .success();
-    cmd("pk dep intersect")
-        .args(["cat/pkg[u]", "cat/pkg[u,u]"])
-        .assert()
-        .success();
+    for args in [
+        ["a/a", "a/a"],
+        ["cat/pkg[u]", "cat/pkg[u,u]"],
+        ["cat/pkg-1", "~cat/pkg-1"],
+        [">=cat/pkg-1", "cat/pkg-9999"],
+    ] {
+        cmd("pk dep intersect").args(args).assert().success();
+    }
 
     // non-intersect
-    cmd("pk dep intersect")
-        .args(["a/a", "a/b"])
-        .assert()
-        .code(1);
+    for args in [
+        ["a/a", "a/b"],
+        ["=cat/pkg-1.1*", "cat/pkg-1"],
+        ["cat/pkg-2", "<cat/pkg-2"],
+        ["cat/pkg[-a]", "cat/pkg[a]"],
+    ] {
+        cmd("pk dep intersect")
+            .args(args)
+            .assert()
+            .failure()
+            .code(1);
+    }
 
     // errors return exit code 2 and output message to stderr
     for args in [vec!["a/b"], vec!["a/b", "a/b/c"]] {
         cmd("pk dep intersect")
             .args(&args)
             .assert()
+            .failure()
             .code(2)
             .stderr(predicate::str::is_empty().not());
     }
@@ -33,11 +42,18 @@ fn args() {
 fn toml() {
     let data = DepToml::load().unwrap();
     for d in data.intersects {
-        let status = cmd("pk dep intersect").args(&d.vals).assert();
+        let (s1, s2) = (d.vals[0].as_str(), d.vals[1].as_str());
+
+        // elements intersect themselves
+        cmd("pk dep intersect").args([s1, s1]).assert();
+        cmd("pk dep intersect").args([s2, s2]).assert();
+
+        // intersects depending on status
+        let status = cmd("pk dep intersect").args([s1, s2]).assert();
         if d.status {
             status.success();
         } else {
-            status.failure();
+            status.failure().code(1);
         }
     }
 }
