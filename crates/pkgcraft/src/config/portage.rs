dcc26e06162317c7e280ab5c1f15122ce218e960
Tim Harder <radhermit@gmail.com>
2023-05-11 07:29:18 -0600

    pkgcraft: rework repos.conf loading to be more combinatorial

--- crates/pkgcraft/src/config/portage.rs
+++ crates/pkgcraft/src/config/portage.rs
@@ -4,43 +4,49 @@ use ini::Ini;
 use crate::repo::Repo;
 use crate::Error;
 
-/// Load repos.conf settings from a given directory or file.
+/// Load repos from a given repos.conf path.
 pub(super) fn load_repos_conf<P: AsRef<Utf8Path>>(path: P) -> crate::Result<Vec<Repo>> {
     let path = path.as_ref();
-    let files: Vec<_> = match path.read_dir_utf8() {
-        Ok(entries) => Ok(entries
+
+    // expand directory path into files
+    let files = match path.read_dir_utf8() {
+        Ok(entries) => entries
             .filter_map(|d| d.ok())
             .map(|d| d.path().to_path_buf())
-            .collect()),
+            .collect(),
         // TODO: switch to `e.kind() == ErrorKind::NotADirectory` on rust stabilization
         // https://github.com/rust-lang/rust/issues/86442
-        Err(e) if e.raw_os_error() == Some(20) => Ok(vec![path.to_path_buf()]),
-        Err(e) => Err(Error::Config(format!("failed reading repos.conf: {path}: {e}"))),
-    }?;
+        Err(e) if e.raw_os_error() == Some(20) => vec![path.to_path_buf()],
+        Err(e) => return Err(Error::Config(format!("failed reading repos.conf: {path}: {e}"))),
+    };
 
-    let mut repos = vec![];
+    // load all repos from a given file
+    let repos_from_file = |f: &Utf8Path| -> crate::Result<Vec<Repo>> {
+        let ini = Ini::load_from_file(f)
+            .map_err(|e| Error::Config(format!("invalid repos.conf file: {f:?}: {e}")))?;
 
-    for f in files {
-        Ini::load_from_file(&f)
-            .map_err(|e| Error::Config(format!("invalid repos.conf file: {f:?}: {e}")))
-            .and_then(|ini| {
-                for (name, settings) in ini.iter().filter_map(|(section, p)| match section {
-                    Some(s) if s != "DEFAULT" => Some((s, p)),
-                    _ => None,
-                }) {
-                    // pull supported fields from config
-                    let priority = settings.get("priority").unwrap_or("0").parse().unwrap_or(0);
-                    let path = settings.get("location").ok_or_else(|| {
-                        Error::Config(format!(
-                            "invalid repos.conf file: {f:?}: missing location field: {name}"
-                        ))
-                    })?;
-
-                    repos.push(Repo::from_path(name, priority, path, false)?);
+        ini.iter()
+            .filter_map(|(section, p)| match section {
+                Some(s) if s != "DEFAULT" => Some((s, p)),
+                _ => None,
+            })
+            .map(|(name, settings)| {
+                // pull supported fields from config
+                let priority = settings.get("priority").unwrap_or("0").parse().unwrap_or(0);
+                if let Some(path) = settings.get("location") {
+                    Repo::from_path(name, priority, path, false)
+                } else {
+                    Err(Error::Config(format!(
+                        "invalid repos.conf file: {f:?}: missing location field: {name}"
+                    )))
                 }
-                Ok(())
-            })?;
-    }
+            })
+            .collect()
+    };
 
-    Ok(repos)
+    files
+        .iter()
+        .map(|f| repos_from_file(f))
+        .collect::<crate::Result<Vec<Vec<_>>>>()
+        .map(|repos| repos.into_iter().flatten().collect())
 }
