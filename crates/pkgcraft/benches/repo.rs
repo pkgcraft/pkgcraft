6d2987fb288e4cae9cd339fbbaaeb3502201c40d
Tim Harder <radhermit@gmail.com>
2023-05-29 06:12:55 -0600

    pkgcraft: fix temporary ebuild repo creation for repo benches

--- crates/pkgcraft/benches/repo.rs
+++ crates/pkgcraft/benches/repo.rs
@@ -6,11 +6,11 @@ use pkgcraft::repo::PkgRepository;
 
 pub fn bench_repo_ebuild(c: &mut Criterion) {
     let mut config = Config::new("pkgcraft", "");
-    let (t, repo) = config.temp_repo("test", 0, None).unwrap();
+    let t = config.temp_repo("test", 0, None).unwrap();
     for i in 0..100 {
         t.create_ebuild(&format!("cat/pkg-{i}"), &[]).unwrap();
     }
-    let repo = repo.as_ref();
+    let repo = t.repo();
 
     c.bench_function("repo-ebuild-iter", |b| {
         let mut pkgs = 0;
