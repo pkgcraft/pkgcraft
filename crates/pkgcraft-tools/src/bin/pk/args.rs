623197e7e0a44b33f1becc90f7c14b1e5d85ed6d
Tim Harder <radhermit@gmail.com>
2023-06-09 13:23:33 -0600

    pk: specifically allow -j0 which will hang pool-based processes using semaphores
    But should be closer to the expected outcome than allowing the process
    to run with a single job.

--- crates/pkgcraft-tools/src/bin/pk/args.rs
+++ crates/pkgcraft-tools/src/bin/pk/args.rs
@@ -1,8 +1,8 @@
-/// Apply bounds to use sane limits for parallel jobs.
+/// Limit parallel jobs to the number of logical CPUs on a system. Note that this allows -j0 which
+/// will hang pools using semaphores for access control.
 pub(crate) fn bounded_jobs(jobs: Option<usize>) -> usize {
     let cpus = num_cpus::get();
     match jobs {
-        Some(j) if j == 0 => 1,
         Some(j) if j <= cpus => j,
         _ => cpus,
     }
