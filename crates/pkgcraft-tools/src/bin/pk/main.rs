57b9341c105942b8d48d4185eecf9f9a7110b637
Tim Harder <radhermit@gmail.com>
2023-06-13 19:19:59 -0600

    pk: use an exit status of 141 for SIGPIPE failures
    To match the standard exit status used for signals: 128 + signal #

--- crates/pkgcraft-tools/src/bin/pk/main.rs
+++ crates/pkgcraft-tools/src/bin/pk/main.rs
@@ -61,7 +61,7 @@ fn main() -> anyhow::Result<ExitCode> {
     args.subcmd
         .run(&config)
         .or_else(|err| match err.root_cause().downcast_ref::<io::Error>() {
-            Some(e) if e.kind() == BrokenPipe => Ok(ExitCode::SUCCESS),
+            Some(e) if e.kind() == BrokenPipe => Ok(ExitCode::from(141)),
             _ => {
                 writeln!(io::stderr(), "pk: error: {err}").ok();
                 Ok(ExitCode::from(2))
