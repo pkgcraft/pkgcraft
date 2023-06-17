a8e37b3647fad21e5499e1f868d46f6580433c79
Tim Harder <radhermit@gmail.com>
2023-06-17 09:55:40 -0600

    pk repo eapis: add initial command tests

--- crates/pkgcraft-tools/tests/pk/repo.rs
+++ crates/pkgcraft-tools/tests/pk/repo.rs
@@ -1,1 +1,2 @@
+mod eapis;
 mod leaf;
---
+++ crates/pkgcraft-tools/tests/pk/repo/eapis.rs
@@ -0,0 +1,56 @@
+use pkgcraft::repo::ebuild_temp::Repo as TempRepo;
+use pkgcraft::test::cmd;
+use predicates::prelude::*;
+
+#[test]
+fn missing_repo_arg() {
+    cmd(&format!("pk repo eapis"))
+        .assert()
+        .stdout("")
+        .stderr(predicate::str::is_empty().not())
+        .failure()
+        .code(2);
+}
+
+#[test]
+fn nonexistent_repo() {
+    cmd(&format!("pk repo eapis path/to/nonexistent/repo"))
+        .assert()
+        .stdout("")
+        .stderr(predicate::str::is_empty().not())
+        .failure()
+        .code(2);
+}
+
+#[test]
+fn no_pkgs() {
+    let t = TempRepo::new("test", None, 0, None).unwrap();
+    cmd(&format!("pk repo eapis {}", t.path()))
+        .assert()
+        .stdout("")
+        .stderr("")
+        .success();
+}
+
+#[test]
+fn single() {
+    let t = TempRepo::new("test", None, 0, None).unwrap();
+    t.create_ebuild("cat/dep-1", &["EAPI=1"]).unwrap();
+    cmd(&format!("pk repo eapis {}", t.path()))
+        .assert()
+        .stdout(predicate::str::is_empty().not())
+        .stderr("")
+        .success();
+}
+
+#[test]
+fn multiple() {
+    let t = TempRepo::new("test", None, 0, None).unwrap();
+    t.create_ebuild("cat/a-1", &["EAPI=7"]).unwrap();
+    t.create_ebuild("cat/b-1", &["EAPI=8"]).unwrap();
+    cmd(&format!("pk repo eapis {}", t.path()))
+        .assert()
+        .stdout(predicate::str::is_empty().not())
+        .stderr("")
+        .success();
+}
