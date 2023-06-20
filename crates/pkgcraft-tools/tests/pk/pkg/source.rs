c0c2b5a49f91335aed942651ea1e850c7c5b3ebc
Tim Harder <radhermit@gmail.com>
2023-06-19 20:11:25 -0600

    pk pkg source: add path target tests

--- crates/pkgcraft-tools/tests/pk/pkg/source.rs
+++ crates/pkgcraft-tools/tests/pk/pkg/source.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use pkgcraft::repo::ebuild_temp::Repo as TempRepo;
 use pkgcraft::test::cmd;
 use predicates::prelude::*;
@@ -34,11 +36,11 @@ fn no_pkgs() {
 }
 
 #[test]
-fn single() {
+fn pkg_target_from_stdin() {
     let t = TempRepo::new("test", None, 0, None).unwrap();
     t.create_ebuild("cat/dep-1", &[]).unwrap();
-    cmd("pk pkg source")
-        .arg(t.path())
+    cmd(format!("pk pkg source -r {} -", t.path()))
+        .write_stdin("cat/dep")
         .assert()
         .stdout(predicate::str::is_empty().not())
         .stderr("")
@@ -46,11 +48,40 @@ fn single() {
 }
 
 #[test]
-fn single_from_stdin() {
+fn path_targets() {
     let t = TempRepo::new("test", None, 0, None).unwrap();
-    t.create_ebuild("cat/dep-1", &[]).unwrap();
-    cmd(format!("pk pkg source -r {} -", t.path()))
-        .write_stdin("cat/dep")
+    t.create_ebuild("cat1/a-1", &[]).unwrap();
+    t.create_ebuild("cat1/b-1", &[]).unwrap();
+    t.create_ebuild("cat2/c-1", &[]).unwrap();
+    t.create_ebuild("cat2/c-2", &[]).unwrap();
+
+    // repo path
+    cmd("pk pkg source")
+        .arg(t.path())
+        .assert()
+        .stdout(predicate::str::is_empty().not())
+        .stderr("")
+        .success();
+
+    // category path
+    cmd("pk pkg source")
+        .arg(t.path().join("cat1"))
+        .assert()
+        .stdout(predicate::str::is_empty().not())
+        .stderr("")
+        .success();
+
+    // package path
+    cmd("pk pkg source")
+        .arg(t.path().join("cat2/c"))
+        .assert()
+        .stdout(predicate::str::is_empty().not())
+        .stderr("")
+        .success();
+
+    // default current working dir
+    env::set_current_dir(t.path().join("cat2/c")).unwrap();
+    cmd("pk pkg source")
         .assert()
         .stdout(predicate::str::is_empty().not())
         .stderr("")
