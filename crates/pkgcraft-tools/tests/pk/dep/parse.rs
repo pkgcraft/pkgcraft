e23080623531c8b50acc41f7082acf74abeabe65
Tim Harder <radhermit@gmail.com>
2023-07-01 03:11:03 -0600

    pk dep parse: add -f/--format tests

--- crates/pkgcraft-tools/tests/pk/dep/parse.rs
+++ crates/pkgcraft-tools/tests/pk/dep/parse.rs
@@ -2,6 +2,8 @@ use itertools::Itertools;
 use pkgcraft::test::{cmd, TEST_DATA};
 use predicates::prelude::*;
 
+use crate::predicates::lines_contain;
+
 #[test]
 fn valid() {
     let deps = TEST_DATA.dep_toml.valid.iter().map(|e| &e.dep).join("\n");
@@ -38,4 +40,42 @@ fn eapi() {
         .stdout("")
         .stderr("")
         .success();
+}
+
+#[test]
+fn format() {
+    for opt in ["-f", "--format"] {
+        for (args, expected) in [
+            (["{BLOCKER}", "!cat/pkg"], "!"),
+            (["{BLOCKER}", "!!cat/pkg"], "!!"),
+            (["{BLOCKER}", "cat/pkg"], "<unset>"),
+            (["{CATEGORY}", "cat/pkg"], "cat"),
+            (["{P}", "=cat/pkg-1-r2"], "pkg-1"),
+            (["{PF}", "=cat/pkg-1-r2"], "pkg-1-r2"),
+            (["{PN}", "=cat/pkg-1-r2"], "pkg"),
+            (["{PR}", "=cat/pkg-1-r2"], "2"),
+            (["{PV}", "=cat/pkg-1-r2"], "1"),
+            (["{PVR}", "=cat/pkg-1-r2"], "1-r2"),
+            (["{CPN}", "=cat/pkg-1-r2"], "cat/pkg"),
+            (["{CPV}", "=cat/pkg-1-r2"], "cat/pkg-1-r2"),
+            (["{OP}", "=cat/pkg-1-r2"], "="),
+            (["{SLOT}", "=cat/pkg-1-r2"], "<unset>"),
+            (["{SLOT}", "=cat/pkg-1-r2:0"], "0"),
+            (["{SUBSLOT}", "=cat/pkg-1-r2"], "<unset>"),
+            (["{SUBSLOT}", "=cat/pkg-1-r2:0/3"], "3"),
+            (["{SLOT_OP}", "=cat/pkg-1-r2"], "<unset>"),
+            (["{SLOT_OP}", "=cat/pkg-1-r2:="], "="),
+            (["{REPO}", "=cat/pkg-1-r2"], "<unset>"),
+            (["{REPO}", "=cat/pkg-1-r2::repo"], "repo"),
+            (["{DEP}", "=cat/pkg-1-r2"], "=cat/pkg-1-r2"),
+        ] {
+            cmd("pk dep parse")
+                .arg(opt)
+                .args(args)
+                .assert()
+                .stdout(lines_contain([expected]))
+                .stderr("")
+                .success();
+        }
+    }
 }
