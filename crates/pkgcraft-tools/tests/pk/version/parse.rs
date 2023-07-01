e111da65dcafcbf3a92e03655f9f4918527348c0
Tim Harder <radhermit@gmail.com>
2023-07-01 01:24:24 -0600

    pk version parse: add -f/--format tests

--- crates/pkgcraft-tools/tests/pk/version/parse.rs
+++ crates/pkgcraft-tools/tests/pk/version/parse.rs
@@ -2,6 +2,8 @@ use itertools::Itertools;
 use pkgcraft::test::{cmd, TEST_DATA};
 use predicates::prelude::*;
 
+use crate::predicates::lines_contain;
+
 #[test]
 fn valid() {
     let intersects = TEST_DATA
@@ -30,4 +32,25 @@ fn invalid() {
         .stdout("")
         .stderr(predicate::str::is_empty().not())
         .failure();
+}
+
+#[test]
+fn format() {
+    for opt in ["-f", "--format"] {
+        for (args, expected) in [
+            (["{OP}", ">1-r2"], ">"),
+            (["{OP}", "1-r2"], "<unset>"),
+            (["{VER}", "1-r2"], "1"),
+            (["{REV}", "1-r2"], "2"),
+            (["{REV}", "1"], "<unset>"),
+        ] {
+            cmd("pk version parse")
+                .arg(opt)
+                .args(args)
+                .assert()
+                .stdout(lines_contain([expected]))
+                .stderr("")
+                .success();
+        }
+    }
 }
