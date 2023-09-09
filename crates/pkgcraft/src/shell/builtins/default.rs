2d7e68173c8dc76337bd08ca5e4b80d2393fc4c8
Tim Harder <radhermit@gmail.com>
2023-09-09 17:56:49 -0600

    pkgcraft: add `default` builtin test for invalid phase usage

--- crates/pkgcraft/src/shell/builtins/default.rs
+++ crates/pkgcraft/src/shell/builtins/default.rs
@@ -21,7 +21,7 @@ pub(crate) fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
     if let Some(builtin) = BUILTINS.get(default_phase.as_str()) {
         builtin.run(&[])
     } else {
-        Err(Error::Base(format!("nonexistent default phase function: {default_phase}")))
+        Err(Error::Base(format!("{phase} phase has no default")))
     }
 }
 
@@ -31,6 +31,7 @@ make_builtin!("default", default_builtin, run, LONG_DOC, USAGE, &[("2..", &[Phas
 #[cfg(test)]
 mod tests {
     use crate::config::Config;
+    use crate::macros::assert_err_re;
     use crate::pkg::BuildablePackage;
     use crate::shell::{get_build_mut, BuildData};
 
@@ -53,6 +54,7 @@ mod tests {
             EAPI=8
             DESCRIPTION="testing default command"
             SLOT=0
+            VAR=1
             src_prepare() {{
                 default
                 VAR=2
@@ -65,5 +67,29 @@ mod tests {
         assert!(get_build_mut().user_patches_applied);
         // verify custom src_prepare() was run
         assert_eq!(scallop::variables::optional("VAR").as_deref(), Some("2"));
+    }
+
+    #[test]
+    fn invalid_phase() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+        let data = indoc::formatdoc! {r#"
+            EAPI=8
+            DESCRIPTION="testing default command"
+            SLOT=0
+            VAR=1
+            pkg_setup() {{
+                default
+                VAR=2
+            }}
+        "#};
+        let pkg = t.create_pkg_from_str("cat/pkg-1", &data).unwrap();
+        BuildData::from_pkg(&pkg);
+        let result = pkg.build();
+        assert_err_re!(result, "pkg_setup phase has no default$");
+        // verify custom pkg_setup() stopped on error
+        assert_eq!(scallop::variables::optional("VAR").as_deref(), Some("1"));
+        // verify build phases didn't keep running
+        assert!(!get_build_mut().user_patches_applied);
     }
 }
