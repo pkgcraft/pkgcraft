d2c06906a6039023177a9f4ef52c1aa688c95d5b
Tim Harder <radhermit@gmail.com>
2023-09-11 03:53:05 -0600

    pkgcraft: add initial usage tests for `default_src_install`

--- crates/pkgcraft/src/shell/builtins/default_src_install.rs
+++ crates/pkgcraft/src/shell/builtins/default_src_install.rs
@@ -25,6 +25,13 @@ make_builtin!(
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
+    use crate::config::Config;
+    use crate::macros::assert_err_re;
+    use crate::pkg::BuildablePackage;
+    use crate::shell::{test::FileTree, BuildData};
+
     use super::super::{assert_invalid_args, builtin_scope_tests};
     use super::run as default_src_install;
     use super::*;
@@ -36,5 +43,61 @@ mod tests {
         assert_invalid_args(default_src_install, &[1]);
     }
 
-    // TODO: add usage tests
+    #[test]
+    fn valid_phase() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+        let data = indoc::formatdoc! {r#"
+            EAPI=8
+            DESCRIPTION="testing default_src_install command"
+            SLOT=0
+            VAR=1
+            DOCS=( "${{FILESDIR}}"/readme )
+            src_install() {{
+                default_src_install
+                VAR=2
+            }}
+        "#};
+        let pkg = t.create_pkg_from_str("cat/pkg-1", &data).unwrap();
+
+        // create docs file
+        let filesdir = pkg.abspath().parent().unwrap().join("files");
+        fs::create_dir(&filesdir).unwrap();
+        fs::write(filesdir.join("readme"), "data").unwrap();
+
+        BuildData::from_pkg(&pkg);
+        let file_tree = FileTree::new_build();
+        pkg.build().unwrap();
+        // verify default src_install() was run
+        file_tree.assert(
+            r#"
+            [[files]]
+            path = "/usr/share/doc/pkg-1/readme"
+        "#,
+        );
+        // verify custom src_install() was run
+        assert_eq!(scallop::variables::optional("VAR").as_deref(), Some("2"));
+    }
+
+    #[test]
+    fn invalid_phase() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+        let data = indoc::formatdoc! {r#"
+            EAPI=8
+            DESCRIPTION="testing default_src_install command"
+            SLOT=0
+            VAR=1
+            pkg_setup() {{
+                default_src_install
+                VAR=2
+            }}
+        "#};
+        let pkg = t.create_pkg_from_str("cat/pkg-1", &data).unwrap();
+        BuildData::from_pkg(&pkg);
+        let r = pkg.build();
+        assert_err_re!(r, "pkg_setup scope doesn't enable command: default_src_install$");
+        // verify custom pkg_setup() stopped on error
+        assert_eq!(scallop::variables::optional("VAR").as_deref(), Some("1"));
+    }
 }
