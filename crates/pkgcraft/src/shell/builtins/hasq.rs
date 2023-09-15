60b5945c07d9baef45a21351db745d27e374c592
Tim Harder <radhermit@gmail.com>
2023-09-15 10:13:31 -0600

    pkgcraft: drop unnecessary func impl for `hasq` builtin

--- crates/pkgcraft/src/shell/builtins/hasq.rs
+++ crates/pkgcraft/src/shell/builtins/hasq.rs
@@ -1,16 +1,8 @@
-use scallop::builtins::ExecStatus;
-
 use super::{has::run as has, make_builtin, Scopes::All};
 
 const LONG_DOC: &str = "Deprecated synonym for has.";
-
-#[doc = stringify!(LONG_DOC)]
-pub(crate) fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
-    has(args)
-}
-
 const USAGE: &str = "hasq needle ${haystack}";
-make_builtin!("hasq", hasq_builtin, run, LONG_DOC, USAGE, &[("0..8", &[All])]);
+make_builtin!("hasq", hasq_builtin, has, LONG_DOC, USAGE, &[("0..8", &[All])]);
 
 #[cfg(test)]
 mod tests {
