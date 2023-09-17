210cd2fcbd3ae468c4beb3ea5f2ec7dca25d1588
Tim Harder <radhermit@gmail.com>
2023-09-17 06:33:52 -0600

    pkgcraft: add initial tests for the `eapply_user` builtin

--- crates/pkgcraft/src/shell/builtins/eapply_user.rs
+++ crates/pkgcraft/src/shell/builtins/eapply_user.rs
@@ -21,9 +21,9 @@ pub(crate) fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
         if !args.is_empty() {
             eapply(&args)?;
         }
-    }
 
-    build.user_patches_applied = true;
+        build.user_patches_applied = true;
+    }
 
     Ok(ExecStatus::Success)
 }
@@ -33,6 +33,14 @@ make_builtin!("eapply_user", eapply_user_builtin, run, LONG_DOC, USAGE, [("6..",
 
 #[cfg(test)]
 mod tests {
+    use std::{env, fs};
+
+    use tempfile::tempdir;
+
+    use crate::config::Config;
+    use crate::macros::assert_err_re;
+    use crate::shell::BuildData;
+
     use super::super::{assert_invalid_args, builtin_scope_tests};
     use super::run as eapply_user;
     use super::*;
@@ -44,5 +52,81 @@ mod tests {
         assert_invalid_args(eapply_user, &[1]);
     }
 
-    // TODO: add tests
+    #[test]
+    fn failure() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+        let pkg = t.create_pkg("cat/pkg-1", &[]).unwrap();
+        BuildData::from_pkg(&pkg);
+        get_build_mut().user_patches = vec!["file.patch".to_string()];
+
+        let file_content = indoc::indoc! {"
+            1
+        "};
+        let bad_content = indoc::indoc! {"
+            --- a/file.txt
+            +++ a/file.txt
+            @@ -1 +1 @@
+            -2
+            +3
+        "};
+        let bad_prefix = indoc::indoc! {"
+            --- a/b/file.txt
+            +++ a/b/file.txt
+            @@ -1 +1 @@
+            -1
+            +2
+        "};
+
+        let dir = tempdir().unwrap();
+        env::set_current_dir(&dir).unwrap();
+        fs::write("file.txt", file_content).unwrap();
+        for data in [bad_content, bad_prefix] {
+            fs::write("file.patch", data).unwrap();
+            let r = eapply_user(&[]);
+            assert_err_re!(r, "^failed applying: file.patch");
+        }
+    }
+
+    #[test]
+    fn success() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+        let pkg = t.create_pkg("cat/pkg-1", &[]).unwrap();
+        BuildData::from_pkg(&pkg);
+        get_build_mut().user_patches =
+            vec!["files/0.patch".to_string(), "files/1.patch".to_string()];
+
+        let file_content = indoc::indoc! {"
+            0
+        "};
+        let patch0 = indoc::indoc! {"
+            --- a/file.txt
+            +++ a/file.txt
+            @@ -1 +1 @@
+            -0
+            +1
+        "};
+        let patch1 = indoc::indoc! {"
+            --- a/file.txt
+            +++ a/file.txt
+            @@ -1 +1 @@
+            -1
+            +2
+        "};
+
+        let dir = tempdir().unwrap();
+        env::set_current_dir(&dir).unwrap();
+        fs::write("file.txt", file_content).unwrap();
+        fs::create_dir("files").unwrap();
+        for (i, data) in [patch0, patch1].iter().enumerate() {
+            let file = format!("files/{i}.patch");
+            fs::write(file, data).unwrap();
+        }
+        assert_eq!(fs::read_to_string("file.txt").unwrap(), "0\n");
+        eapply_user(&[]).unwrap();
+        assert_eq!(fs::read_to_string("file.txt").unwrap(), "2\n");
+        // re-running doesn't apply patches
+        eapply_user(&[]).unwrap();
+    }
 }
