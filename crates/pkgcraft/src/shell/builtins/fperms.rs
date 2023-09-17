56086c50bf96afc906c958bfb9f666abaedc8942
Tim Harder <radhermit@gmail.com>
2023-09-17 08:02:00 -0600

    pkgcraft: fix error handling for `fperms` failures and add tests

--- crates/pkgcraft/src/shell/builtins/fperms.rs
+++ crates/pkgcraft/src/shell/builtins/fperms.rs
@@ -20,7 +20,7 @@ pub(crate) fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
     Command::new("chmod")
         .args(args.iter().map(|s| s.trim_start_matches('/')))
         .current_dir(get_build_mut().destdir())
-        .run()?;
+        .run_with_output()?;
 
     Ok(ExecStatus::Success)
 }
@@ -37,6 +37,12 @@ make_builtin!(
 
 #[cfg(test)]
 mod tests {
+    use crate::command::run_commands;
+    use crate::config::Config;
+    use crate::macros::assert_err_re;
+    use crate::pkg::BuildablePackage;
+    use crate::shell::{test::FileTree, BuildData};
+
     use super::super::{assert_invalid_args, builtin_scope_tests};
     use super::run as fperms;
     use super::*;
@@ -48,5 +54,63 @@ mod tests {
         assert_invalid_args(fperms, &[0, 1]);
     }
 
-    // TODO: add usage tests
+    #[test]
+    fn failure() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+        let data = indoc::formatdoc! {r#"
+            EAPI=8
+            DESCRIPTION="testing fperms command"
+            SLOT=0
+            src_install() {{
+                fperms 0777 /nonexistent
+            }}
+        "#};
+        let pkg = t.create_pkg_from_str("cat/pkg-1", &data).unwrap();
+        BuildData::from_pkg(&pkg);
+        let _file_tree = FileTree::new();
+        run_commands(|| {
+            let r = pkg.build();
+            assert_err_re!(
+                r,
+                "failed running: chmod: cannot access 'nonexistent': No such file or directory$"
+            );
+        })
+    }
+
+    #[test]
+    fn success() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+        for eapi in BUILTIN.scope.keys() {
+            let data = indoc::formatdoc! {r#"
+                EAPI={eapi}
+                DESCRIPTION="testing fperms command"
+                SLOT=0
+                src_install() {{
+                    touch file1 file2
+                    doins file1 file2
+                    # absolute paths work
+                    fperms 0777 /file1
+                    # relative paths work
+                    fperms 0757 file2
+                }}
+            "#};
+            let pkg = t.create_pkg_from_str("cat/pkg-1", &data).unwrap();
+            BuildData::from_pkg(&pkg);
+            let file_tree = FileTree::new();
+            run_commands(|| pkg.build().unwrap());
+            // verify file modes were changed
+            file_tree.assert(
+                r#"
+                [[files]]
+                path = "/file1"
+                mode = 0o100777
+                [[files]]
+                path = "/file2"
+                mode = 0o100757
+            "#,
+            );
+        }
+    }
 }
