d1ee9a5cc5cf7f95872aa45bea5c8891c61a4dcd
Tim Harder <radhermit@gmail.com>
2023-09-18 10:41:49 -0600

    pkgcraft: add initial implementations for `best_version` and `has_version`

--- crates/pkgcraft/src/shell/builtins/best_version.rs
+++ crates/pkgcraft/src/shell/builtins/best_version.rs
@@ -1,13 +1,31 @@
 use scallop::builtins::ExecStatus;
+use scallop::Error;
+
+use crate::shell::{get_build_mut, write_stdout};
 
 use super::{make_builtin, Scopes::Phases};
 
 const LONG_DOC: &str = "Output the highest matching version of a package dependency is installed.";
 
 #[doc = stringify!(LONG_DOC)]
-pub(crate) fn run(_args: &[&str]) -> scallop::Result<ExecStatus> {
-    // TODO: fill out this stub
-    Ok(ExecStatus::Success)
+pub(crate) fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
+    let build = get_build_mut();
+    let dep = match args[..] {
+        [s] => build.eapi().dep(s)?,
+        _ => return Err(Error::Base(format!("requires 1 arg, got {}", args.len()))),
+    };
+
+    // TODO: use the build config's install repo
+    let mut cpvs: Vec<_> = build.repo()?.iter_cpv_restrict(&dep).collect();
+    cpvs.sort();
+
+    if let Some(cpv) = cpvs.last() {
+        write_stdout!("{cpv}")?;
+        Ok(ExecStatus::Success)
+    } else {
+        write_stdout!("")?;
+        Ok(ExecStatus::Failure(1))
+    }
 }
 
 const USAGE: &str = "best_version cat/pkg";
@@ -15,8 +33,16 @@ make_builtin!("best_version", best_version_builtin, run, LONG_DOC, USAGE, [(".."
 
 #[cfg(test)]
 mod tests {
-    use super::super::builtin_scope_tests;
+    use super::super::{assert_invalid_args, builtin_scope_tests};
+    use super::run as best_version;
     use super::*;
 
     builtin_scope_tests!(USAGE);
+
+    #[test]
+    fn invalid_args() {
+        assert_invalid_args(best_version, &[0]);
+    }
+
+    // TODO: add usage tests
 }
--- crates/pkgcraft/src/shell/builtins/has_version.rs
+++ crates/pkgcraft/src/shell/builtins/has_version.rs
@@ -1,13 +1,26 @@
 use scallop::builtins::ExecStatus;
+use scallop::Error;
+
+use crate::shell::get_build_mut;
 
 use super::{make_builtin, Scopes::Phases};
 
 const LONG_DOC: &str = "Determine if a package dependency is installed.";
 
 #[doc = stringify!(LONG_DOC)]
-pub(crate) fn run(_args: &[&str]) -> scallop::Result<ExecStatus> {
-    // TODO: fill out this stub
-    Ok(ExecStatus::Success)
+pub(crate) fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
+    let build = get_build_mut();
+    let dep = match args[..] {
+        [s] => build.eapi().dep(s)?,
+        _ => return Err(Error::Base(format!("requires 1 arg, got {}", args.len()))),
+    };
+
+    // TODO: use the build config's install repo
+    if build.repo()?.iter_cpv_restrict(&dep).next().is_some() {
+        Ok(ExecStatus::Success)
+    } else {
+        Ok(ExecStatus::Failure(1))
+    }
 }
 
 const USAGE: &str = "has_version 'cat/pkg[use]'";
@@ -15,8 +28,16 @@ make_builtin!("has_version", has_version_builtin, run, LONG_DOC, USAGE, [("..", 
 
 #[cfg(test)]
 mod tests {
-    use super::super::builtin_scope_tests;
+    use super::super::{assert_invalid_args, builtin_scope_tests};
+    use super::run as has_version;
     use super::*;
 
     builtin_scope_tests!(USAGE);
+
+    #[test]
+    fn invalid_args() {
+        assert_invalid_args(has_version, &[0]);
+    }
+
+    // TODO: add usage tests
 }
