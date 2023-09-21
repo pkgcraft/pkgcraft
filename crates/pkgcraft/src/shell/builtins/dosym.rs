ef4c72836fec5d45602bc1e3a654a1119f9f0f55
Tim Harder <radhermit@gmail.com>
2023-09-21 05:14:24 -0600

    pkgcraft: dosym: use utf8 path support for arguments

--- crates/pkgcraft/src/shell/builtins/dosym.rs
+++ crates/pkgcraft/src/shell/builtins/dosym.rs
@@ -1,10 +1,11 @@
 use std::os::unix::fs::symlink;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
+use camino::{Utf8Path, Utf8PathBuf};
 use scallop::builtins::ExecStatus;
 use scallop::Error;
 
-use crate::eapi::Feature;
+use crate::eapi::Feature::DosymRelative;
 use crate::shell::get_build_mut;
 use crate::shell::phase::PhaseKind::SrcInstall;
 use crate::utils::relpath;
@@ -14,19 +15,20 @@ use super::make_builtin;
 const LONG_DOC: &str = "Create symbolic links.";
 
 /// Convert link target from an absolute path to a path relative to its name.
-fn convert_target(target: &str, name: &str) -> scallop::Result<PathBuf> {
-    let (target_path, name_path) = (Path::new(target), Path::new(name));
+fn convert_target<P: AsRef<Utf8Path>>(target: P, name: P) -> scallop::Result<PathBuf> {
+    let target = target.as_ref();
+    let name = name.as_ref();
 
-    if !target_path.is_absolute() {
+    if !target.is_absolute() {
         return Err(Error::Base(format!("absolute path required with '-r': {target}")));
     }
 
-    let mut parent = PathBuf::from("/");
-    if let Some(p) = name_path.parent() {
+    let mut parent = Utf8PathBuf::from("/");
+    if let Some(p) = name.parent() {
         parent.push(p)
     }
 
-    relpath(target_path, &parent)
+    relpath(target, &parent)
         .ok_or_else(|| Error::Base(format!("invalid relative path: {target} -> {name}")))
 }
 
@@ -34,7 +36,7 @@ fn convert_target(target: &str, name: &str) -> scallop::Result<PathBuf> {
 pub(crate) fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
     let eapi = get_build_mut().eapi();
     let (target, name) = match args[..] {
-        [opt, target, name] if opt == "-r" && eapi.has(Feature::DosymRelative) => {
+        [opt, target, name] if opt == "-r" && eapi.has(DosymRelative) => {
             (convert_target(target, name)?, name)
         }
         [target, name] => (PathBuf::from(target), name),
@@ -42,7 +44,7 @@ pub(crate) fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
     };
 
     // check for unsupported dir target arg -- https://bugs.gentoo.org/379899
-    let name_path = Path::new(name);
+    let name_path = Utf8Path::new(name);
     if name.ends_with('/') || (name_path.is_dir() && !name_path.is_symlink()) {
         return Err(Error::Base(format!("missing filename target: {target:?}")));
     }
