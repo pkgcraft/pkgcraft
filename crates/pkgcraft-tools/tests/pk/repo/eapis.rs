0608ad1495de2d449014ec07b9b4944c8ee2a47d
Tim Harder <radhermit@gmail.com>
2023-11-02 09:18:22 -0600

    pk repo eapis: add tests for --eapi option

--- crates/pkgcraft-tools/tests/pk/repo/eapis.rs
+++ crates/pkgcraft-tools/tests/pk/repo/eapis.rs
@@ -2,6 +2,8 @@ use pkgcraft::repo::ebuild::temp::Repo as TempRepo;
 use pkgcraft::test::cmd;
 use predicates::prelude::*;
 
+use crate::predicates::lines_contain;
+
 #[test]
 fn missing_repo_arg() {
     cmd("pk repo eapis")
@@ -68,6 +70,38 @@ fn multiple_repos() {
         .args([t1.path(), t2.path()])
         .assert()
         .stdout(predicate::str::is_empty().not())
+        .stderr("")
+        .success();
+}
+
+#[test]
+fn option_eapi() {
+    let t = TempRepo::new("test", None, 0, None).unwrap();
+    t.create_raw_pkg("a/b-1", &["EAPI=8"]).unwrap();
+    t.create_raw_pkg("cat/pkg-2", &["EAPI=8"]).unwrap();
+
+    // invalid EAPI
+    cmd("pk repo eapis --eapi nonexistent")
+        .arg(t.path())
+        .assert()
+        .stdout("")
+        .stderr(predicate::str::is_empty().not())
+        .failure()
+        .code(2);
+
+    // matching packages
+    cmd("pk repo eapis --eapi 8")
+        .arg(t.path())
+        .assert()
+        .stdout(lines_contain(["a/b-1", "cat/pkg-2"]))
+        .stderr("")
+        .success();
+
+    // no matching packages
+    cmd("pk repo eapis --eapi 7")
+        .arg(t.path())
+        .assert()
+        .stdout("")
         .stderr("")
         .success();
 }
