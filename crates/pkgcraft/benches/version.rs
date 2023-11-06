5a5b9aaf969578c45998c08a72f6c4df5d522ccb
Tim Harder <radhermit@gmail.com>
2023-11-06 07:57:53 -0700

    pkgcraft: use Version::new() instead of FromStr for benchmarks

--- crates/pkgcraft/benches/version.rs
+++ crates/pkgcraft/benches/version.rs
@@ -1,34 +1,32 @@
-use std::str::FromStr;
-
 use criterion::Criterion;
 
 use pkgcraft::dep::{Intersects, Version};
 
 pub fn bench_pkg_versions(c: &mut Criterion) {
-    c.bench_function("version-parse", |b| b.iter(|| Version::from_str("1.2.3_alpha4-r5")));
+    c.bench_function("version-parse", |b| b.iter(|| Version::new("1.2.3_alpha4-r5")));
 
     c.bench_function("version-cmp-eq", |b| {
-        let v1 = Version::from_str("1.2.3a_beta4-r5").unwrap();
-        let v2 = Version::from_str("1.2.3a_beta4-r5").unwrap();
+        let v1 = Version::new("1.2.3a_beta4-r5").unwrap();
+        let v2 = Version::new("1.2.3a_beta4-r5").unwrap();
         b.iter(|| v1 == v2);
     });
 
     c.bench_function("version-cmp-lt", |b| {
-        let v1 = Version::from_str("1.2.3a_beta4-r4").unwrap();
-        let v2 = Version::from_str("1.2.3a_beta5-r5").unwrap();
+        let v1 = Version::new("1.2.3a_beta4-r4").unwrap();
+        let v2 = Version::new("1.2.3a_beta5-r5").unwrap();
         b.iter(|| v1 < v2);
     });
 
     c.bench_function("version-intersects", |b| {
-        let v1 = Version::from_str(">=1.2.3").unwrap();
-        let v2 = Version::from_str("=1.2*").unwrap();
+        let v1 = Version::new(">=1.2.3").unwrap();
+        let v2 = Version::new("=1.2*").unwrap();
         b.iter(|| v1.intersects(&v2));
     });
 
     c.bench_function("version-cmp-sort-simple", |b| {
         let mut versions: Vec<_> = (0..100)
             .rev()
-            .map(|s| Version::from_str(&format!("{}", s)).unwrap())
+            .map(|s| Version::new(&format!("{}", s)).unwrap())
             .collect();
         b.iter(|| versions.sort());
     });
@@ -59,7 +57,7 @@ pub fn bench_pkg_versions(c: &mut Criterion) {
         ]
         .into_iter()
         .rev()
-        .map(|s| Version::from_str(s).unwrap())
+        .map(|s| Version::new(s).unwrap())
         .collect();
         b.iter(|| versions.sort());
     });
