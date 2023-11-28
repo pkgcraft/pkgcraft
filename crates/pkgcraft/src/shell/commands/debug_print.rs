2022fbf5cae31afb669e10c2fc201b864dad09a2
Tim Harder <radhermit@gmail.com>
2023-11-27 19:41:20 -0700

    pkgcraft: add basic eclass and global scope tests for debug-print* commands

--- crates/pkgcraft/src/shell/commands/debug_print.rs
+++ crates/pkgcraft/src/shell/commands/debug_print.rs
@@ -18,10 +18,61 @@ make_builtin!("debug-print", debug_print_builtin);
 
 #[cfg(test)]
 mod tests {
+    use tracing_test::traced_test;
+
+    use crate::config::Config;
+    use crate::macros::assert_logs_re;
+    use crate::pkg::Source;
+
     use super::super::cmd_scope_tests;
     use super::*;
 
     cmd_scope_tests!(USAGE);
 
-    // TODO: add usage tests
+    #[traced_test]
+    #[test]
+    fn eclass() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+
+        let eclass = indoc::indoc! {r#"
+            # stub eclass
+            debug-print "eclass: ${ECLASS}"
+        "#};
+        t.create_eclass("e1", eclass).unwrap();
+        let data = indoc::indoc! {r#"
+            EAPI=8
+            inherit e1
+            DESCRIPTION="testing debug-print"
+            SLOT=0
+        "#};
+        let raw_pkg = t.create_raw_pkg_from_str("cat/pkg-1", data).unwrap();
+        raw_pkg.source().unwrap();
+        assert_logs_re!("eclass: e1$");
+    }
+
+    #[traced_test]
+    #[test]
+    fn global() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+
+        let eclass = indoc::indoc! {r#"
+            # stub eclass
+            e1_func() {
+                debug-print ${FUNCNAME}: "$@"
+            }
+        "#};
+        t.create_eclass("e1", eclass).unwrap();
+        let data = indoc::indoc! {r#"
+            EAPI=8
+            inherit e1
+            DESCRIPTION="testing debug-print"
+            SLOT=0
+            e1_func msg 1 2 3
+        "#};
+        let raw_pkg = t.create_raw_pkg_from_str("cat/pkg-1", data).unwrap();
+        raw_pkg.source().unwrap();
+        assert_logs_re!("e1_func: msg 1 2 3$");
+    }
 }
--- crates/pkgcraft/src/shell/commands/debug_print_section.rs
+++ crates/pkgcraft/src/shell/commands/debug_print_section.rs
@@ -21,6 +21,12 @@ make_builtin!("debug-print-section", debug_print_section_builtin);
 
 #[cfg(test)]
 mod tests {
+    use tracing_test::traced_test;
+
+    use crate::config::Config;
+    use crate::macros::assert_logs_re;
+    use crate::pkg::Source;
+
     use super::super::{assert_invalid_args, cmd_scope_tests, debug_print_section};
     use super::*;
 
@@ -31,5 +37,53 @@ mod tests {
         assert_invalid_args(debug_print_section, &[0]);
     }
 
-    // TODO: add usage tests
+    #[traced_test]
+    #[test]
+    fn eclass() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+
+        let eclass = indoc::indoc! {r#"
+            # stub eclass
+            e1_func() {
+                debug-print-section section1 "$@"
+            }
+            e1_func msg 1 2 3
+        "#};
+        t.create_eclass("e1", eclass).unwrap();
+        let data = indoc::indoc! {r#"
+            EAPI=8
+            inherit e1
+            DESCRIPTION="testing debug-print-section"
+            SLOT=0
+        "#};
+        let raw_pkg = t.create_raw_pkg_from_str("cat/pkg-1", data).unwrap();
+        raw_pkg.source().unwrap();
+        assert_logs_re!("now in section section1 msg 1 2 3$");
+    }
+
+    #[traced_test]
+    #[test]
+    fn global() {
+        let mut config = Config::default();
+        let t = config.temp_repo("test", 0, None).unwrap();
+
+        let eclass = indoc::indoc! {r#"
+            # stub eclass
+            e1_func() {
+                debug-print-section section1 "$@"
+            }
+        "#};
+        t.create_eclass("e1", eclass).unwrap();
+        let data = indoc::indoc! {r#"
+            EAPI=8
+            inherit e1
+            DESCRIPTION="testing debug-print-section"
+            SLOT=0
+            e1_func msg 1 2 3
+        "#};
+        let raw_pkg = t.create_raw_pkg_from_str("cat/pkg-1", data).unwrap();
+        raw_pkg.source().unwrap();
+        assert_logs_re!("now in section section1 msg 1 2 3$");
+    }
 }
