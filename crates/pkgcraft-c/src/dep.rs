e47ed9d3d313ee44c0264cf9524377297eeb99a9
Tim Harder <radhermit@gmail.com>
2023-12-07 20:36:56 -0700

    pkgcraft-c: split uri support into dep::uri

--- crates/pkgcraft-c/src/dep.rs
+++ crates/pkgcraft-c/src/dep.rs
@@ -23,6 +23,7 @@ use crate::utils::boxed;
 
 pub mod cpv;
 pub mod pkg;
+pub mod uri;
 pub mod version;
 
 /// DepSet variants.
@@ -1451,46 +1452,5 @@ pub unsafe extern "C" fn pkgcraft_dep_set_into_iter_conditionals_free(
 pub unsafe extern "C" fn pkgcraft_dep_set_free(d: *mut DepSet) {
     if !d.is_null() {
         unsafe { drop(Box::from_raw(d)) };
-    }
-}
-
-/// Get the main URI from a Uri object.
-///
-/// # Safety
-/// The argument must be a Uri pointer.
-#[no_mangle]
-pub unsafe extern "C" fn pkgcraft_uri_uri(u: *mut Uri) -> *mut c_char {
-    let uri = try_ref_from_ptr!(u);
-    try_ptr_from_str!(uri.uri())
-}
-
-/// Get the filename for a Uri.
-///
-/// # Safety
-/// The argument must be a Uri pointer.
-#[no_mangle]
-pub unsafe extern "C" fn pkgcraft_uri_filename(u: *mut Uri) -> *mut c_char {
-    let uri = try_ref_from_ptr!(u);
-    try_ptr_from_str!(uri.filename())
-}
-
-/// Return the formatted string for a Uri object.
-///
-/// # Safety
-/// The argument must be a Uri pointer.
-#[no_mangle]
-pub unsafe extern "C" fn pkgcraft_uri_str(u: *mut Uri) -> *mut c_char {
-    let uri = try_ref_from_ptr!(u);
-    try_ptr_from_str!(uri.to_string())
-}
-
-/// Free a Uri object.
-///
-/// # Safety
-/// The argument must be a valid Uri pointer or NULL.
-#[no_mangle]
-pub unsafe extern "C" fn pkgcraft_uri_free(u: *mut Uri) {
-    if !u.is_null() {
-        unsafe { drop(Box::from_raw(u)) };
     }
 }
---
+++ crates/pkgcraft-c/src/dep/uri.rs
@@ -0,0 +1,46 @@
+use std::ffi::c_char;
+
+use pkgcraft::dep::Uri;
+
+use crate::macros::*;
+
+/// Get the main URI from a Uri object.
+///
+/// # Safety
+/// The argument must be a Uri pointer.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_uri_uri(u: *mut Uri) -> *mut c_char {
+    let uri = try_ref_from_ptr!(u);
+    try_ptr_from_str!(uri.uri())
+}
+
+/// Get the filename for a Uri.
+///
+/// # Safety
+/// The argument must be a Uri pointer.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_uri_filename(u: *mut Uri) -> *mut c_char {
+    let uri = try_ref_from_ptr!(u);
+    try_ptr_from_str!(uri.filename())
+}
+
+/// Return the formatted string for a Uri object.
+///
+/// # Safety
+/// The argument must be a Uri pointer.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_uri_str(u: *mut Uri) -> *mut c_char {
+    let uri = try_ref_from_ptr!(u);
+    try_ptr_from_str!(uri.to_string())
+}
+
+/// Free a Uri object.
+///
+/// # Safety
+/// The argument must be a valid Uri pointer or NULL.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_uri_free(u: *mut Uri) {
+    if !u.is_null() {
+        unsafe { drop(Box::from_raw(u)) };
+    }
+}
