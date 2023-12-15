52466c84032ef053c5a63ced353713d11d1b4da9
Tim Harder <radhermit@gmail.com>
2023-12-15 12:25:30 -0700

    pkgcraft: add dep-new benchmark to directly compare with dep-parse
    For owned vs borrowed Dep objects.

--- crates/pkgcraft/benches/dep.rs
+++ crates/pkgcraft/benches/dep.rs
@@ -4,22 +4,24 @@ use pkgcraft::dep::{Dep, DepField};
 
 pub fn bench_pkg_deps(c: &mut Criterion) {
     c.bench_function("dep-parse", |b| {
-        b.iter(|| Dep::parse(">=cat/pkg-1.2.3-r4:5/6=[a,b,c]", None))
+        b.iter(|| Dep::parse(">=cat/pkg-1.2.3-r4:5/6=[a,-b,c?]", None))
     });
 
-    c.bench_function("dep-new-unversioned", |b| b.iter(|| Dep::new("cat/pkg")));
+    c.bench_function("dep-new", |b| b.iter(|| Dep::new(">=cat/pkg-1.2.3-r4:5/6=[a,-b,c?]")));
 
-    c.bench_function("dep-new-slotdep", |b| b.iter(|| Dep::new("cat/pkg:0")));
+    c.bench_function("dep-unversioned", |b| b.iter(|| Dep::new("cat/pkg")));
 
-    c.bench_function("dep-new-versioned", |b| b.iter(|| Dep::new(">=cat/pkg-4-r1")));
+    c.bench_function("dep-slotdep", |b| b.iter(|| Dep::new("cat/pkg:0")));
 
-    c.bench_function("dep-new-versioned-slotdep", |b| b.iter(|| Dep::new(">=cat/pkg-4-r1:0=")));
+    c.bench_function("dep-versioned", |b| b.iter(|| Dep::new(">=cat/pkg-4-r1")));
 
-    c.bench_function("dep-new-usedeps", |b| {
+    c.bench_function("dep-versioned-slotdep", |b| b.iter(|| Dep::new(">=cat/pkg-4-r1:0=")));
+
+    c.bench_function("dep-usedeps", |b| {
         b.iter(|| Dep::new(">=cat/pkg-4-r1:0=[a,b=,!c=,d?,!e?,-f]"))
     });
 
-    c.bench_function("dep-new-long-usedeps", |b| {
+    c.bench_function("dep-long-usedeps", |b| {
         let flags: Vec<String> = (0..100).map(|s| s.to_string()).collect();
         let s = format!("cat/pkg[{}]", &flags.join(","));
         b.iter(|| Dep::new(&s));
