24e8b73a1011755cff9118752fbc61384bd345e4
Tim Harder <radhermit@gmail.com>
2023-12-31 07:36:22 -0700

    pk dep compare: support Cpv targets

--- crates/pkgcraft-tools/src/bin/pk/subcmds/dep/compare.rs
+++ crates/pkgcraft-tools/src/bin/pk/subcmds/dep/compare.rs
@@ -4,29 +4,29 @@ use std::process::ExitCode;
 use anyhow::{anyhow, bail};
 use clap::Args;
 use itertools::Itertools;
-use pkgcraft::dep::Dep;
+use pkgcraft::dep::CpvOrDep;
 
 use crate::args::StdinOrArgs;
 
 #[derive(Debug, Args)]
 pub struct Command {
-    /// Dep comparison expressions (uses stdin if "-")
+    /// Comparison expressions (uses stdin if "-")
     #[arg(value_name = "EXPR")]
-    vals: Vec<String>,
+    exprs: Vec<String>,
 }
 
 impl Command {
     pub(super) fn run(mut self) -> anyhow::Result<ExitCode> {
         let mut status = ExitCode::SUCCESS;
 
-        let vals = mem::take(&mut self.vals);
-        for s in vals.stdin_or_args() {
+        let exprs = mem::take(&mut self.exprs);
+        for s in exprs.stdin_or_args() {
             let (lhs, op, rhs) = s
                 .split_whitespace()
                 .collect_tuple()
                 .ok_or_else(|| anyhow!("invalid comparison format: {s}"))?;
-            let lhs = Dep::parse(lhs, Default::default())?;
-            let rhs = Dep::parse(rhs, Default::default())?;
+            let lhs = CpvOrDep::parse(lhs)?;
+            let rhs = CpvOrDep::parse(rhs)?;
             let result = match op {
                 "<" => lhs < rhs,
                 "<=" => lhs <= rhs,
