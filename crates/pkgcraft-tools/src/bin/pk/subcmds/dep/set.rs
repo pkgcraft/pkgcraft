a8d1b557a8c92336e23be071bfe4e282fd0e6642
Tim Harder <radhermit@gmail.com>
2023-12-31 07:31:04 -0700

    pk dep set: support Cpv targets

--- crates/pkgcraft-tools/src/bin/pk/subcmds/dep/set.rs
+++ crates/pkgcraft-tools/src/bin/pk/subcmds/dep/set.rs
@@ -3,26 +3,27 @@ use std::process::ExitCode;
 
 use clap::Args;
 use indexmap::IndexSet;
-use pkgcraft::dep::Dep;
+use pkgcraft::dep::CpvOrDep;
 
 use crate::args::StdinOrArgs;
 
 #[derive(Debug, Args)]
 pub struct Command {
-    vals: Vec<String>,
+    values: Vec<String>,
 }
 
 impl Command {
     pub(super) fn run(self) -> anyhow::Result<ExitCode> {
-        let deps: Vec<_> = self.vals.stdin_or_args().split_whitespace().collect();
-        let deps: Result<IndexSet<_>, _> = deps
-            .iter()
-            .map(|s| Dep::parse(s, Default::default()))
+        let values: Result<IndexSet<_>, _> = self
+            .values
+            .stdin_or_args()
+            .split_whitespace()
+            .map(|s| CpvOrDep::try_new(&s))
             .collect();
 
         let mut stdout = io::stdout().lock();
-        for d in deps? {
-            writeln!(stdout, "{d}")?;
+        for v in values? {
+            writeln!(stdout, "{v}")?;
         }
         Ok(ExitCode::SUCCESS)
     }
