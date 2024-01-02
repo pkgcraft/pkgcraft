15c356f80e8b5cbcc019a281d3bae0acdc829057
Tim Harder <radhermit@gmail.com>
2024-01-02 01:46:50 -0700

    pk cpv set: add initial tests

---
+++ crates/pkgcraft-tools/tests/pk/cpv/set.rs
@@ -0,0 +1,46 @@
+use pkgcraft::test::{cmd, TEST_DATA};
+
+#[test]
+fn stdin() {
+    // iterates over all values from stdin separated by whitespace
+    cmd("pk cpv set -")
+        .write_stdin("a/b-1 a/b-2\ncat/pkg-0 \ta/b-1")
+        .assert()
+        .stdout("a/b-1\na/b-2\ncat/pkg-0\n");
+
+    // invalid args
+    cmd("pk cpv set -").write_stdin("=a/b-1").assert().failure();
+
+    // remaining args ignored when "-" specified
+    cmd("pk cpv set -")
+        .args(["a/b-1", "a/b-2"])
+        .write_stdin("x/y-1 cat/pkg-2")
+        .assert()
+        .stdout("x/y-1\ncat/pkg-2\n");
+}
+
+#[test]
+fn args() {
+    // valid args
+    cmd("pk cpv set")
+        .args(["a/b-1", "a/b-1"])
+        .assert()
+        .stdout("a/b-1\n");
+
+    // invalid args
+    cmd("pk cpv set").arg("a/b").assert().failure();
+
+    for d in &TEST_DATA.version_toml.hashing {
+        let cpvs: Vec<_> = d.versions.iter().map(|s| format!("cat/pkg-{s}")).collect();
+        let output = cmd("pk cpv set").args(&cpvs).output().unwrap();
+        let set: Vec<_> = std::str::from_utf8(&output.stdout)
+            .unwrap()
+            .split_whitespace()
+            .collect();
+        if d.equal {
+            assert_eq!(set.len(), 1, "failed hashing cpvs: {cpvs:?}");
+        } else {
+            assert_eq!(set.len(), cpvs.len(), "failed hashing cpvs: {cpvs:?}");
+        }
+    }
+}
