32010e1f342d0fd39b83d720a1365beea925219c
Tim Harder <radhermit@gmail.com>
2024-01-17 11:20:31 -0700

    pkgcruft: DeprecatedDependency: collapse duplicate reports
    Since the report drops use dependencies, some ebuilds especially those
    that use eclasses with multiple, conditional, generated deps targeting
    different use deps (e.g. python) would cause duplicate reports.

--- crates/pkgcruft/src/check/dependency.rs
+++ crates/pkgcruft/src/check/dependency.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
 use pkgcraft::dep::{Flatten, Operator};
 use pkgcraft::pkg::ebuild::Pkg;
 use pkgcraft::pkg::Package;
@@ -36,16 +39,22 @@ impl<'a> CheckRun<Pkg<'a>> for DependencyCheck<'_> {
         use VersionReport::*;
 
         for key in pkg.eapi().dep_keys() {
+            let mut deprecated = HashSet::new();
+
             for dep in pkg.dependencies(&[*key]).into_iter_flatten() {
                 if self.repo.deprecated(dep).is_some() {
                     // drop use deps since package.deprecated doesn't include them
-                    let msg = format!("{key}: {}", dep.no_use_deps());
-                    reports.push(DeprecatedDependency.report(pkg, msg));
+                    deprecated.insert(dep.no_use_deps());
                 }
 
                 if matches!(dep.op(), Some(Operator::Equal)) && dep.revision().is_none() {
                     reports.push(MissingRevision.report(pkg, format!("{key}: {dep}")));
                 }
+            }
+
+            if !deprecated.is_empty() {
+                let msg = format!("{key}: {}", deprecated.iter().sorted().join(", "));
+                reports.push(DeprecatedDependency.report(pkg, msg));
             }
         }
 
