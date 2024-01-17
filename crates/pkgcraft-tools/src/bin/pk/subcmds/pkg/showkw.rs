af585ebcde4c2d96ee91995bc08338e2c52f95d5
Tim Harder <radhermit@gmail.com>
2024-01-17 15:56:23 -0700

    pk pkg showkw: migrate from target_restriction() to TargetRestrictions

--- crates/pkgcraft-tools/src/bin/pk/subcmds/pkg/showkw.rs
+++ crates/pkgcraft-tools/src/bin/pk/subcmds/pkg/showkw.rs
@@ -1,37 +1,49 @@
 use std::io::{self, Write};
 use std::process::ExitCode;
 
+use clap::builder::ArgPredicate;
 use clap::Args;
 use itertools::Itertools;
-use pkgcraft::cli::target_restriction;
+use pkgcraft::cli::TargetRestrictions;
 use pkgcraft::config::Config;
-use pkgcraft::repo::{PkgRepository, RepoFormat};
+use pkgcraft::repo::PkgRepository;
 
 use crate::args::StdinOrArgs;
 
 #[derive(Debug, Args)]
+#[clap(next_help_heading = "Target options")]
 pub struct Command {
+    /// Target repo
+    #[arg(long)]
+    repo: Option<String>,
+
     // positionals
     /// Target packages or paths
-    #[arg(value_name = "TARGET", default_value = ".")]
+    #[arg(
+        // default to the current working directory
+        default_value = ".",
+        // default to all packages when targeting a repo
+        default_value_if("repo", ArgPredicate::IsPresent, Some("*")),
+        help_heading = "Arguments",
+    )]
     targets: Vec<String>,
 }
 
 impl Command {
     pub(super) fn run(self, config: &mut Config) -> anyhow::Result<ExitCode> {
         // determine target restrictions
-        let targets: Result<Vec<_>, _> = self
-            .targets
-            .stdin_or_args()
-            .split_whitespace()
-            .map(|s| target_restriction(config, Some(RepoFormat::Ebuild), &s))
-            .collect();
-        let targets = targets?;
+        let targets = TargetRestrictions::new(config)
+            .repo(self.repo)?
+            .targets(self.targets.stdin_or_args().split_whitespace())?;
 
         let mut stdout = io::stdout().lock();
-        for (repos, restrict) in targets {
+        for (repo_set, restricts) in targets {
             // find matching packages from targeted repos
-            let pkgs = repos.ebuild().flat_map(|r| r.iter_restrict(&restrict));
+            let pkgs = restricts.iter().flat_map(|r| {
+                repo_set
+                    .ebuild()
+                    .flat_map(move |repo| repo.iter_restrict(r))
+            });
 
             // TODO: use tabular formatting output
             for pkg in pkgs {
