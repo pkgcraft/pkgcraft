c6b0213f519ef1fbb2ab9920374de3d7e64875e5
Tim Harder <radhermit@gmail.com>
2024-01-19 03:48:21 -0700

    pkgcruft replay: verify deserialization from stdin

--- crates/pkgcruft/tests/pkgcruft/replay.rs
+++ crates/pkgcruft/tests/pkgcruft/replay.rs
@@ -1,6 +1,8 @@
 use std::io::Write;
 
-use pkgcraft::test::cmd;
+use pkgcraft::repo::Repository;
+use pkgcraft::test::{cmd, TEST_DATA};
+use predicates::prelude::*;
 use predicates::str::contains;
 use tempfile::NamedTempFile;
 
@@ -31,6 +33,34 @@ fn invalid_file_target() {
 
     cmd("pkgcruft replay")
         .arg(file.path())
+        .assert()
+        .stdout("")
+        .stderr(contains("failed deserializing report"))
+        .failure()
+        .code(2);
+}
+
+#[test]
+fn stdin() {
+    let repo = TEST_DATA.ebuild_repo("qa-primary").unwrap();
+
+    // run scan, saving serialized report output
+    let output = cmd("pkgcruft scan -R json")
+        .arg(repo.path())
+        .output()
+        .unwrap();
+
+    // valid
+    cmd("pkgcruft replay -")
+        .write_stdin(output.stdout)
+        .assert()
+        .stdout(predicate::str::is_empty().not())
+        .stderr("")
+        .success();
+
+    // invalid
+    cmd("pkgcruft replay -")
+        .write_stdin("invalid serialized report\n")
         .assert()
         .stdout("")
         .stderr(contains("failed deserializing report"))
