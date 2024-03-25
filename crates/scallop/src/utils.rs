c78f5cd63d208086b054d8124a85b0a1f9c90bc7
Tim Harder <radhermit@gmail.com>
2024-03-25 13:37:55 -0600

    scallop: add todo note for dropping sigpipe hack

--- crates/scallop/src/utils.rs
+++ crates/scallop/src/utils.rs
@@ -1,4 +1,7 @@
-/// Reset the SIGPIPE to the default behavior.
+// TODO: drop this once stable rust supports `unix_sigpipe`,
+// see https://github.com/rust-lang/rust/issues/97889.
+//
+/// Reset SIGPIPE to the default behavior.
 pub fn reset_sigpipe() {
     unsafe {
         libc::signal(libc::SIGPIPE, libc::SIG_DFL);
