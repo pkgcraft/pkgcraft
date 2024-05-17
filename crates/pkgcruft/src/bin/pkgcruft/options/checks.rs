b5a57e6c75d2374f96eaef8f1b7a4cb0d0f44b36
Tim Harder <radhermit@gmail.com>
2024-05-17 00:47:54 -0600

    pkgcruft: add initial -l/--level support for specifying report variants

--- crates/pkgcruft/src/bin/pkgcruft/options/checks.rs
+++ crates/pkgcruft/src/bin/pkgcruft/options/checks.rs
@@ -1,7 +1,9 @@
+use std::collections::HashSet;
+
 use clap::builder::{PossibleValuesParser, TypedValueParser};
 use clap::Args;
 use pkgcruft::check::{Check, CheckKind, CHECKS, SOURCE_CHECKS};
-use pkgcruft::report::{ReportKind, REPORTS, REPORT_CHECKS};
+use pkgcruft::report::{ReportKind, ReportLevel, REPORTS, REPORT_CHECKS};
 use pkgcruft::source::SourceKind;
 use strum::VariantNames;
 
@@ -19,6 +21,18 @@ pub(crate) struct Checks {
             .map(|s| s.parse::<CheckKind>().unwrap()),
     )]
     checks: Vec<CheckKind>,
+
+    /// Limit to specific report levels
+    #[arg(
+        short,
+        long,
+        value_name = "LEVEL[,...]",
+        value_delimiter = ',',
+        hide_possible_values = true,
+        value_parser = PossibleValuesParser::new(ReportLevel::VARIANTS)
+            .map(|s| s.parse::<ReportLevel>().unwrap()),
+    )]
+    levels: Vec<ReportLevel>,
 
     /// Limit to specific report variants
     #[arg(
@@ -47,22 +61,34 @@ pub(crate) struct Checks {
 
 impl Checks {
     pub(crate) fn collapse(self) -> (Vec<Check>, Vec<ReportKind>) {
+        let mut reports = self.reports;
+
+        // enable reports related to level options
+        if !self.levels.is_empty() {
+            let levels: HashSet<_> = self.levels.into_iter().collect();
+            reports.extend(REPORTS.iter().filter(|r| levels.contains(&r.level())));
+        }
+
+        // enable reports related to source options
+        if !self.sources.is_empty() {
+            reports.extend(self.sources.iter().flat_map(|s| {
+                SOURCE_CHECKS
+                    .get(s)
+                    .unwrap_or_else(|| panic!("no checks for source variant: {s}"))
+                    .into_iter()
+                    .flat_map(|x| x.reports())
+            }));
+        }
+
         let mut checks: Vec<_> = self.checks.iter().map(Check::from).collect();
 
-        // add checks related to report options
-        checks.extend(self.reports.iter().flat_map(|r| {
+        // add checks related to enabled report variants
+        checks.extend(reports.iter().flat_map(|r| {
             REPORT_CHECKS
                 .get(r)
                 .unwrap_or_else(|| panic!("no checks for report variant: {r}"))
         }));
 
-        // add checks related to source options
-        checks.extend(self.sources.iter().flat_map(|s| {
-            SOURCE_CHECKS
-                .get(s)
-                .unwrap_or_else(|| panic!("no checks for source variant: {s}"))
-        }));
-
-        (checks, self.reports)
+        (checks, reports)
     }
 }
