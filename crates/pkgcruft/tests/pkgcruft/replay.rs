da72924984dd68a700085e0ea1b924bf556b613c
Tim Harder <radhermit@gmail.com>
2024-05-17 18:35:26 -0600

    pkgcruft replay: add tests for -c/--checks option

--- crates/pkgcruft/tests/pkgcruft/replay.rs
+++ crates/pkgcruft/tests/pkgcruft/replay.rs
@@ -95,6 +95,51 @@ fn file_targets() {
 }
 
 #[test]
+fn checks() {
+    let reports = indoc::indoc! {r#"
+        {"scope":{"Version":"x11-wm/qtile-0.22.1-r3"},"kind":{"Version":"DeprecatedDependency"},"description":"BDEPEND: media-sound/pulseaudio"}
+        {"scope":{"Version":"x11-wm/qtile-0.23.0-r1"},"kind":{"Version":"DeprecatedDependency"},"description":"BDEPEND: media-sound/pulseaudio"}
+        {"scope":{"Package":"x11-wm/qtile"},"kind":{"Package":"UnstableOnly"},"description":"x86"}
+        {"scope":{"Version":"x11-wm/mutter-45.1"},"kind":{"Version":"DroppedKeywords"},"description":"ppc64"}
+    "#};
+    let expected: Vec<_> = reports.lines().collect();
+
+    for opt in ["-c", "--checks"] {
+        // invalid
+        cmd("pkgcruft replay")
+            .args([opt, "invalid"])
+            .arg(QA_PRIMARY_FILE.path())
+            .assert()
+            .stdout("")
+            .stderr(predicate::str::is_empty().not())
+            .failure()
+            .code(2);
+
+        // valid
+        let output = cmd("pkgcruft replay -R json -")
+            .args([opt, "Dependency"])
+            .write_stdin(reports)
+            .output()
+            .unwrap()
+            .stdout;
+        let data = String::from_utf8(output).unwrap();
+        let data: Vec<_> = data.lines().collect();
+        assert_eq!(&data, &expected[0..2]);
+
+        // multiple values
+        let output = cmd("pkgcruft replay -R json -")
+            .args([opt, "Dependency,UnstableOnly"])
+            .write_stdin(reports)
+            .output()
+            .unwrap()
+            .stdout;
+        let data = String::from_utf8(output).unwrap();
+        let data: Vec<_> = data.lines().collect();
+        assert_eq!(&data, &expected[0..3]);
+    }
+}
+
+#[test]
 fn reports() {
     let reports = indoc::indoc! {r#"
         {"scope":{"Version":"x11-wm/qtile-0.22.1-r3"},"kind":{"Version":"DeprecatedDependency"},"description":"BDEPEND: media-sound/pulseaudio"}
@@ -135,7 +180,7 @@ fn reports() {
         let data: Vec<_> = data.lines().collect();
         assert_eq!(&data, &expected[0..2]);
 
-        // multiple report variants
+        // multiple values
         let output = cmd("pkgcruft replay -R json -")
             .args([opt, "DeprecatedDependency,UnstableOnly"])
             .write_stdin(reports)
