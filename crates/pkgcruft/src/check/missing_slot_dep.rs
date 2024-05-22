c2f4bb7e4a5a0adc64eeae48bc96c66697979814
Tim Harder <radhermit@gmail.com>
2024-05-22 11:23:52 -0600

    pkgcruft: add initial tests for MissingSlotDep

--- crates/pkgcruft/src/check/missing_slot_dep.rs
+++ crates/pkgcruft/src/check/missing_slot_dep.rs
@@ -44,3 +44,31 @@ impl<'a> super::CheckRun<&Pkg<'a>> for Check<'a> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use pkgcraft::repo::Repository;
+    use pkgcraft::test::TEST_DATA;
+    use pretty_assertions::assert_eq;
+
+    use crate::check::CheckKind::MissingSlotDep;
+    use crate::scanner::Scanner;
+    use crate::test::glob_reports;
+
+    #[test]
+    fn check() {
+        let repo = TEST_DATA.repo("qa-primary").unwrap();
+        let check_dir = repo.path().join(MissingSlotDep.as_ref());
+        let scanner = Scanner::new().jobs(1).checks([MissingSlotDep]);
+        let expected = glob_reports!("{check_dir}/*/reports.json");
+
+        // check dir restriction
+        let restrict = repo.restrict_from_path(&check_dir).unwrap();
+        let reports: Vec<_> = scanner.run(repo, [&restrict]).collect();
+        assert_eq!(&reports, &expected);
+
+        // repo restriction
+        let reports: Vec<_> = scanner.run(repo, [repo]).collect();
+        assert_eq!(&reports, &expected);
+    }
+}
