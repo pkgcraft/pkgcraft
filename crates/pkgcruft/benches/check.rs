65cffa345c0f9b86f1704695520a8ccaad405961
Tim Harder <radhermit@gmail.com>
2024-05-27 02:05:44 -0600

    pkgcruft: skip check benchmarks when required path variable is unset
    [skip ci]

--- crates/pkgcruft/benches/check.rs
+++ crates/pkgcruft/benches/check.rs
@@ -10,16 +10,18 @@ pub fn bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("Check");
     group.sample_size(10);
 
-    let path =
-        env::var("PKGCRUFT_GENTOO_REPO").unwrap_or_else(|e| panic!("PKGCRUFT_GENTOO_REPO: {e}"));
-    let repo = Repo::from_path("gentoo", path, 0, true).unwrap();
-    // TODO: checkout a specific commit
+    if let Ok(path) = env::var("PKGCRUFT_GENTOO_REPO") {
+        let repo = Repo::from_path("gentoo", path, 0, true).unwrap();
+        // TODO: checkout a specific commit
 
-    // run benchmark for every check
-    for check in CheckKind::iter() {
-        group.bench_function(check.to_string(), |b| {
-            let scanner = Scanner::new().checks([check]);
-            b.iter(|| scanner.run(&repo, [&repo]).count());
-        });
+        // run benchmark for every check
+        for check in CheckKind::iter() {
+            group.bench_function(check.to_string(), |b| {
+                let scanner = Scanner::new().checks([check]);
+                b.iter(|| scanner.run(&repo, [&repo]).count());
+            });
+        }
+    } else {
+        eprintln!("skipping check benchmarks: $PKGCRUFT_GENTOO_REPO unset");
     }
 }
