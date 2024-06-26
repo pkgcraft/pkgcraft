092cebd2181313f6963cc58d907648c49d08b071
Tim Harder <radhermit@gmail.com>
2024-06-26 14:44:04 -0600

    pk repo eapis: use shared test data repos for tests

--- crates/pkgcraft-tools/tests/pk/repo/eapis.rs
+++ crates/pkgcraft-tools/tests/pk/repo/eapis.rs
@@ -1,9 +1,6 @@
-use pkgcraft::repo::ebuild::temp::Repo as TempRepo;
 use pkgcraft::repo::Repository;
 use pkgcraft::test::{cmd, TEST_DATA};
 use predicates::prelude::*;
-
-use crate::predicates::lines_contain;
 
 #[test]
 fn missing_repo_arg() {
@@ -37,22 +34,8 @@ fn empty_repo() {
 }
 
 #[test]
-fn single() {
-    let repo = TempRepo::new("test", None, 0, None).unwrap();
-    repo.create_raw_pkg("cat/dep-1", &["EAPI=8"]).unwrap();
-    cmd("pk repo eapis")
-        .arg(repo.path())
-        .assert()
-        .stdout(predicate::str::is_empty().not())
-        .stderr("")
-        .success();
-}
-
-#[test]
-fn multiple() {
-    let repo = TempRepo::new("test", None, 0, None).unwrap();
-    repo.create_raw_pkg("cat/a-1", &["EAPI=7"]).unwrap();
-    repo.create_raw_pkg("cat/b-1", &["EAPI=8"]).unwrap();
+fn single_repo() {
+    let repo = TEST_DATA.ebuild_repo("metadata").unwrap();
     cmd("pk repo eapis")
         .arg(repo.path())
         .assert()
@@ -63,12 +46,10 @@ fn multiple() {
 
 #[test]
 fn multiple_repos() {
-    let t1 = TempRepo::new("test1", None, 0, None).unwrap();
-    t1.create_raw_pkg("cat/a-1", &["EAPI=7"]).unwrap();
-    let t2 = TempRepo::new("test2", None, 0, None).unwrap();
-    t2.create_raw_pkg("cat/b-1", &["EAPI=8"]).unwrap();
+    let repo1 = TEST_DATA.ebuild_repo("metadata").unwrap();
+    let repo2 = TEST_DATA.ebuild_repo("gentoo").unwrap();
     cmd("pk repo eapis")
-        .args([t1.path(), t2.path()])
+        .args([repo1.path(), repo2.path()])
         .assert()
         .stdout(predicate::str::is_empty().not())
         .stderr("")
@@ -77,9 +58,7 @@ fn multiple_repos() {
 
 #[test]
 fn option_eapi() {
-    let repo = TempRepo::new("test", None, 0, None).unwrap();
-    repo.create_raw_pkg("a/b-1", &["EAPI=8"]).unwrap();
-    repo.create_raw_pkg("cat/pkg-2", &["EAPI=8"]).unwrap();
+    let repo = TEST_DATA.ebuild_repo("metadata").unwrap();
 
     // invalid EAPI
     cmd("pk repo eapis --eapi nonexistent")
@@ -94,12 +73,12 @@ fn option_eapi() {
     cmd("pk repo eapis --eapi 8")
         .arg(repo.path())
         .assert()
-        .stdout(lines_contain(["a/b-1", "cat/pkg-2"]))
+        .stdout(predicate::str::is_empty().not())
         .stderr("")
         .success();
 
     // no matching packages
-    cmd("pk repo eapis --eapi 7")
+    cmd("pk repo eapis --eapi 6")
         .arg(repo.path())
         .assert()
         .stdout("")
