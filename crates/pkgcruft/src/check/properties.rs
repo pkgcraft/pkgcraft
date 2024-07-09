52789cb7e19df1b74cc27ff009192b5aa16b9108
Tim Harder <radhermit@gmail.com>
2024-07-08 18:39:16 -0600

    pkgcruft: add initial tests for Properties check

--- crates/pkgcruft/src/check/properties.rs
+++ crates/pkgcruft/src/check/properties.rs
@@ -55,3 +55,31 @@ impl EbuildPkgCheck for Check {
         // TODO: verify USE flags in conditionals
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use pkgcraft::repo::Repository;
+    use pkgcraft::test::{TEST_DATA, TEST_DATA_PATCHED};
+    use pretty_assertions::assert_eq;
+
+    use crate::scanner::Scanner;
+    use crate::test::glob_reports;
+
+    use super::*;
+
+    #[test]
+    fn check() {
+        // primary unfixed
+        let repo = TEST_DATA.repo("qa-primary").unwrap();
+        let dir = repo.path().join(CHECK);
+        let scanner = Scanner::new().jobs(1).checks([CHECK]);
+        let expected = glob_reports!("{dir}/*/reports.json");
+        let reports: Vec<_> = scanner.run(repo, [repo]).collect();
+        assert_eq!(&reports, &expected);
+
+        // primary fixed
+        let repo = TEST_DATA_PATCHED.repo("qa-primary").unwrap();
+        let reports: Vec<_> = scanner.run(repo, [repo]).collect();
+        assert_eq!(&reports, &[]);
+    }
+}
