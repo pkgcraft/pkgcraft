e0232cc9e0c97f3b7b6944ed3470ae1b3174cd1c
Tim Harder <radhermit@gmail.com>
2024-11-14 03:01:50 -0700

    pkgcruft diff: add output test

--- crates/pkgcruft/tests/pkgcruft/diff.rs
+++ crates/pkgcruft/tests/pkgcruft/diff.rs
@@ -1,5 +1,9 @@
+use std::io::Write;
+
 use pkgcraft::test::cmd;
 use predicates::str::contains;
+use pretty_assertions::assert_eq;
+use tempfile::NamedTempFile;
 
 use crate::replay::QA_PRIMARY_FILE;
 
@@ -61,7 +65,41 @@ fn empty() {
         .output()
         .unwrap()
         .stdout;
-    let data = String::from_utf8(output).unwrap();
-    let data: Vec<_> = data.lines().collect();
-    assert!(data.is_empty());
+    let output = String::from_utf8(output).unwrap();
+    let output: Vec<_> = output.lines().collect();
+    assert!(output.is_empty());
+}
+
+#[test]
+fn output() {
+    let old = indoc::indoc! {r#"
+        {"kind":"UnstableOnly","scope":{"Package":"cat/pkg"},"message":"arch"}
+        {"kind":"DependencyDeprecated","scope":{"Version":["cat/pkg-1-r2",null]},"message":"BDEPEND: cat/deprecated"}
+        {"kind":"WhitespaceInvalid","scope":{"Version":["cat/pkg-1-r2",{"line":3,"column":28}]},"message":"character '\\u{2001}'"}
+    "#};
+    let new = indoc::indoc! {r#"
+        {"kind":"UnstableOnly","scope":{"Package":"cat/pkg"},"message":"arch"}
+        {"kind":"DependencyDeprecated","scope":{"Version":["cat/pkg-1-r2",null]},"message":"BDEPEND: cat/deprecated"}
+        {"kind":"WhitespaceUnneeded","scope":{"Version":["cat/pkg-1-r2",{"line":3,"column":0}]},"message":"empty line"}
+    "#};
+
+    let mut old_file = NamedTempFile::new().unwrap();
+    old_file.write_all(old.as_bytes()).unwrap();
+    let mut new_file = NamedTempFile::new().unwrap();
+    new_file.write_all(new.as_bytes()).unwrap();
+
+    let expected = indoc::indoc! {r#"
+        -cat/pkg-1-r2, line 3, column 28: WhitespaceInvalid: character '\u{2001}'
+        +cat/pkg-1-r2, line 3: WhitespaceUnneeded: empty line
+    "#};
+    let expected: Vec<_> = expected.lines().collect();
+
+    let output = cmd("pkgcruft diff")
+        .args([old_file.path(), new_file.path()])
+        .output()
+        .unwrap()
+        .stdout;
+    let output = String::from_utf8(output).unwrap();
+    let output: Vec<_> = output.lines().collect();
+    assert_eq!(&output, &expected);
 }
