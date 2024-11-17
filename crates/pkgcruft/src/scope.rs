75583552cfe50f8bcb3561e32696c102a2ad1670
Tim Harder <radhermit@gmail.com>
2024-11-16 18:38:31 -0700

    pkgcruft: support converting restrictions into scopes

--- crates/pkgcruft/src/scope.rs
+++ crates/pkgcruft/src/scope.rs
@@ -1,3 +1,5 @@
+use pkgcraft::restrict::dep::Restrict as DepRestrict;
+use pkgcraft::restrict::Restrict;
 use strum::{AsRefStr, Display, EnumIter, EnumString, VariantNames};
 
 #[derive(
@@ -21,4 +23,31 @@ pub enum Scope {
     Package,
     Category,
     Repo,
+}
+
+impl From<&Restrict> for Scope {
+    fn from(value: &Restrict) -> Self {
+        use DepRestrict::{Category, Package, Version};
+
+        let restrict_scope = |restrict: &Restrict| match restrict {
+            Restrict::Dep(Category(_)) => Scope::Category,
+            Restrict::Dep(Package(_)) => Scope::Package,
+            Restrict::Dep(Version(Some(_))) => Scope::Version,
+            _ => Scope::Repo,
+        };
+
+        match value {
+            Restrict::And(vals) => vals
+                .iter()
+                .map(|x| restrict_scope(x))
+                .min()
+                .unwrap_or(Scope::Repo),
+            Restrict::Or(vals) => vals
+                .iter()
+                .map(|x| restrict_scope(x))
+                .max()
+                .unwrap_or(Scope::Repo),
+            _ => restrict_scope(value),
+        }
+    }
 }
