16088cb3664166a83c0fdc9bd67082afe7e90960
Tim Harder <radhermit@gmail.com>
2024-11-18 21:12:08 -0700

    pkgcruft: support converting pkgcraft errors into pkgcruft errors

--- crates/pkgcruft/src/error.rs
+++ crates/pkgcruft/src/error.rs
@@ -3,6 +3,8 @@ use std::io;
 #[derive(Debug, Clone, thiserror::Error)]
 pub enum Error {
     #[error("{0}")]
+    Pkgcraft(pkgcraft::Error),
+    #[error("{0}")]
     InvalidValue(String),
     #[error("skipping remaining checks due to failure")]
     SkipRemainingChecks,
@@ -13,5 +15,11 @@ pub enum Error {
 impl From<io::Error> for Error {
     fn from(e: io::Error) -> Self {
         Error::IO(format!("{e}: {}", e.kind()))
+    }
+}
+
+impl From<pkgcraft::Error> for Error {
+    fn from(e: pkgcraft::Error) -> Self {
+        Error::Pkgcraft(e)
     }
 }
