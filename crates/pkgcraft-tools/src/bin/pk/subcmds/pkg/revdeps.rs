4ea8c0cf2cda1a236513d0b04e5429053ba9f33c
Tim Harder <radhermit@gmail.com>
2024-11-22 14:44:59 -0700

    pk pkg revdeps: only run over a singular repo target

--- crates/pkgcraft-tools/src/bin/pk/subcmds/pkg/revdeps.rs
+++ crates/pkgcraft-tools/src/bin/pk/subcmds/pkg/revdeps.rs
@@ -6,14 +6,15 @@ use itertools::Itertools;
 use pkgcraft::cli::MaybeStdinVec;
 use pkgcraft::config::Config;
 use pkgcraft::dep::{CpvOrDep, Flatten};
-use pkgcraft::repo::RepoFormat;
 use pkgcraft::traits::{Intersects, LogErrors};
+
+use crate::args::target_ebuild_repo;
 
 #[derive(Debug, Args)]
 pub(crate) struct Command {
     /// Target repository
     #[arg(short, long, value_name = "REPO", default_value = ".")]
-    repo: Option<String>,
+    repo: String,
 
     // positionals
     /// Target dependencies
@@ -23,12 +24,8 @@ pub(crate) struct Command {
 
 impl Command {
     pub(super) fn run(&self, config: &mut Config) -> anyhow::Result<ExitCode> {
-        // determine target repo set
-        let repos = if let Some(target) = self.repo.as_ref() {
-            config.add_target_repo(target)?.into()
-        } else {
-            config.repos.set(Some(RepoFormat::Ebuild))
-        };
+        let repo = target_ebuild_repo(config, &self.repo)?;
+        config.finalize()?;
 
         // convert targets to Cpv or Dep objects
         let targets: Vec<_> = self
@@ -41,20 +38,16 @@ impl Command {
 
         // TODO: use a revdeps cache for queries (#120)
         // TODO: use parallel iterators (#121)
-        let mut failed = false;
         let mut stdout = io::stdout().lock();
-        for repo in repos.ebuild() {
-            let mut iter = repo.iter_ordered().log_errors();
-            for pkg in &mut iter {
-                for dep in pkg.dependencies([]).into_iter_flatten() {
-                    if targets.iter().any(|t| t.intersects(dep)) && dep.blocker().is_none() {
-                        writeln!(stdout, "{pkg}: {dep}")?;
-                    }
+        let mut iter = repo.iter_ordered().log_errors();
+        for pkg in &mut iter {
+            for dep in pkg.dependencies([]).into_iter_flatten() {
+                if targets.iter().any(|t| t.intersects(dep)) && dep.blocker().is_none() {
+                    writeln!(stdout, "{pkg}: {dep}")?;
                 }
             }
-            failed |= iter.failed();
         }
 
-        Ok(ExitCode::from(failed as u8))
+        Ok(ExitCode::from(iter))
     }
 }
