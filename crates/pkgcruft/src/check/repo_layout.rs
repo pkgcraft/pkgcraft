78ad1940b8d04d21f0ba12e558ca78a31157fe24
Tim Harder <radhermit@gmail.com>
2024-12-03 14:17:13 -0700

    pkgcruft: allow flagging nested empty pkgs in empty categories

--- crates/pkgcruft/src/check/repo_layout.rs
+++ crates/pkgcruft/src/check/repo_layout.rs
@@ -25,14 +25,16 @@ struct Check;
 impl RepoCheck for Check {
     fn run(&self, repo: &EbuildRepo, filter: &mut ReportFilter) {
         for category in repo.categories() {
-            let pkgs = repo.packages(&category);
-            if pkgs.is_empty() {
-                RepoCategoryEmpty.category(&category).report(filter);
-            }
-            for pkg in pkgs {
+            let mut pkgs = vec![];
+            for pkg in repo.packages(&category) {
                 if repo.versions(&category, &pkg).is_empty() {
                     RepoPackageEmpty.package((&category, &pkg)).report(filter);
+                } else {
+                    pkgs.push(pkg);
                 }
+            }
+            if pkgs.is_empty() {
+                RepoCategoryEmpty.category(&category).report(filter);
             }
         }
     }
