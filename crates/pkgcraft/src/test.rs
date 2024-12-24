4504150116a5d261c9f03d3924ee2e13c175f946
Tim Harder <radhermit@gmail.com>
2024-12-24 09:08:13 -0700

    pkgcraft: don't clear env when running test commands
    So capturing coverage works for external binaries since cargo-llvm uses
    environment variables to pass down required information.
    
    Fixes #221.

--- crates/pkgcraft/src/test.rs
+++ crates/pkgcraft/src/test.rs
@@ -20,7 +20,6 @@ pub fn cmd<S: AsRef<str>>(cmd: S) -> Command {
     let args: Vec<_> = cmd.as_ref().split_whitespace().collect();
     let mut cmd = Command::cargo_bin(args[0]).unwrap();
     cmd.args(&args[1..]);
-    cmd.env_clear();
     // disable config loading by default for pkgcraft-related commands
     cmd.env("PKGCRAFT_NO_CONFIG", "1");
     cmd
