56c84ee9ff2388ccd46e9524ed5666b5e2a593c9
Tim Harder <radhermit@gmail.com>
2024-12-26 06:16:50 -0700

    pk pkg metadata: add initial target tests

--- crates/pkgcraft-tools/tests/pk/pkg/metadata.rs
+++ crates/pkgcraft-tools/tests/pk/pkg/metadata.rs
@@ -1,1 +1,57 @@
+use std::env;
+
+use pkgcraft::config::Config;
+use pkgcraft::repo::ebuild::cache::Cache;
+use pkgcraft::repo::ebuild::EbuildRepoBuilder;
+use pkgcraft::test::cmd;
+
 super::cmd_arg_tests!("pk pkg metadata");
+
+#[test]
+fn targets() {
+    let mut config = Config::default();
+    let mut temp = EbuildRepoBuilder::new().build().unwrap();
+    temp.create_ebuild("cat/pkg-1", &[]).unwrap();
+    temp.create_ebuild("cat/pkg-2", &[]).unwrap();
+    temp.create_ebuild("cat/a-1", &[]).unwrap();
+    let repo = config
+        .add_repo(&temp, false)
+        .unwrap()
+        .into_ebuild()
+        .unwrap();
+
+    env::set_current_dir(&repo).unwrap();
+
+    // Cpv target
+    cmd("pk pkg metadata cat/pkg-1")
+        .assert()
+        .stdout("")
+        .stderr("")
+        .success();
+    for (cpv, status) in [("cat/pkg-1", true), ("cat/pkg-2", false), ("cat/a-1", false)] {
+        let path = repo.metadata().cache().path().join(cpv);
+        assert_eq!(path.exists(), status, "failed for {cpv}: {path}");
+    }
+
+    // Cpn target
+    cmd("pk pkg metadata cat/pkg")
+        .assert()
+        .stdout("")
+        .stderr("")
+        .success();
+    for (cpv, status) in [("cat/pkg-1", true), ("cat/pkg-2", true), ("cat/a-1", false)] {
+        let path = repo.metadata().cache().path().join(cpv);
+        assert_eq!(path.exists(), status, "failed for {cpv}: {path}");
+    }
+
+    // category target
+    cmd("pk pkg metadata cat")
+        .assert()
+        .stdout("")
+        .stderr("")
+        .success();
+    for (cpv, status) in [("cat/pkg-1", true), ("cat/pkg-2", true), ("cat/a-1", true)] {
+        let path = repo.metadata().cache().path().join(cpv);
+        assert_eq!(path.exists(), status, "failed for {cpv}: {path}");
+    }
+}
