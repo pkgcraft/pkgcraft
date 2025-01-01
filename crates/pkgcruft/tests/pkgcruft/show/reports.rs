835f4d617bd31ce17934bd69544b95364418bac5
Tim Harder <radhermit@gmail.com>
2025-01-01 10:33:45 -0700

    pkgcruft show reports: add more report alias tests

--- crates/pkgcruft/tests/pkgcruft/show/reports.rs
+++ crates/pkgcruft/tests/pkgcruft/show/reports.rs
@@ -1,5 +1,7 @@
 use itertools::Itertools;
 use pkgcraft::test::cmd;
+use pkgcraft::restrict::Scope;
+use pkgcruft::check::CheckKind;
 use pkgcruft::report::{ReportKind, ReportLevel};
 use predicates::prelude::*;
 use strum::IntoEnumIterator;
@@ -16,10 +18,34 @@ fn all() {
 }
 
 #[test]
-fn levels() {
+fn aliases() {
     for opt in ["-r", "--reports"] {
+        for check in CheckKind::iter() {
+            cmd(format!("pkgcruft show reports {opt} @{check}"))
+                .assert()
+                .stdout(predicate::str::is_empty().not())
+                .stderr("")
+                .success();
+        }
+
         for level in ReportLevel::iter() {
             cmd(format!("pkgcruft show reports {opt} @{level}"))
+                .assert()
+                .stdout(predicate::str::is_empty().not())
+                .stderr("")
+                .success();
+        }
+
+        for report in ReportKind::iter() {
+            cmd(format!("pkgcruft show reports {opt} {report}"))
+                .assert()
+                .stdout(format!("{report}\n"))
+                .stderr("")
+                .success();
+        }
+
+        for scope in Scope::iter() {
+            cmd(format!("pkgcruft show reports {opt} @{scope}"))
                 .assert()
                 .stdout(predicate::str::is_empty().not())
                 .stderr("")
