1deef24b7ff2f766c18469811e08e450f553f532
Tim Harder <radhermit@gmail.com>
2025-01-16 14:01:30 -0700

    pkgcruft: run non-package checks after package checks for repo scope scans
    So any generated reports are closer to finalized check reports which
    should mostly use repo scope as well.

--- crates/pkgcruft/src/iter.rs
+++ crates/pkgcruft/src/iter.rs
@@ -113,16 +113,16 @@ fn pkg_producer(
     finish_tx: Sender<Check>,
 ) -> thread::JoinHandle<()> {
     thread::spawn(move || {
-        // run non-package checks in parallel
-        for check in runner.checks().filter(|c| c.scope() > Scope::Package) {
-            tx.send((Some(check), Target::Repo)).ok();
-        }
-
         // parallelize running checks per package
         if runner.checks().any(|c| c.scope() <= Scope::Package) {
             for cpn in run.repo.iter_cpn_restrict(&run.restrict) {
                 tx.send((None, Target::Cpn(cpn))).ok();
             }
+        }
+
+        // run non-package checks in parallel
+        for check in runner.checks().filter(|c| c.scope() > Scope::Package) {
+            tx.send((Some(check), Target::Repo)).ok();
         }
 
         // wait for all parallelized checks to finish
