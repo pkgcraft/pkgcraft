90205f3cc26ccf2c5508396a8fcce2581d47e967
Tim Harder <radhermit@gmail.com>
2025-01-22 05:24:14 -0700

    pk pkg showkw: add initial -i/--ignore tests

--- crates/pkgcraft-tools/tests/pk/pkg/showkw.rs
+++ crates/pkgcraft-tools/tests/pk/pkg/showkw.rs
@@ -4,14 +4,27 @@ use predicates::prelude::*;
 super::cmd_arg_tests!("pk pkg showkw");
 
 #[test]
-fn invalid_pkgs() {
+fn ignore() {
     let data = test_data();
-    let repo = data.ebuild_repo("bad").unwrap();
+    let repo = data.ebuild_repo("qa-primary").unwrap();
+
+    // invalid pkgs log errors and cause failure by default
     cmd("pk pkg showkw")
         .arg(repo)
         .assert()
-        .stdout("")
+        .stdout(predicate::str::is_empty().not())
         .stderr(predicate::str::is_empty().not())
         .failure()
         .code(1);
+
+    // ignoring invalid pkgs entirely skips them
+    for opt in ["-i", "--ignore"] {
+        cmd("pk pkg showkw")
+            .arg(opt)
+            .arg(repo)
+            .assert()
+            .stdout(predicate::str::is_empty().not())
+            .stderr("")
+            .success();
+    }
 }
