fb6e14041b8cc95ce868845e9af75a5676507bec
Tim Harder <radhermit@gmail.com>
2025-02-21 00:19:11 -0700

    pkgcraft: migrate `eend` to clap parser

--- crates/pkgcraft/src/shell/commands/eend.rs
+++ crates/pkgcraft/src/shell/commands/eend.rs
@@ -1,37 +1,38 @@
 use std::io::Write;
 
-use scallop::{Error, ExecStatus};
+use scallop::ExecStatus;
 
 use crate::io::stderr;
-use crate::shell::unescape::unescape_iter;
+use crate::shell::unescape::unescape;
 
-use super::make_builtin;
+use super::{make_builtin, TryParseArgs};
 
-const LONG_DOC: &str = "Display information message when starting a process.";
+#[derive(clap::Parser, Debug)]
+#[command(
+    name = "eend",
+    long_about = "Indicates that the process begun with an ebegin message has completed."
+)]
+struct Command {
+    status: i32,
+    #[arg(required = false, default_value = "")]
+    message: String,
+}
 
-#[doc = stringify!(LONG_DOC)]
 fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
-    let (status, args) = match args {
-        [n, args @ ..] => match n.parse::<i32>() {
-            Err(_) => return Err(Error::Base(format!("invalid return value: {n}"))),
-            Ok(status) => (status, args),
-        },
-        [] => return Err(Error::Base("requires 1 or more args, got 0".to_string())),
-    };
-
+    let cmd = Command::try_parse_args(args)?;
     // TODO: support column-based formatting for success/failure indicators
     let mut stderr = stderr();
-    if status == 0 {
+    if cmd.status == 0 {
         writeln!(stderr, "[ ok ]")?;
     } else {
-        if !args.is_empty() {
-            let msg = unescape_iter(args)?.join(" ");
+        if !cmd.message.is_empty() {
+            let msg = unescape(&cmd.message)?;
             write!(stderr, "{msg} ")?;
         }
         writeln!(stderr, "[ !! ]")?;
     }
 
-    Ok(ExecStatus::from(status))
+    Ok(ExecStatus::from(cmd.status))
 }
 
 const USAGE: &str = "eend $?";
@@ -41,20 +42,20 @@ make_builtin!("eend", eend_builtin);
 mod tests {
     use crate::test::assert_err_re;
 
-    use super::super::{assert_invalid_args, cmd_scope_tests, eend};
+    use super::super::{assert_invalid_cmd, cmd_scope_tests, eend};
     use super::*;
 
     cmd_scope_tests!(USAGE);
 
     #[test]
     fn invalid_args() {
-        assert_invalid_args(eend, &[0]);
+        assert_invalid_cmd(eend, &[0, 3]);
     }
 
     #[test]
     fn non_numeric_return_code() {
-        assert_err_re!(eend(&["msg"]), "^invalid return value: msg$");
-        assert_err_re!(eend(&["a", "msg"]), "^invalid return value: a$");
+        assert!(eend(&["msg"]).is_err());
+        assert!(eend(&["a", "msg"]).is_err());
     }
 
     #[test]
@@ -62,11 +63,11 @@ mod tests {
         for (args, expected) in [
             (vec!["0"], "[ ok ]\n"),
             (vec!["0", "msg"], "[ ok ]\n"),
-            (vec!["0", "msg1", "msg2"], "[ ok ]\n"),
+            (vec!["0", "msg1 msg2"], "[ ok ]\n"),
             (vec!["1"], "[ !! ]\n"),
             (vec!["1", "msg"], "msg [ !! ]\n"),
             (vec!["1", r"\tmsg"], "\tmsg [ !! ]\n"),
-            (vec!["1", "msg1", "msg2"], "msg1 msg2 [ !! ]\n"),
+            (vec!["1", "msg1 msg2"], "msg1 msg2 [ !! ]\n"),
         ] {
             eend(&args).unwrap();
             assert_eq!(stderr().get(), expected);
