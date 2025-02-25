d2fa7747bca850a491f34ed08d896f7b15efc20b
Tim Harder <radhermit@gmail.com>
2025-02-25 12:41:32 -0700

    pkgcraft: simplify `docompress` pre phase hook

--- crates/pkgcraft/src/shell/hooks/docompress.rs
+++ crates/pkgcraft/src/shell/hooks/docompress.rs
@@ -6,8 +6,9 @@ use crate::shell::BuildData;
 pub(crate) fn pre(build: &mut BuildData) -> scallop::Result<ExecStatus> {
     let docompress_include_defaults = ["/usr/share/doc", "/usr/share/info", "/usr/share/man"]
         .into_iter()
-        .map(String::from);
-    let docompress_exclude_defaults = [format!("/usr/share/doc/{}/html", build.cpv().pf())];
+        .map(Into::into);
+    let pf = build.cpv().pf();
+    let docompress_exclude_defaults = [format!("/usr/share/doc/{pf}/html")];
     build.compress_include.extend(docompress_include_defaults);
     build.compress_exclude.extend(docompress_exclude_defaults);
     Ok(ExecStatus::Success)
