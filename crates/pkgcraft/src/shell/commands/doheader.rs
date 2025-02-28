74e787ca8a067ef91dc6af2992981df36c7b93a5
Tim Harder <radhermit@gmail.com>
2025-02-28 01:31:41 -0700

    pkgcraft: migrate `doheader` to clap parser

--- crates/pkgcraft/src/shell/commands/doheader.rs
+++ crates/pkgcraft/src/shell/commands/doheader.rs
@@ -1,5 +1,4 @@
-use std::path::Path;
-
+use camino::Utf8PathBuf;
 use itertools::Either;
 use scallop::{Error, ExecStatus};
 
@@ -7,21 +6,19 @@ use crate::eapi::Feature::ConsistentFileOpts;
 use crate::files::NO_WALKDIR_FILTER;
 use crate::shell::get_build_mut;
 
-use super::make_builtin;
+use super::{make_builtin, TryParseArgs};
 
-const LONG_DOC: &str = "Install header files into /usr/include/.";
+#[derive(clap::Parser, Debug)]
+#[command(name = "dodoc", long_about = "Install header files into /usr/include.")]
+struct Command {
+    #[arg(short = 'r')]
+    recursive: bool,
+    #[arg(required = true, value_name = "PATH")]
+    paths: Vec<Utf8PathBuf>,
+}
 
-#[doc = stringify!(LONG_DOC)]
 fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
-    let (recursive, args) = match args {
-        ["-r", args @ ..] => (true, args),
-        _ => (false, args),
-    };
-
-    if args.is_empty() {
-        return Err(Error::Base("requires 1 or more args, got 0".to_string()));
-    }
-
+    let cmd = Command::try_parse_args(args)?;
     let build = get_build_mut();
     let dest = "/usr/include";
     let opts = if build.eapi().has(ConsistentFileOpts) {
@@ -31,13 +28,12 @@ fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
     };
     let install = build.install().dest(dest)?.file_options(opts);
 
-    let (dirs, files): (Vec<_>, Vec<_>) = args.iter().map(Path::new).partition(|p| p.is_dir());
+    let (dirs, files): (Vec<_>, Vec<_>) = cmd.paths.iter().partition(|p| p.is_dir());
 
-    if !dirs.is_empty() {
-        if recursive {
+    if let Some(dir) = dirs.first() {
+        if cmd.recursive {
             install.recursive(dirs, NO_WALKDIR_FILTER)?;
         } else {
-            let dir = dirs[0].to_string_lossy();
             return Err(Error::Base(format!("installing directory without -r: {dir}")));
         }
     }
@@ -59,18 +55,17 @@ mod tests {
     use crate::shell::BuildData;
     use crate::test::assert_err_re;
 
-    use super::super::{assert_invalid_args, cmd_scope_tests, doheader, insopts};
+    use super::super::{assert_invalid_cmd, cmd_scope_tests, doheader, insopts};
     use super::*;
 
     cmd_scope_tests!(USAGE);
 
     #[test]
     fn invalid_args() {
-        assert_invalid_args(doheader, &[0]);
+        assert_invalid_cmd(doheader, &[0]);
 
         // missing args
-        let r = doheader(&["-r"]);
-        assert_err_re!(r, "^requires 1 or more args, got 0");
+        assert!(doheader(&["-r"]).is_err());
 
         let _file_tree = FileTree::new();
 
