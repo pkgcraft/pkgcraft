e575ddd7b6b9df2da88b52243bb65f8a679559b7
Tim Harder <radhermit@gmail.com>
2025-02-28 21:09:50 -0700

    pkgcraft: migrate `doconfd` to clap parser

--- crates/pkgcraft/src/shell/commands/doconfd.rs
+++ crates/pkgcraft/src/shell/commands/doconfd.rs
@@ -1,19 +1,24 @@
+use camino::Utf8PathBuf;
 use itertools::Either;
-use scallop::{Error, ExecStatus};
+use scallop::ExecStatus;
 
 use crate::eapi::Feature::ConsistentFileOpts;
 use crate::shell::get_build_mut;
 
-use super::make_builtin;
+use super::{make_builtin, TryParseArgs};
 
-const LONG_DOC: &str = "Install config files into /etc/conf.d/.";
+#[derive(clap::Parser, Debug)]
+#[command(
+    name = "doconfd",
+    long_about = "Install config files into /etc/conf.d/."
+)]
+struct Command {
+    #[arg(required = true, value_name = "PATH")]
+    paths: Vec<Utf8PathBuf>,
+}
 
-#[doc = stringify!(LONG_DOC)]
 fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
-    if args.is_empty() {
-        return Err(Error::Base("requires 1 or more args, got 0".into()));
-    }
-
+    let cmd = Command::try_parse_args(args)?;
     let build = get_build_mut();
     let dest = "/etc/conf.d";
     let opts = if build.eapi().has(ConsistentFileOpts) {
@@ -21,7 +26,11 @@ fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
     } else {
         Either::Right(build.insopts.iter().map(|s| s.as_str()))
     };
-    build.install().dest(dest)?.file_options(opts).files(args)?;
+    build
+        .install()
+        .dest(dest)?
+        .file_options(opts)
+        .files(&cmd.paths)?;
     Ok(ExecStatus::Success)
 }
 
@@ -37,14 +46,14 @@ mod tests {
     use crate::shell::BuildData;
     use crate::test::assert_err_re;
 
-    use super::super::{assert_invalid_args, cmd_scope_tests, doconfd, insopts};
+    use super::super::{assert_invalid_cmd, cmd_scope_tests, doconfd, insopts};
     use super::*;
 
     cmd_scope_tests!(USAGE);
 
     #[test]
     fn invalid_args() {
-        assert_invalid_args(doconfd, &[0]);
+        assert_invalid_cmd(doconfd, &[0]);
 
         let _file_tree = FileTree::new();
 
