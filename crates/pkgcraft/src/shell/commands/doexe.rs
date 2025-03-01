b600a38815fc6596a44351bd39c575b0cb4c13f1
Tim Harder <radhermit@gmail.com>
2025-02-28 21:00:19 -0700

    pkgcraft: migrate `doexe` to clap parser

--- crates/pkgcraft/src/shell/commands/doexe.rs
+++ crates/pkgcraft/src/shell/commands/doexe.rs
@@ -1,22 +1,28 @@
-use scallop::{Error, ExecStatus};
+use camino::Utf8PathBuf;
+use scallop::ExecStatus;
 
 use crate::shell::environment::Variable::EXEDESTTREE;
 use crate::shell::get_build_mut;
 
-use super::make_builtin;
+use super::{make_builtin, TryParseArgs};
 
-const LONG_DOC: &str = "Install executables.";
+#[derive(clap::Parser, Debug)]
+#[command(name = "doexe", long_about = "Install executables.")]
+struct Command {
+    #[arg(required = true, value_name = "PATH")]
+    paths: Vec<Utf8PathBuf>,
+}
 
-#[doc = stringify!(LONG_DOC)]
 fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
-    if args.is_empty() {
-        return Err(Error::Base("requires 1 or more args, got 0".into()));
-    }
-
+    let cmd = Command::try_parse_args(args)?;
     let build = get_build_mut();
     let dest = build.env(EXEDESTTREE);
     let opts = &build.exeopts;
-    build.install().dest(dest)?.file_options(opts).files(args)?;
+    build
+        .install()
+        .dest(dest)?
+        .file_options(opts)
+        .files(&cmd.paths)?;
     Ok(ExecStatus::Success)
 }
 
@@ -30,14 +36,14 @@ mod tests {
     use crate::shell::test::FileTree;
     use crate::test::assert_err_re;
 
-    use super::super::{assert_invalid_args, cmd_scope_tests, doexe, exeinto, exeopts};
+    use super::super::{assert_invalid_cmd, cmd_scope_tests, doexe, exeinto, exeopts};
     use super::*;
 
     cmd_scope_tests!(USAGE);
 
     #[test]
     fn invalid_args() {
-        assert_invalid_args(doexe, &[0]);
+        assert_invalid_cmd(doexe, &[0]);
 
         let _file_tree = FileTree::new();
 
