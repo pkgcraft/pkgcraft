4e5bd54967b1cda4fdf108479137b25cfaa76c7b
Tim Harder <radhermit@gmail.com>
2025-03-18 13:46:21 -0600

    pkgcraft: support serializing ebuild repo revdeps cache to qa reports format
    Note that this follows the QA reports formatting except it names the
    variant directories after the lowercase metadata key, e.g. RDEPEND -> rdepend,
    while upstream uses a custom name mapping, e.g. RDEPEND -> rindex.
    
    Fixes #273.

--- crates/pkgcraft/src/repo/ebuild/revdeps.rs
+++ crates/pkgcraft/src/repo/ebuild/revdeps.rs
@@ -1,6 +1,12 @@
 use std::collections::{HashMap, HashSet};
+use std::fs::{self, File};
+use std::io::Write;
+
+use camino::Utf8Path;
+use itertools::Itertools;
 
 use crate::dep::{ConditionalFlatten, Cpn, Cpv, Dep, UseDep};
+use crate::macros::build_path;
 use crate::pkg::ebuild::metadata::Key;
 use crate::pkg::Package;
 use crate::traits::LogErrors;
@@ -37,7 +43,7 @@ impl RevDepCache {
         let mut cache = Self::default();
 
         // TODO: build cache in parallel
-        let mut iter = repo.iter_ordered().log_errors(ignore);
+        let mut iter = repo.iter_unordered().log_errors(ignore);
         for pkg in &mut iter {
             for key in pkg.eapi().dep_keys().iter().copied() {
                 for (use_deps, dep) in pkg.dependencies([key]).into_iter_conditional_flatten()
@@ -63,5 +69,65 @@ impl RevDepCache {
     /// Get the reverse dependencies for a Cpn.
     pub fn get(&self, cpn: &Cpn) -> Option<&HashMap<RevDep, HashSet<Key>>> {
         self.0.get(cpn)
+    }
+
+    /// Serialize the cache to a directory using qa reports format.
+    pub fn serialize_to_qa<P: AsRef<Utf8Path>>(&self, path: P) -> crate::Result<()> {
+        let path = path.as_ref();
+        let dir = path.join("revdeps");
+        fs::create_dir_all(&dir)?;
+
+        // convert cache into qa reports compatible mapping
+        let mut mapping: HashMap<_, HashMap<_, Vec<_>>> = HashMap::new();
+        for (cpn, revdeps) in &self.0 {
+            for (revdep, keys) in revdeps {
+                for key in keys {
+                    mapping
+                        .entry(key.as_ref().to_lowercase())
+                        .or_default()
+                        .entry(cpn)
+                        .or_default()
+                        .push(revdep);
+                }
+            }
+        }
+
+        // return the prefix for the USE dependency
+        let enabled = |use_dep: &UseDep| -> &str {
+            if use_dep.enabled() {
+                ""
+            } else {
+                "!"
+            }
+        };
+
+        // write entries to disk in the expected file layout and format
+        for (key, revdeps) in mapping {
+            for (cpn, mut revdeps) in revdeps {
+                let path = build_path!(&dir, &key, cpn.category(), cpn.package());
+                fs::create_dir_all(path.parent().unwrap())?;
+                let mut f = File::create(path)?;
+                revdeps.sort_by(|a, b| a.cpv.cmp(&b.cpv));
+                for r in revdeps {
+                    let blocker = r.dep.blocker().map(|_| "[B]").unwrap_or_default();
+                    let cpv = &r.cpv;
+                    let flags = if !r.use_deps.is_empty() {
+                        format!(
+                            ":{}",
+                            r.use_deps
+                                .iter()
+                                .sorted()
+                                .map(|x| format!("{}{}", enabled(x), x.flag()))
+                                .join("+")
+                        )
+                    } else {
+                        Default::default()
+                    };
+                    writeln!(f, "{blocker}{cpv}{flags}")?;
+                }
+            }
+        }
+
+        Ok(())
     }
 }
