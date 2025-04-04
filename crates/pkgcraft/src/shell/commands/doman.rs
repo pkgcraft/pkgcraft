d2650d2e85d38d75e1e30af8cdc2b5249579d5c7
Tim Harder <radhermit@gmail.com>
2025-04-04 03:19:50 -0600

    pkgcraft: migrate `doman` to clap parser

--- crates/pkgcraft/src/shell/commands/doman.rs
+++ crates/pkgcraft/src/shell/commands/doman.rs
@@ -1,36 +1,70 @@
 use std::collections::HashSet;
+use std::str::FromStr;
 use std::sync::LazyLock;
 
-use camino::{Utf8Path, Utf8PathBuf};
+use camino::Utf8PathBuf;
 use regex::Regex;
 use scallop::{Error, ExecStatus};
 
 use crate::shell::get_build_mut;
 
-use super::make_builtin;
-
-const LONG_DOC: &str = "Install man pages into /usr/share/man.";
+use super::{make_builtin, TryParseArgs};
 
 static DETECT_LANG_RE: LazyLock<Regex> =
     LazyLock::new(|| Regex::new(r"^(?P<name>\w+)\.(?P<lang>[a-z]{2}(_[A-Z]{2})?)$").unwrap());
 
-#[doc = stringify!(LONG_DOC)]
-fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
-    let (args, mut lang) = match args {
-        [s, files @ ..] => {
-            let lang = s.strip_prefix("-i18n=").map(|s| s.trim_matches('"'));
-            if lang.is_some() {
-                // only the -i18n option was specified
-                if files.is_empty() {
-                    return Err(Error::Base("missing filename target".to_string()));
-                }
-                (files, lang)
-            } else {
-                (args, lang)
-            }
+#[derive(Debug, Clone)]
+struct Lang(String);
+
+impl FromStr for Lang {
+    type Err = Error;
+
+    fn from_str(s: &str) -> scallop::Result<Self> {
+        if let Some(value) = s.strip_prefix("-i18n=") {
+            Ok(Self(value.to_string()))
+        } else {
+            Err(Error::Base(format!("invalid lang option: {s}")))
         }
-        _ => return Err(Error::Base("requires 1 or more args, got 0".to_string())),
-    };
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ManPath(Utf8PathBuf);
+
+impl FromStr for ManPath {
+    type Err = Error;
+
+    fn from_str(s: &str) -> scallop::Result<Self> {
+        if s.strip_prefix("-i18n=").is_some() {
+            Err(Error::Base("missing filename target".to_string()))
+        } else {
+            Ok(Self(s.into()))
+        }
+    }
+}
+
+#[derive(clap::Parser, Debug)]
+#[command(
+    name = "doman",
+    disable_help_flag = true,
+    allow_missing_positional = true,
+    long_about = "Install man pages into /usr/share/man."
+)]
+struct Command {
+    #[arg(long, action = clap::ArgAction::HelpLong)]
+    help: Option<bool>,
+
+    // TODO: migrate to long option when clap supports single hyphens
+    // See https://github.com/clap-rs/clap/issues/2468.
+    #[arg(allow_hyphen_values = true)]
+    lang: Option<Lang>,
+
+    #[arg(required = true, allow_hyphen_values = true, value_name = "PATH")]
+    paths: Vec<ManPath>,
+}
+
+fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
+    let cmd = Command::try_parse_args(args)?;
 
     let install = get_build_mut()
         .install()
@@ -39,8 +73,9 @@ fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
 
     let mut dirs = HashSet::new();
     let mut files = vec![];
+    let mut lang = cmd.lang.as_ref().map(|x| x.0.as_str());
 
-    for path in args.iter().map(Utf8Path::new) {
+    for path in cmd.paths.iter().map(|x| x.0.as_path()) {
         let (mut base, ext) = match (path.file_stem(), path.extension()) {
             (Some(base), Some(ext)) => (base, ext),
             _ => {
@@ -69,7 +104,6 @@ fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
     Ok(ExecStatus::Success)
 }
 
-const USAGE: &str = "doman path/to/man/page";
 make_builtin!("doman", doman_builtin, true);
 
 #[cfg(test)]
@@ -79,24 +113,23 @@ mod tests {
     use crate::shell::test::FileTree;
     use crate::test::assert_err_re;
 
-    use super::super::{assert_invalid_args, cmd_scope_tests, doman};
-    use super::*;
+    use super::super::{assert_invalid_cmd, cmd_scope_tests, doman};
 
-    cmd_scope_tests!(USAGE);
+    cmd_scope_tests!("doman path/to/man/page");
 
     #[test]
     fn invalid_args() {
-        assert_invalid_args(doman, &[0]);
+        assert_invalid_cmd(doman, &[0]);
 
         let _file_tree = FileTree::new();
 
         // no targets
         let r = doman(&["-i18n=en"]);
-        assert_err_re!(r, "^missing filename target$");
+        assert_err_re!(r, "missing filename target");
 
         // nonexistent
         let r = doman(&["manpage"]);
-        assert_err_re!(r, "^invalid file target, use `newman`: .*$");
+        assert_err_re!(r, "invalid file target, use `newman`: .*");
     }
 
     #[test]
@@ -124,15 +157,13 @@ mod tests {
         );
 
         // -i18n option with empty lang
-        for opt in ["-i18n=", "-i18n=\"\""] {
-            doman(&[opt, "pkgcraft.1"]).unwrap();
-            file_tree.assert(
-                r#"
-                [[files]]
-                path = "/usr/share/man/man1/pkgcraft.1"
-            "#,
-            );
-        }
+        doman(&["-i18n=", "pkgcraft.1"]).unwrap();
+        file_tree.assert(
+            r#"
+            [[files]]
+            path = "/usr/share/man/man1/pkgcraft.1"
+        "#,
+        );
 
         // filename lang detection
         for (file, path) in [
