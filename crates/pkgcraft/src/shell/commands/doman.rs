8b521f0c073ae9b7b9989d33d0c865ee107a2edd
Tim Harder <radhermit@gmail.com>
2025-04-04 05:14:14 -0600

    pkgcraft: collapse file name parsing for `doman` targets

--- crates/pkgcraft/src/shell/commands/doman.rs
+++ crates/pkgcraft/src/shell/commands/doman.rs
@@ -10,8 +10,9 @@ use crate::shell::get_build_mut;
 
 use super::{make_builtin, TryParseArgs};
 
-static DETECT_LANG_RE: LazyLock<Regex> =
-    LazyLock::new(|| Regex::new(r"^(?P<name>\w+)\.(?P<lang>[a-z]{2}(_[A-Z]{2})?)$").unwrap());
+static DETECT_LANG_RE: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::new(r"^(?P<base>\w+)(\.(?P<lang>[a-z]{2}(_[A-Z]{2})?))?\.(?P<section>\d)$").unwrap()
+});
 
 #[derive(Debug, Clone)]
 struct Lang(String);
@@ -29,16 +30,43 @@ impl FromStr for Lang {
 }
 
 #[derive(Debug, Clone)]
-struct ManPath(Utf8PathBuf);
+struct ManPage {
+    base: String,
+    lang: Option<String>,
+    section: String,
+    path: Utf8PathBuf,
+}
 
-impl FromStr for ManPath {
+impl ManPage {
+    /// Return the name of the man page.
+    fn name(&self) -> String {
+        format!("{}.{}", self.base, self.section)
+    }
+
+    /// Return the directory of the man page.
+    fn dir(&self) -> String {
+        format!("man{}", self.section)
+    }
+}
+
+impl FromStr for ManPage {
     type Err = Error;
 
     fn from_str(s: &str) -> scallop::Result<Self> {
         if s.strip_prefix("-i18n=").is_some() {
-            Err(Error::Base("missing filename target".to_string()))
+            return Err(Error::Base("missing filename target".to_string()));
+        }
+
+        let path = Utf8PathBuf::from(s);
+        if let Some(m) = path.file_name().and_then(|x| DETECT_LANG_RE.captures(x)) {
+            Ok(Self {
+                base: m.name("base").unwrap().as_str().into(),
+                lang: m.name("lang").map(|x| x.as_str().into()),
+                section: m.name("section").unwrap().as_str().into(),
+                path,
+            })
         } else {
-            Ok(Self(s.into()))
+            Err(Error::Base(format!("invalid target, use `newman`: {s}")))
         }
     }
 }
@@ -60,7 +88,7 @@ struct Command {
     lang: Option<Lang>,
 
     #[arg(required = true, allow_hyphen_values = true, value_name = "PATH")]
-    paths: Vec<ManPath>,
+    manpages: Vec<ManPage>,
 }
 
 fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
@@ -73,29 +101,18 @@ fn run(args: &[&str]) -> scallop::Result<ExecStatus> {
 
     let mut dirs = HashSet::new();
     let mut files = vec![];
-    let mut lang = cmd.lang.as_ref().map(|x| x.0.as_str());
+    let opt_lang = cmd.lang.as_ref().map(|x| x.0.as_str());
 
-    for path in cmd.paths.iter().map(|x| x.0.as_path()) {
-        let (mut base, ext) = match (path.file_stem(), path.extension()) {
-            (Some(base), Some(ext)) => (base, ext),
-            _ => {
-                return Err(Error::Base(format!("invalid file target, use `newman`: {path}")))
-            }
-        };
-
-        if let Some(m) = DETECT_LANG_RE.captures(base) {
-            base = m.name("name").unwrap().as_str();
-            if lang.is_none() {
-                lang = Some(m.name("lang").unwrap().as_str());
-            }
-        }
+    for man in &cmd.manpages {
+        // determine man page language
+        let lang = opt_lang.or(man.lang.as_deref()).unwrap_or_default();
 
         // construct man page subdirectory
-        let mut mandir = Utf8PathBuf::from(lang.unwrap_or(""));
-        mandir.push(format!("man{ext}"));
+        let mut dir = Utf8PathBuf::from(lang);
+        dir.push(man.dir());
 
-        files.push((path, mandir.join(format!("{base}.{ext}"))));
-        dirs.insert(mandir);
+        files.push((&man.path, dir.join(man.name())));
+        dirs.insert(dir);
     }
 
     install.dirs(dirs)?;
@@ -128,16 +145,16 @@ mod tests {
         assert_err_re!(r, "missing filename target");
 
         // nonexistent
-        let r = doman(&["manpage"]);
-        assert_err_re!(r, "invalid file target, use `newman`: .*");
+        let r = doman(&["file"]);
+        assert_err_re!(r, "invalid target, use `newman`: .*");
     }
 
     #[test]
     fn creation() {
         let file_tree = FileTree::new();
+        fs::File::create("pkgcraft.1").unwrap();
 
         // standard file
-        fs::File::create("pkgcraft.1").unwrap();
         doman(&["pkgcraft.1"]).unwrap();
         file_tree.assert(
             r#"
