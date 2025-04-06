96a89e0a1a660012955e0220eabd39724adbe7b2
Tim Harder <radhermit@gmail.com>
2025-04-06 07:57:56 -0600

    pk completion: add mutually exclusive -d/--dir option to generate all files

--- crates/pkgcraft-tools/src/bin/pk/subcmds/completion.rs
+++ crates/pkgcraft-tools/src/bin/pk/subcmds/completion.rs
@@ -1,20 +1,37 @@
-use std::io;
+use std::{fs, io};
 use std::process::ExitCode;
 
-use clap::{Args, CommandFactory};
-use clap_complete::{generate, Shell};
+use camino::Utf8PathBuf;
+use clap::{Args, CommandFactory, ValueEnum};
+use clap_complete::{generate, generate_to, Shell};
 
 use crate::command;
 
 #[derive(Args)]
+#[clap(next_help_heading = "Completion options")]
 pub(crate) struct Command {
-    shell: Shell,
+    /// Target directory for completion files
+    #[arg(short, long, exclusive = true)]
+    dir: Option<Utf8PathBuf>,
+
+    /// Target shell
+    #[arg(required_unless_present = "dir")]
+    shell: Option<Shell>,
 }
 
 impl Command {
     pub(super) fn run(&self) -> anyhow::Result<ExitCode> {
         let mut cmd = command::Command::command();
-        generate(self.shell, &mut cmd, "pk", &mut io::stdout());
+
+        if let Some(dir) = &self.dir {
+            fs::create_dir_all(dir)?;
+            for &shell in Shell::value_variants() {
+                generate_to(shell, &mut cmd, "pk", dir)?;
+            }
+        } else if let Some(shell) = self.shell {
+            generate(shell, &mut cmd, "pk", &mut io::stdout());
+        }
+
         Ok(ExitCode::SUCCESS)
     }
 }
