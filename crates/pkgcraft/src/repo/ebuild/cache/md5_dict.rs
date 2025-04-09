f2abc6513ed983e7e6112d0f9c49658e77153a5f
Tim Harder <radhermit@gmail.com>
2025-04-09 06:53:20 -0600

    pkgcraft: simplify md5-dict metadata cache get()

--- crates/pkgcraft/src/repo/ebuild/cache/md5_dict.rs
+++ crates/pkgcraft/src/repo/ebuild/cache/md5_dict.rs
@@ -238,20 +238,14 @@ impl Cache for Md5Dict {
 
     fn get(&self, pkg: &EbuildRawPkg) -> Option<crate::Result<Self::Entry>> {
         let path = self.path.join(pkg.cpv().to_string());
-        let data = match fs::read_to_string(&path) {
-            Ok(value) => value,
-            Err(e) if e.kind() == io::ErrorKind::NotFound => return None,
-            Err(e) => {
-                return Some(Err(Error::IO(format!(
-                    "failed loading ebuild metadata: {path}: {e}"
-                ))));
-            }
-        };
-
-        Some(
-            data.parse::<Self::Entry>()
-                .and_then(|entry| entry.verify(pkg).and(Ok(entry))),
-        )
+        match fs::read_to_string(&path) {
+            Ok(data) => Some(
+                data.parse::<Self::Entry>()
+                    .and_then(|entry| entry.verify(pkg).and(Ok(entry))),
+            ),
+            Err(e) if e.kind() == io::ErrorKind::NotFound => None,
+            Err(e) => Some(Err(Error::IO(format!("failed loading metadata: {path}: {e}")))),
+        }
     }
 
     fn update(&self, pkg: &EbuildRawPkg, meta: &Metadata) -> crate::Result<()> {
