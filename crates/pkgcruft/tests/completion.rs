56689d61d31335135c86dd47a9493cdc2c3d0fde
Tim Harder <radhermit@gmail.com>
2025-04-13 05:17:22 -0600

    pkgcruft completion: add initial tests

---
+++ crates/pkgcruft/tests/completion.rs
@@ -0,0 +1,48 @@
+use std::fs;
+
+use pkgcraft::test::cmd;
+use predicates::prelude::*;
+use tempfile::tempdir;
+
+#[test]
+fn no_target() {
+    cmd("pkgcruft completion")
+        .assert()
+        .stdout("")
+        .stderr(predicate::str::is_empty().not())
+        .failure()
+        .code(2);
+}
+
+#[test]
+fn dir() {
+    let dir = tempdir().unwrap();
+    for opt in ["-d", "--dir"] {
+        cmd("pkgcruft completion")
+            .arg(opt)
+            .arg(dir.path())
+            .assert()
+            .stdout("")
+            .stderr("")
+            .success();
+        assert!(fs::read_dir(dir.path()).unwrap().next().is_some());
+    }
+}
+
+#[test]
+fn target() {
+    // invalid
+    cmd("pkgcruft completion unknown")
+        .assert()
+        .stdout("")
+        .stderr(predicate::str::is_empty().not())
+        .failure()
+        .code(2);
+
+    // valid
+    cmd("pkgcruft completion zsh")
+        .assert()
+        .stdout(predicate::str::is_empty().not())
+        .stderr("")
+        .success();
+}
--- crates/pkgcruft/tests/pkgcruft.rs
+++ crates/pkgcruft/tests/pkgcruft.rs
@@ -1,3 +1,4 @@
+mod completion;
 mod diff;
 mod ignore;
 mod replay;
