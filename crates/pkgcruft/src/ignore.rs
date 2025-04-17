182ad77a34d624e4e146c7ea31f077bef80145f3
Tim Harder <radhermit@gmail.com>
2025-04-17 12:17:24 -0600

    Revert "pkgcruft: simplify repo handling for IgnoreScopes iterator type"
    This creates a lot of unnecessary repo strings since the IgnoreScopes
    iterator is potentially created for every generated report variant
    across a scanning run.
    
    This reverts commit 8938f811020e9be17c8508364c2f7383c736ad53.

--- crates/pkgcruft/src/ignore.rs
+++ crates/pkgcruft/src/ignore.rs
@@ -2,7 +2,7 @@ use std::{fmt, fs};
 
 use dashmap::{mapref::one::RefMut, DashMap};
 use indexmap::{IndexMap, IndexSet};
-use pkgcraft::repo::{ebuild::EbuildRepo, PkgRepository, Repository};
+use pkgcraft::repo::{ebuild::EbuildRepo, PkgRepository};
 use pkgcraft::restrict::{Restrict, Scope};
 use pkgcraft::traits::FilterLines;
 use rayon::prelude::*;
@@ -180,23 +180,23 @@ impl fmt::Display for Ignore {
 /// This iterates in reverse precedence order allowing more specific ignore entries to
 /// override those at a larger scope. For example, package specific entries override repo
 /// settings.
-struct IgnoreScopes<'a> {
-    repo: String,
-    target: &'a ReportScope,
+struct IgnoreScopes<'a, 'b> {
+    repo: &'a EbuildRepo,
+    target: &'b ReportScope,
     scope: Option<Scope>,
 }
 
-impl<'a> IgnoreScopes<'a> {
-    fn new<R: Repository>(repo: R, target: &'a ReportScope) -> Self {
+impl<'a, 'b> IgnoreScopes<'a, 'b> {
+    fn new(repo: &'a EbuildRepo, target: &'b ReportScope) -> Self {
         Self {
-            repo: repo.to_string(),
+            repo,
             target,
             scope: Some(Scope::Repo),
         }
     }
 }
 
-impl Iterator for IgnoreScopes<'_> {
+impl Iterator for IgnoreScopes<'_, '_> {
     type Item = ReportScope;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -214,7 +214,7 @@ impl Iterator for IgnoreScopes<'_> {
                 (Scope::Category, ReportScope::Version(cpv, _)) => {
                     ReportScope::Category(cpv.category().into())
                 }
-                _ => ReportScope::Repo(self.repo.clone()),
+                _ => ReportScope::Repo(self.repo.to_string()),
             };
 
             // set the scope to the next lower level
