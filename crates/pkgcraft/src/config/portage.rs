d120d7779d3a1cf8f49c13605b1fab0f3f05955f
Tim Harder <radhermit@gmail.com>
2025-04-23 16:30:49 -0600

    pkgcraft: move closure into function for repos.conf file loading

--- crates/pkgcraft/src/config/portage.rs
+++ crates/pkgcraft/src/config/portage.rs
@@ -8,7 +8,50 @@ use tracing::{error, info};
 use crate::repo::{Repo, Repository};
 use crate::Error;
 
-/// Load repos from a given repos.conf path.
+/// Load repos from a repos.conf file.
+fn repos_from_file(path: &Utf8Path) -> crate::Result<Vec<Repo>> {
+    let ini = Ini::load_from_file(path)
+        .map_err(|e| Error::Config(format!("invalid repos.conf file: {path}: {e}")))?;
+
+    let repos: Vec<_> = ini
+        .iter()
+        .filter_map(|(section, settings)| match section {
+            Some(name) if name != "DEFAULT" => Some((name, settings)),
+            _ => None,
+        })
+        .filter_map(|(name, settings)| {
+            // pull supported fields from config
+            let priority = settings.get("priority").unwrap_or("0").parse().unwrap_or(0);
+            let Some(repo_path) = settings.get("location") else {
+                error!("invalid repos.conf file: {path}: missing location field: {name}");
+                return None;
+            };
+
+            // ignore invalid repos
+            match Repo::from_path(name, repo_path, priority) {
+                Ok(repo) => Some(repo),
+                Err(err) => {
+                    error!("{err}");
+                    None
+                }
+            }
+        })
+        .collect();
+
+    // log repos loaded from the file
+    let msg = if !repos.is_empty() {
+        repos.iter().map(|r| r.id()).join(", ")
+    } else {
+        "no repos found".to_string()
+    };
+
+    info!("loading portage config: {path}: {msg}");
+    Ok(repos)
+}
+
+/// Load all repos defined in a repos.conf path.
+///
+/// For directory targets, files are loaded in lexical order.
 pub(super) fn load_repos_conf<P: AsRef<Utf8Path>>(path: P) -> crate::Result<Vec<Repo>> {
     let path = path.as_ref();
 
@@ -22,47 +65,6 @@ pub(super) fn load_repos_conf<P: AsRef<Utf8Path>>(path: P) -> crate::Result<Vec<
         Err(e) => {
             return Err(Error::Config(format!("failed reading repos.conf: {path}: {e}")))
         }
-    };
-
-    // load all repos from a given file
-    let repos_from_file = |path: &Utf8Path| -> crate::Result<Vec<Repo>> {
-        let ini = Ini::load_from_file(path)
-            .map_err(|e| Error::Config(format!("invalid repos.conf file: {path}: {e}")))?;
-
-        let repos: Vec<_> = ini
-            .iter()
-            .filter_map(|(section, settings)| match section {
-                Some(name) if name != "DEFAULT" => Some((name, settings)),
-                _ => None,
-            })
-            .filter_map(|(name, settings)| {
-                // pull supported fields from config
-                let priority = settings.get("priority").unwrap_or("0").parse().unwrap_or(0);
-                let Some(repo_path) = settings.get("location") else {
-                    error!("invalid repos.conf file: {path}: missing location field: {name}");
-                    return None;
-                };
-
-                // ignore invalid repos
-                match Repo::from_path(name, repo_path, priority) {
-                    Ok(repo) => Some(repo),
-                    Err(err) => {
-                        error!("{err}");
-                        None
-                    }
-                }
-            })
-            .collect();
-
-        // log repos loaded from the file
-        let msg = if !repos.is_empty() {
-            repos.iter().map(|r| r.id()).join(", ")
-        } else {
-            "no repos found".to_string()
-        };
-
-        info!("loading portage config: {path}: {msg}");
-        Ok(repos)
     };
 
     // load ini files in lexical order
