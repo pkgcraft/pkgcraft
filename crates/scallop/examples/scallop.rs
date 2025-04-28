86e1fef1d65e9850a78bc16a4dbc09d48a7aa205
Tim Harder <radhermit@gmail.com>
2025-04-27 22:35:44 -0600

    scallop: drop unnecessary init when running as an interactive shell

--- crates/scallop/examples/scallop.rs
+++ crates/scallop/examples/scallop.rs
@@ -3,9 +3,6 @@ use std::env;
 use scallop::{builtins, shell};
 
 fn main() {
-    // initialize shell
-    shell::init();
-
     // load and enable builtins
     let builtins = [builtins::profile::BUILTIN];
     builtins::register(builtins);
