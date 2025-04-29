cf811071ac5204708d01c7da817d12ac935acdf5
Luca Barbato <lu_zero@gentoo.org>
2025-04-29 23:53:49 +0200

    pk pkg showkw: Provide a tabular view

--- crates/pkgcraft-tools/src/subcmds/pkg/showkw.rs
+++ crates/pkgcraft-tools/src/subcmds/pkg/showkw.rs
@@ -1,15 +1,17 @@
 use std::io::{self, Write};
 use std::process::ExitCode;
 
-use clap::{Args, builder::ArgPredicate};
+use clap::{builder::ArgPredicate, Args};
 use indexmap::IndexSet;
 use itertools::Itertools;
 use pkgcraft::cli::{MaybeStdinVec, Targets, TriState};
 use pkgcraft::config::Config;
+use pkgcraft::pkg::ebuild::keyword::{Arch, KeywordStatus};
 use pkgcraft::pkg::RepoPackage;
-use pkgcraft::pkg::ebuild::keyword::Arch;
 use pkgcraft::repo::RepoFormat;
 use pkgcraft::traits::LogErrors;
+use tabled::builder::Builder;
+use tabled::settings::Style;
 
 #[derive(Args)]
 #[clap(next_help_heading = "Target options")]
@@ -51,15 +53,29 @@ pub(crate) struct Command {
 impl Command {
     pub(super) fn run(&self, config: &mut Config) -> anyhow::Result<ExitCode> {
         // convert targets to pkgs
-        let mut iter = Targets::new(config)
+        let pkg_targets = Targets::new(config)
             .repo_format(RepoFormat::Ebuild)
             .repo(self.repo.as_deref())?
-            .finalize_pkgs(self.targets.iter().flatten())?
-            .ebuild_pkgs()
-            .log_errors(self.ignore);
+            .finalize_pkgs(self.targets.iter().flatten())?;
 
         let selected: IndexSet<_> = self.arches.iter().cloned().collect();
         let arch_filter = |arch: &Arch| -> bool { self.prefix || !arch.is_prefix() };
+
+        let arches: IndexSet<_> = pkg_targets
+            .ebuild_repo_restricts()
+            .map(|(repos, _)| repos.arches())
+            .flatten()
+            .filter(|&x| arch_filter(x))
+            .cloned()
+            .collect();
+
+        let mut b = Builder::new();
+        b.push_record(
+            std::iter::once(String::new())
+                .chain(arches.iter().map(|a| a.to_string().chars().join("\n"))),
+        );
+
+        let mut iter = pkg_targets.ebuild_pkgs().log_errors(self.ignore);
 
         let mut stdout = io::stdout().lock();
         for pkg in &mut iter {
@@ -74,11 +90,26 @@ impl Command {
                 .collect();
             // filter defaults by selected arches
             TriState::enabled(&mut enabled, &selected);
-
-            // TODO: support tabular output formats
-            let mut keywords = pkg.keywords().iter().filter(|x| enabled.contains(x.arch()));
-            writeln!(stdout, "{pkg}: {}", keywords.join(" "))?;
+            b.push_record(
+                std::iter::once(pkg.to_string()).chain(
+                    pkg.keywords()
+                        .iter()
+                        .filter(|x| enabled.contains(x.arch()))
+                        .map(|k| {
+                            match k.status() {
+                                KeywordStatus::Disabled => " ",
+                                KeywordStatus::Stable => "+",
+                                KeywordStatus::Unstable => "~",
+                            }
+                            .to_string()
+                        }),
+                ),
+            );
         }
+        let mut table = b.build();
+
+        table.with(Style::modern());
+        writeln!(stdout, "{table}")?;
 
         Ok(ExitCode::from(iter))
     }
