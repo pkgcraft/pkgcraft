c55df95881b8b70ff5fb807752e7ab3f9ae38ba2
Tim Harder <radhermit@gmail.com>
2025-05-24 05:08:07 -0600

    pkgcruft-git: add scan command tests

--- crates/pkgcruft-git/tests/lib.rs
+++ crates/pkgcruft-git/tests/lib.rs
@@ -3,7 +3,8 @@ use std::str;
 use std::sync::LazyLock;
 
 use assert_cmd::Command as assert_command;
-use pkgcraft::test::test_data;
+use pkgcraft::repo::ebuild::EbuildRepoBuilder;
+use pretty_assertions::assert_eq;
 use tempfile::Builder;
 
 static TARGET_DIR: LazyLock<String> = LazyLock::new(|| {
@@ -12,9 +13,9 @@ static TARGET_DIR: LazyLock<String> = LazyLock::new(|| {
 });
 
 #[tokio::test]
-async fn test_uds() {
-    let data = test_data();
-    let repo = data.ebuild_repo("qa-primary").unwrap();
+async fn uds() {
+    let repo = EbuildRepoBuilder::new().name("repo").build().unwrap();
+    git2::Repository::init(repo.path()).unwrap();
 
     // run from build dir
     let env: [(&str, &str); 1] = [("PATH", &TARGET_DIR)];
@@ -39,9 +40,9 @@ async fn test_uds() {
 }
 
 #[tokio::test]
-async fn test_tcp() {
-    let data = test_data();
-    let repo = data.ebuild_repo("qa-primary").unwrap();
+async fn tcp() {
+    let repo = EbuildRepoBuilder::new().name("repo").build().unwrap();
+    git2::Repository::init(repo.path()).unwrap();
 
     // run from build dir
     let env: [(&str, &str); 1] = [("PATH", &TARGET_DIR)];
@@ -70,4 +71,38 @@ async fn test_tcp() {
 
         service.kill().await.unwrap();
     }
+}
+
+#[tokio::test]
+async fn scan() {
+    let mut repo = EbuildRepoBuilder::new().name("repo").build().unwrap();
+    git2::Repository::init(repo.path()).unwrap();
+
+    // run from build dir
+    let env: [(&str, &str); 1] = [("PATH", &TARGET_DIR)];
+    let args = [repo.path().as_str()];
+
+    let tmp_dir = Builder::new().prefix("pkgcruft.").tempdir().unwrap();
+    let socket = tmp_dir.path().join("pkgcruft.sock");
+
+    let (mut service, socket) = pkgcruft_git::spawn(&socket, Some(env), Some(args), Some(5))
+        .await
+        .unwrap();
+
+    // empty repo
+    let mut cmd = assert_command::cargo_bin("pkgcruft-git").unwrap();
+    let output = cmd.arg("-c").arg(&socket).arg("scan").output().unwrap();
+    assert_eq!(str::from_utf8(&output.stdout).unwrap().trim(), "");
+
+    // invalid pkg
+    repo.create_ebuild("cat/pkg-1", &["EAPI=invalid"]).unwrap();
+    let mut cmd = assert_command::cargo_bin("pkgcruft-git").unwrap();
+    let output = cmd.arg("-c").arg(&socket).arg("scan").output().unwrap();
+    let expected = indoc::indoc! {"
+        cat/pkg
+          MetadataError: version 1: unsupported EAPI: invalid
+    "};
+    assert_eq!(str::from_utf8(&output.stdout).unwrap(), expected);
+
+    service.kill().await.unwrap();
 }
