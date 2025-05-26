7f122be9369301840662f8821555365a3fac9a1b
Tim Harder <radhermit@gmail.com>
2025-05-25 23:33:45 -0600

    pkgcruft-git: fix nonexistent unix domain socket path handling

--- crates/pkgcruft-git/src/uds.rs
+++ crates/pkgcruft-git/src/uds.rs
@@ -8,15 +8,12 @@ use crate::Error;
 // Verify if a given path is an accept UNIX domain socket path.
 pub(crate) fn verify_socket_path<P: AsRef<Utf8Path>>(path: P) -> crate::Result<()> {
     let path = path.as_ref();
-    let path = path
-        .canonicalize_utf8()
-        .map_err(|e| Error::InvalidValue(format!("invalid socket: {path}: {e}")))?;
     let socket_dir = &path
         .parent()
         .ok_or_else(|| Error::InvalidValue(format!("invalid socket: {path}")))?;
 
     // check if the socket is already in use
-    if UnixStream::connect(&path).is_ok() {
+    if UnixStream::connect(path).is_ok() {
         return Err(Error::InvalidValue(format!("service already running on: {path}")));
     }
 
@@ -24,7 +21,7 @@ pub(crate) fn verify_socket_path<P: AsRef<Utf8Path>>(path: P) -> crate::Result<(
     fs::create_dir_all(socket_dir).map_err(|e| {
         Error::InvalidValue(format!("failed creating socket dir: {socket_dir}: {e}"))
     })?;
-    fs::remove_file(&path).unwrap_or_default();
+    fs::remove_file(path).unwrap_or_default();
 
     Ok(())
 }
