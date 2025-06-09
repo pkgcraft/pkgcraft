dcf7c4afedac59708fa171f4bec00f836cb34a01
Tim Harder <radhermit@gmail.com>
2025-06-09 00:42:10 -0600

    pkgcraft: don't export Mirror::get_url()

--- crates/pkgcraft/src/repo/ebuild/metadata.rs
+++ crates/pkgcraft/src/repo/ebuild/metadata.rs
@@ -291,7 +291,7 @@ impl Mirror {
     }
 
     /// Return a mirrored url for a path.
-    pub fn get_url(&self, path: &str) -> crate::Result<Url> {
+    pub(crate) fn get_url(&self, path: &str) -> crate::Result<Url> {
         self.url
             .join(path.strip_prefix('/').unwrap_or(path))
             .map_err(|e| Error::InvalidValue(format!("invalid url: {e}")))
