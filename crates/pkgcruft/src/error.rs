6e925a50240e21f45fafd3888528231f4bc2bc7d
Tim Harder <radhermit@gmail.com>
2025-06-13 01:46:55 -0600

    pkgcruft: simplify converting pkgcraft errors

--- crates/pkgcruft/src/error.rs
+++ crates/pkgcruft/src/error.rs
@@ -4,17 +4,11 @@ use crate::report::ReportKind;
 #[derive(Debug, Clone, thiserror::Error)]
 pub enum Error {
     #[error("{0}")]
-    Pkgcraft(String),
-    #[error("{0}")]
     InvalidValue(String),
     #[error("{0}: report {1}")]
     ReportInit(ReportKind, String),
     #[error("{0}: check {1}")]
     CheckInit(Check, String),
-}
-
-impl From<pkgcraft::Error> for Error {
-    fn from(e: pkgcraft::Error) -> Self {
-        Error::Pkgcraft(e.to_string())
-    }
+    #[error("{0}")]
+    Pkgcraft(#[from] pkgcraft::Error),
 }
