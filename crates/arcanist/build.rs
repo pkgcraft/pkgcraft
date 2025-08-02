27b44d23d9b18ed50f7bfbcfa2822ad1a61ae3fc
Tim Harder <radhermit@gmail.com>
2025-08-02 14:12:32 -0600

    cargo: update to tonic and prost 0.14.*

--- crates/arcanist/build.rs
+++ crates/arcanist/build.rs
@@ -1,5 +1,6 @@
 fn build_proto() {
-    tonic_build::compile_protos("proto/arcanist.proto")
+    tonic_prost_build::configure()
+        .compile_protos(&["proto/arcanist.proto"], &["proto"])
         .unwrap_or_else(|e| panic!("failed to compile proto: {e}"));
     println!("cargo::rerun-if-changed=proto/arcanist.proto");
 }
--- crates/pkgcruft-git/build.rs
+++ crates/pkgcruft-git/build.rs
@@ -1,5 +1,6 @@
 fn build_proto() {
-    tonic_build::compile_protos("proto/pkgcruft.proto")
+    tonic_prost_build::configure()
+        .compile_protos(&["proto/pkgcruft.proto"], &["proto"])
         .unwrap_or_else(|e| panic!("failed to compile proto: {e}"));
     println!("cargo::rerun-if-changed=proto/pkgcruft.proto");
 }
