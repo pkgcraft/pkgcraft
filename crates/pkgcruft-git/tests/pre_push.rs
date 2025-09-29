e6d663b23b0c7d7a46cba162c37151deab0c0f7f
Tim Harder <radhermit@gmail.com>
2025-09-28 21:06:31 -0600

    pkgcruft-git: trigger conditional metadata scanning for pre-push hook tests

--- crates/pkgcruft-git/tests/pre_push.rs
+++ crates/pkgcruft-git/tests/pre_push.rs
@@ -50,9 +50,26 @@ async fn hook() {
     let hook_path = git_repo.path().join("hooks/pre-push");
     symlink(env!("CARGO_BIN_EXE_pkgcruft-git-pre-push"), hook_path).unwrap();
 
-    // add good commit to client repo
+    // create eclass
+    let data = indoc::indoc! {"
+        # stub eclass
+    "};
+    repo.create_eclass("e1", data).unwrap();
+    // create package
+    let data = indoc::indoc! {r#"
+        EAPI=8
+
+        inherit e1
+
+        DESCRIPTION="committed package"
+        HOMEPAGE="https://pkgcraft.pkgcraft"
+        LICENSE="abc"
+        SLOT=0
+    "#};
     repo.create_ebuild_from_str("cat/pkg-2", data).unwrap();
-    git_repo.stage(&["cat/pkg"]).unwrap();
+
+    // add good commit to client repo
+    git_repo.stage(&["eclass", "cat/pkg"]).unwrap();
     git!("commit -m good").current_dir(&repo).assert().success();
 
     // trigger hook via `git push`
@@ -63,14 +80,16 @@ async fn hook() {
         .stderr(predicate::str::is_empty().not())
         .success();
 
-    // add bad commit to client repo
+    // create bad package
     let data = indoc::indoc! {r#"
-        DESCRIPTION="bad package"
+        DESCRIPTION="package with unsupported EAPI"
         HOMEPAGE="https://pkgcraft.pkgcraft"
         LICENSE="abc"
         SLOT=0
     "#};
     repo.create_ebuild_from_str("cat/pkg-2", data).unwrap();
+
+    // add bad commit to client repo
     git_repo.stage(&["cat/pkg"]).unwrap();
     git!("commit -m bad").current_dir(&repo).assert().success();
 
