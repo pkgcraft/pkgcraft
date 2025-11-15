4ec63125c198f5cdeee48e16052c5c797cf28e11
Tim Harder <radhermit@gmail.com>
2025-11-15 10:35:00 -0700

    pkgcruft-git: simplify exit handling when running the daemon for tests

--- crates/pkgcruft-git/tests/utils.rs
+++ crates/pkgcruft-git/tests/utils.rs
@@ -62,29 +62,13 @@ impl PkgcruftServiceBuilder {
         let f = BufReader::new(stderr);
         let timeout = Duration::from_secs(1);
         let socket = match timeout_future(timeout, f.lines().next_line()).await {
-            Ok(Ok(Some(line))) => {
-                match PKGCRUFT_RE.captures(&line) {
-                    Some(m) => m.name("socket").unwrap().as_str().to_owned(),
-                    None => {
-                        // try to kill service, but ignore failures
-                        service.kill().await.ok();
-                        panic!("unknown message: {line}");
-                    }
-                }
-            }
-            Err(_) => {
-                service.kill().await.ok();
-                panic!("timed out");
-            }
-            Ok(Err(e)) => {
-                // unknown IO error
-                service.kill().await.ok();
-                panic!("{e}");
-            }
-            Ok(Ok(None)) => {
-                service.kill().await.ok();
-                panic!("no startup message found");
-            }
+            Ok(Ok(Some(line))) => match PKGCRUFT_RE.captures(&line) {
+                Some(m) => m.name("socket").unwrap().as_str().to_owned(),
+                None => panic!("unknown message: {line}"),
+            },
+            Err(_) => panic!("timed out"),
+            Ok(Err(e)) => panic!("{e}"),
+            Ok(Ok(None)) => panic!("no startup message found"),
         };
 
         PkgcruftService { _service: service, socket }
