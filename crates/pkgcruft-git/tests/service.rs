eb1bc25f0e5fcba4bab537eb7aebeda69d3276ed
Tim Harder <radhermit@gmail.com>
2025-11-15 20:18:13 -0700

    pkgcruft-git: re-enable scan test

--- crates/pkgcruft-git/tests/service.rs
+++ crates/pkgcruft-git/tests/service.rs
@@ -63,35 +63,37 @@ async fn tcp() {
     }
 }
 
-// TODO: fix failures due to stream disconnects under testing
-/*#[tokio::test]
+#[tokio::test]
 async fn scan() {
     let mut repo = EbuildRepoBuilder::new().name("repo").build().unwrap();
     git2::Repository::init(repo.path()).unwrap();
 
-    let service = PkgcruftServiceBuilder::new(repo.path()).spawn().await;
+    let tmp = NamedTempFile::new().unwrap();
+    let socket = tmp.path().to_str().unwrap();
+    let _service = PkgcruftServiceBuilder::new(repo.path())
+        .socket(socket)
+        .spawn()
+        .await
+        .unwrap();
 
     // empty repo
-    cmd("pkgcruft-git")
-        .arg("-c")
-        .arg(&service.socket)
-        .arg("scan")
-        .assert()
-        .stdout("")
-        .stderr("")
-        .success();
+    let mut cmd = Command::new(env!("CARGO_BIN_EXE_pkgcruft-git"));
+    cmd.args(["-c", socket]);
+    cmd.arg("scan");
+    let output = cmd.output().await.unwrap();
+    let data = String::from_utf8(output.stdout).unwrap();
+    assert_eq!(data, "");
 
     // invalid pkg
     repo.create_ebuild("cat/pkg-1", &["EAPI=invalid"]).unwrap();
-    cmd("pkgcruft-git")
-        .arg("-c")
-        .arg(&service.socket)
-        .arg("scan")
-        .assert()
-        .stdout(indoc::indoc! {"
-            cat/pkg
-              MetadataError: version 1: unsupported EAPI: invalid
-        "})
-        .stderr("")
-        .success();
-}*/
+    let mut cmd = Command::new(env!("CARGO_BIN_EXE_pkgcruft-git"));
+    cmd.args(["-c", socket]);
+    cmd.arg("scan");
+    let output = cmd.output().await.unwrap();
+    let data = String::from_utf8(output.stdout).unwrap();
+    let expected = indoc::indoc! {"
+        cat/pkg
+          MetadataError: version 1: unsupported EAPI: invalid
+    "};
+    assert_eq!(data, expected);
+}
