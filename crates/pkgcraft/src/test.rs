e68fee8f6d2bd23b802ece0750a93239f5315dc4
Tim Harder <radhermit@gmail.com>
2025-12-06 19:01:07 -0700

    drop nextest checks when initializing tests
    This breaks running individual test binaries under gdb or similar.
    Having it documented that tests require using nextest in general should
    be good enough.

--- crates/pkgcraft/src/test.rs
+++ crates/pkgcraft/src/test.rs
@@ -41,11 +41,6 @@ pub use define_cmd;
 #[cfg(test)]
 #[ctor::ctor]
 fn initialize() {
-    // verify running under `cargo nextest` ignoring benchmark runs
-    if !env::args().any(|x| x == "--bench") {
-        env::var("NEXTEST").expect("tests must be run via cargo-nextest");
-    }
-
     // initialize bash
     crate::shell::init().unwrap();
 
--- crates/scallop/src/test.rs
+++ crates/scallop/src/test.rs
@@ -1,14 +1,8 @@
 #![cfg(test)]
-use std::env;
 
 /// Initialization for all test executables.
 #[ctor::ctor]
 fn initialize() {
-    // verify running under `cargo nextest` ignoring benchmark runs
-    if !env::args().any(|x| x == "--bench") {
-        env::var("NEXTEST").expect("tests must be run via cargo-nextest");
-    }
-
     // initialize bash
     crate::shell::init(crate::shell::Env::default());
 }
