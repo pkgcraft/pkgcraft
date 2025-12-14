42d8b67e638de6d3201b95cf6214dbc88c9fe5e5
Tim Harder <radhermit@gmail.com>
2025-12-14 13:32:53 -0700

    pkgcraft: add ebuild-based pipestatus tests

--- crates/pkgcraft/src/shell/commands/pipestatus.rs
+++ crates/pkgcraft/src/shell/commands/pipestatus.rs
@@ -30,6 +30,11 @@ make_builtin!("pipestatus", pipestatus_builtin);
 
 #[cfg(test)]
 mod tests {
+    use crate::config::Config;
+    use crate::pkg::Build;
+    use crate::repo::ebuild::EbuildRepoBuilder;
+    use crate::shell::BuildData;
+
     use scallop::source;
 
     use super::super::{assert_invalid_cmd, cmd_scope_tests, functions::pipestatus};
@@ -76,5 +81,47 @@ mod tests {
         assert_eq!(stdout().get(), "");
         assert_eq!(pipestatus(&["-v"]).unwrap(), ExecStatus::Failure(123));
         assert_eq!(stdout().get(), "1 123 0\n");
+    }
+
+    #[test]
+    fn ebuild() {
+        let mut config = Config::default();
+        let mut temp = EbuildRepoBuilder::new().build().unwrap();
+
+        let data = indoc::indoc! {r#"
+            EAPI=9
+            DESCRIPTION="testing pipestatus"
+            SLOT=0
+
+            func1() {
+                return 1
+            }
+
+            func2() {
+                return 123
+            }
+
+            src_configure() {
+                # success
+                true | true | true
+                pipestatus -v; status=$?
+                [[ ${status} == 0 ]] || die "invalid pipestatus success status: ${status}"
+
+                # failure
+                func1 | func2 | true
+                pipestatus -v; status=$?
+                [[ ${status} == 123 ]] || die "invalid pipestatus failure status: ${status}"
+            }
+        "#};
+
+        let repo = config.add_repo(&temp).unwrap().into_ebuild().unwrap();
+        config.finalize().unwrap();
+
+        temp.create_ebuild_from_str("cat/pkg-1", data).unwrap();
+        let pkg = repo.get_pkg("cat/pkg-1").unwrap();
+        BuildData::from_pkg(&pkg);
+        let r = pkg.build();
+        assert!(r.is_ok(), "{}", r.unwrap_err());
+        assert_eq!(stdout().get(), "0 0 0\n1 123 0\n");
     }
 }
