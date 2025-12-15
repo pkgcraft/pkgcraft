48fc7541516136d018066e0bf7f85408373b7582
Tim Harder <radhermit@gmail.com>
2025-12-14 23:04:53 -0700

    scallop: add symmetric conversion tests for ExecStatus

--- crates/scallop/src/status.rs
+++ crates/scallop/src/status.rs
@@ -8,21 +8,21 @@ pub enum ExecStatus {
     Failure(i32),
 }
 
-impl From<ExecStatus> for i32 {
-    fn from(exec: ExecStatus) -> i32 {
-        match exec {
-            ExecStatus::Success => bash::EXECUTION_SUCCESS as i32,
-            ExecStatus::Failure(n) => n,
-        }
-    }
-}
-
 impl From<Error> for ExecStatus {
     fn from(e: Error) -> ExecStatus {
         match e {
             Error::Bail(_) => ExecStatus::Failure(bash::EX_LONGJMP as i32),
             Error::Status(n) => ExecStatus::Failure(n),
             _ => ExecStatus::Failure(1),
+        }
+    }
+}
+
+impl From<ExecStatus> for i32 {
+    fn from(exec: ExecStatus) -> i32 {
+        match exec {
+            ExecStatus::Success => bash::EXECUTION_SUCCESS as i32,
+            ExecStatus::Failure(n) => n,
         }
     }
 }
@@ -49,5 +49,25 @@ impl From<bool> for ExecStatus {
 impl From<ExecStatus> for bool {
     fn from(exec: ExecStatus) -> bool {
         matches!(exec, ExecStatus::Success)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn symmetric_conversion() {
+        // i32
+        for expected in [0, 1, 123] {
+            let status: ExecStatus = expected.into();
+            assert_eq!(expected, status.into());
+        }
+
+        // bool
+        for expected in [true, false] {
+            let status: ExecStatus = expected.into();
+            assert_eq!(expected, status.into());
+        }
     }
 }
