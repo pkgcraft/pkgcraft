13dcca8e8de36ced41e6d7bfee6ef72b0ab18bf7
Tim Harder <radhermit@gmail.com>
2025-12-31 06:17:29 -0700

    pk pkg env: ignore OS specific tmpdir path metadata
    Fixes tests on macos.

--- crates/pkgcraft-tools/tests/pkg/env.rs
+++ crates/pkgcraft-tools/tests/pkg/env.rs
@@ -77,7 +77,7 @@ fn globs() {
         .success();
 
     // disabled
-    cmd("pk pkg env -f -P*,-FILESDIR")
+    cmd("pk pkg env -f -P*,-FILESDIR,-DISTDIR,-HOME,-S,-T,-TMPDIR,-WORKDIR")
         .args(["-r", repo])
         .arg("inherit/indirect-8")
         .assert()
@@ -86,10 +86,8 @@ fn globs() {
             CATEGORY=inherit
             DEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             DESCRIPTION=ebuild with indirect eclass inherit
-            DISTDIR=/tmp
             EAPI=8
             EPREFIX=
-            HOME=/tmp
             HOMEPAGE=https://github.com/pkgcraft/a https://github.com/pkgcraft
             IDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             INHERITED=b a
@@ -98,13 +96,9 @@ fn globs() {
             RDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             REQUIRED_USE=global eclass a global eclass b
             RESTRICT=global eclass a global eclass b
-            S=/tmp
             SLOT=0/1
             SRC_URI=https://github.com/pkgcraft/a.tar.xz https://github.com/pkgcraft/pkgcraft-9999.tar.xz
-            T=/tmp
-            TMPDIR=/tmp
             USE=
-            WORKDIR=/tmp
         "})
         .stderr("")
         .success();
@@ -117,17 +111,15 @@ fn current_dir() {
 
     // package directory
     env::set_current_dir(repo.path().join("inherit/indirect")).unwrap();
-    cmd("pk pkg env -f -FILESDIR")
+    cmd("pk pkg env -f -FILESDIR,-DISTDIR,-HOME,-S,-T,-TMPDIR,-WORKDIR")
         .assert()
         .stdout(indoc::indoc! {"
             BDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             CATEGORY=inherit
             DEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             DESCRIPTION=ebuild with indirect eclass inherit
-            DISTDIR=/tmp
             EAPI=8
             EPREFIX=
-            HOME=/tmp
             HOMEPAGE=https://github.com/pkgcraft/a https://github.com/pkgcraft
             IDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             INHERITED=b a
@@ -144,20 +136,16 @@ fn current_dir() {
             RDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             REQUIRED_USE=global eclass a global eclass b
             RESTRICT=global eclass a global eclass b
-            S=/tmp
             SLOT=0/1
             SRC_URI=https://github.com/pkgcraft/a.tar.xz https://github.com/pkgcraft/pkgcraft-9999.tar.xz
-            T=/tmp
-            TMPDIR=/tmp
             USE=
-            WORKDIR=/tmp
         "})
         .stderr("")
         .success();
 
     // category directory
     env::set_current_dir(repo.path().join("inherit")).unwrap();
-    cmd("pk pkg env -f -FILESDIR")
+    cmd("pk pkg env -f -FILESDIR,-DISTDIR,-HOME,-S,-T,-TMPDIR,-WORKDIR")
         .assert()
         .stdout(indoc::indoc! {"
             inherit/direct-8::metadata
@@ -165,10 +153,8 @@ fn current_dir() {
             CATEGORY=inherit
             DEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg
             DESCRIPTION=ebuild with direct eclass inherit
-            DISTDIR=/tmp
             EAPI=8
             EPREFIX=
-            HOME=/tmp
             HOMEPAGE=https://github.com/pkgcraft
             IDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg
             INHERITED=a
@@ -185,23 +171,17 @@ fn current_dir() {
             RDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg
             REQUIRED_USE=global eclass a
             RESTRICT=global eclass a
-            S=/tmp
             SLOT=0
             SRC_URI=https://github.com/pkgcraft/pkgcraft-9999.tar.xz
-            T=/tmp
-            TMPDIR=/tmp
             USE=
-            WORKDIR=/tmp
 
             inherit/indirect-8::metadata
             BDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             CATEGORY=inherit
             DEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             DESCRIPTION=ebuild with indirect eclass inherit
-            DISTDIR=/tmp
             EAPI=8
             EPREFIX=
-            HOME=/tmp
             HOMEPAGE=https://github.com/pkgcraft/a https://github.com/pkgcraft
             IDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             INHERITED=b a
@@ -218,23 +198,17 @@ fn current_dir() {
             RDEPEND=cat/pkg ebuild/pkg cat/pkg eclass/pkg a/pkg cat/pkg eclass/pkg b/pkg
             REQUIRED_USE=global eclass a global eclass b
             RESTRICT=global eclass a global eclass b
-            S=/tmp
             SLOT=0/1
             SRC_URI=https://github.com/pkgcraft/a.tar.xz https://github.com/pkgcraft/pkgcraft-9999.tar.xz
-            T=/tmp
-            TMPDIR=/tmp
             USE=
-            WORKDIR=/tmp
 
             inherit/none-8::metadata
             BDEPEND=cat/pkg ebuild/pkg
             CATEGORY=inherit
             DEPEND=cat/pkg ebuild/pkg
             DESCRIPTION=ebuild with no eclass inherits
-            DISTDIR=/tmp
             EAPI=8
             EPREFIX=
-            HOME=/tmp
             HOMEPAGE=https://github.com/pkgcraft
             IDEPEND=cat/pkg ebuild/pkg
             LICENSE=l1
@@ -246,13 +220,9 @@ fn current_dir() {
             PV=8
             PVR=8
             RDEPEND=cat/pkg ebuild/pkg
-            S=/tmp
             SLOT=0
             SRC_URI=https://github.com/pkgcraft/pkgcraft-9999.tar.xz
-            T=/tmp
-            TMPDIR=/tmp
             USE=
-            WORKDIR=/tmp
         "})
         .stderr("")
         .success();
