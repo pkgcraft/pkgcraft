dc0e03c0db920cb7fff7f81ca841817ad314dc97
Tim Harder <radhermit@gmail.com>
2026-01-03 12:11:56 -0700

    pkgcraft: simplify assert_err_re!() variables

--- crates/pkgcraft/src/test.rs
+++ crates/pkgcraft/src/test.rs
@@ -360,18 +360,17 @@ pub use assert_unordered_eq;
 /// Assert an error matches a given regular expression for testing.
 #[macro_export]
 macro_rules! assert_err_re {
-    ($res:expr, $x:expr) => {
-        $crate::test::assert_err_re!($res, $x, "");
+    ($result:expr, $pattern:expr) => {
+        $crate::test::assert_err_re!($result, $pattern, "");
     };
-    ($res:expr, $re:expr, $msg:expr) => {
-        let err = $res.unwrap_err();
-        let s = err.to_string();
-        let re = ::regex::Regex::new($re.as_ref()).unwrap();
-        let err_msg = format!("{s:?} does not match regex: {:?}", $re);
+    ($result:expr, $pattern:expr, $msg:expr) => {
+        let err = $result.unwrap_err().to_string();
+        let re = ::regex::Regex::new(&$pattern).unwrap();
+        let err_msg = format!("{err:?} does not match regex: {re:?}");
         if $msg.is_empty() {
-            assert!(re.is_match(&s), "{}", err_msg);
+            assert!(re.is_match(&err), "{}", err_msg);
         } else {
-            assert!(re.is_match(&s), "{}", format!("{err_msg}: {}", $msg));
+            assert!(re.is_match(&err), "{}", format!("{err_msg}: {}", $msg));
         }
     };
 }
